a67e8eb4cd443be296630514efe6cc8ee1e5304b
agl <agl@unknown>
2003-05-07 19:15:01 +0000

    Change to the broken banner page
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@3583 67a373e5-eb02-0410-a15c-ee090a768436

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-testing
+<html>
+<head><title>Freenet Project - Technical Problems</title></head>
+<body>
+The Freenet website is having some technical problems at the moment.
+<p>
+In the mean time, Windows users may download Freenet <a
+href="/snapshots/freenet-webinstall.exe">here</a>, and Linux users may
+download Freenet <a
+href="http://freenetproject.org/snapshots/">here</a>.
+</body>
+</html>
 
