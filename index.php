5c098adc12da17980951dac5d4249f05e3dc7d7b
agl <agl@unknown>
2003-05-07 19:20:46 +0000

    Another minor change to test
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@3586 67a373e5-eb02-0410-a15c-ee090a768436

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head><title>Freenet Project - Technical Problems</title></head>
 <body>
-The Freenet website is having some technical problems at the moment.
+The Freenet website is having some major technical problems at the moment.
 <p>
 In the mean time Windows users may download Freenet <a
 href="/snapshots/freenet-webinstall.exe">here</a> and Linux users may
