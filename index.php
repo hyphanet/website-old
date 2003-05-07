50f505c771c9a818d7dc5d04a11c52c05cfeae6f
agl <agl@unknown>
2003-05-07 19:17:36 +0000

    Just removing a comma so I can see if the update scripts work
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@3584 67a373e5-eb02-0410-a15c-ee090a768436

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 The Freenet website is having some technical problems at the moment.
 <p>
-In the mean time, Windows users may download Freenet <a
+In the mean time Windows users may download Freenet <a
 href="/snapshots/freenet-webinstall.exe">here</a>, and Linux users may
 download Freenet <a
 href="http://freenetproject.org/snapshots/">here</a>.
