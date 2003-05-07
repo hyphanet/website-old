2e715fcfe95a8d508ab5ff1e4db43b86475202d7
agl <agl@unknown>
2003-05-07 20:15:20 +0000

    From Google cache
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@3595 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ pages/fcptools.php
@@ -0,0 +1,46 @@
+<h2>What are the FCPTools?
+
+</h2>
+<br/>The most useful component of the FCPTools is ezFCPlib, a portable ANSI-C based Freenet Client Library. It's been written to make writing Freenet clients as easy as possible.
+<br/>
+<br/>In addition, the FCPTools include command line utilities to insert and retrieve files (fcpput, fcpputsplit, fcpget) as well as entire freesites (fcpputsite) via ezFCPlib.
+<br/>
+<br/><hr/><h2>Contents of FCPTools:
+</h2>
+<br/><b>ezFCPlib</b> - the engine that drives the tools. Draft API doc included under ./ezFCPlib/doc.
+<br/>
+<br/><b>fcpget</b> - a key request utility.
+<br/>
+<br/><b>fcpput</b> - a key insert utility.
+
+<br/>
+<br/><b>fcpputsite</b> - a freesite insertion utility.
+<br/>
+<br/><b>fcpputsplit</b> - a splitfile insertion utility.
+<br/>
+<br/>
+<br/><hr/><h2><b style="color:black;background-color:#ffff66">Download</b>
+</h2>
+<br/><h3>Source
+</h3>
+<br/>Source tarballs are available from the <a class='wiki'  href='http://freenetproject.org/snapshots/'>snapshots directory</a>:
+
+<br/>
+<br/><a class='wiki'  href='http://freenetproject.org/snapshots/fcptools-unstable.tgz'>fcptools-unstable</a>: Latest version in CVS (mainly of interest to developers).
+<br/><a class='wiki'  href='http://freenetproject.org/snapshots/fcptools-stable.tgz'>fcptools-stable</a>: Latest working version (for users and freesite publishers).
+<br/>
+<br/>Uncompress the source as follows:
+<br/>
+<br/><tt>jcl@nero:~$ tar xfz FILENAME.tgz</tt>
+<br/>
+<br/>This will unpack the source into a new directory. Change into this directory, and follow the instructions in the INSTALL.* and README files.
+<br/>
+<br/><h3>Binaries
+</h3>
+<br/><a class='wiki'  href='http://freenetproject.org/snapshots/fcptools-bin.zip'><b style="color:black;background-color:#ffff66">Download</b> pre-compiled Win32 binaries</a> based on the stable source tree only. If you are running Windows and want to try the unstable code, follow the instructions in INSTALL.msvc to compile the source yourself.
+
+<br/>
+<br/><hr/><h2>Contributing
+</h2>
+<br/>The current maintainer of FCPTools is Jay Oliveri (<a class='wiki'  href='mailto:ilnero@gmx.net'>ilnero@gmx.net</a>). Send patches, backtraces and conspiracies to him. Be sure to read the <a href='tiki-index.php?page=DeveloperDocs' class='wiki'>developer documentation</a>.<br/>
+
