c39e09f945654cba9dee5426679341f04ed99902
agl <agl@unknown>
2003-05-07 19:18:41 +0000

    Removing another comma as another test
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@3585 67a373e5-eb02-0410-a15c-ee090a768436

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 The Freenet website is having some technical problems at the moment.
 <p>
 In the mean time Windows users may download Freenet <a
-href="/snapshots/freenet-webinstall.exe">here</a>, and Linux users may
+href="/snapshots/freenet-webinstall.exe">here</a> and Linux users may
 download Freenet <a
 href="http://freenetproject.org/snapshots/">here</a>.
 </body>
