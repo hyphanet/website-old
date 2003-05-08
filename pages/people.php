a4588178a5dea3248e9b89e13002acc7a54c1c50
agl <agl@unknown>
2003-05-08 11:34:07 +0000

    From Google cache
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@3599 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ pages/people.php
@@ -0,0 +1,30 @@
+<h3><b style="color:black;background-color:#ffff66">People</b></h3>
+
+<br/><b>Note:</b> This is an incomplete list, a large number of <b style="color:black;background-color:#ffff66">people</b> have
+<br/>contributed to the project. 
+<br/>
+<br/><a href="http://locut.us/" target="new"><b>Ian Clarke</b></a>
+<br/>Freenet is based on Ian's paper "A Distributed Decentralised Information
+<br/>Storage and Retrieval System".  Ian started the Freenet Project around July of
+<br/>1999, and continues to coordinate the project.
+<br/>
+<br/><b>Oskar Sandberg</b>
+<br/>Oskar was also one of the earliest contributors to the Freenet project, and
+
+<br/>continues to play a core role in ongoing development.
+<br/>
+<br/><a href="http://sgmiller.org" target="new"><b>Scott Miller</b></a>
+<br/>Scott is responsible for the implementation of much of the cryptography
+<br/>elements within Freenet.
+<br/>
+<br/><b>Matthew Toseland</b>
+<br/>Matthew has been working on Freenet since before the 0.5 release.  His work
+<br/>and that of others has resulted in dramatic improvements to the performance
+<br/>and stability of the network.
+<br/>
+<br/><b>Steven Starr</b>
+<br/>Steven is the coordinator of the Freenet Project non-profit corporation.<br/></div>
+
+<p class="editdate">Last modification date: Thursday 20 of February, 2003 [22:52:47] by todd</p>
+<br/>
+
