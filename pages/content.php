aab1b2bbc4ba0ca54dc0716c42a4f2cfe3918e55
agl <agl@unknown>
2003-05-08 20:25:12 +0000

    *** empty log message ***
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@3606 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ pages/content.php
@@ -0,0 +1,25 @@
+<h1>Content</h1>
+<h3>Freenet as a Content Distribution System</h3>
+In addition to its anonymity features, Freenet is also an effective way to distribute large files - often achieving download speeds faster than other content distribution systems.
+<p>
+As a content distribution network Freenet has the following advantages:
+<ul>
+<li><b>Totally decentralized</b><br>
+Information can be inserted into Freenet for download without any reliance on a centralized server.  All the inserter needs to do is give the content's "key" to someone else and they will be able to download it.
+<li><b>Adaptive caching</b><br>
+
+Many CDN architectures only allow peers which have already downloaded a file to share it with other peers.  Freenet will adaptively cache information on peers as necessary to meet demand regardless of what that peer has downloaded.  This allows Freenet to "scale-up" much more quickly than most solutions for popular files, and improves load-balancing.
+<li><b>Strong Security</b><br>
+Freenet has long-supported the concept of "Content Hash Keys" which guarantee the integrity of retrieved data.  This approach has since been adopted by other architectures.  Freenet also supports "Signed Subspace Keys" which allow content to be digitally signed.  This also allows content integrity to be guaranteed, but is more flexible than CHKs.
+<li><b>Forward Error Correction</b><br>
+In-common with some other CDN architectures, Freenet employs "Forward Error Correction", which allows files to be reconstructed even if some of the parts of that file can't be retrieved.  Uniquely, Freenet also supports "healing", which involves the reconstruction and reinsertion of missing file components.
+</ul>
+<p>
+This page contains links to freely available content on Freenet.  If you would like to add a new link please email it to <a href="mailto:ian@locut.us">us</a> - remember that we will only link to content that can be legally distributed in the United States of America.
+<h3>The Content Itself</h3>
+<h4>Software</h4>
+
+<ul>
+<li>Redhat 9 - 
+<a href="index.php?page=flink&flinkttl=shrike-i386-disc1.iso&key=CHK@JysskVMMHCjer-ITwbDaBmz5bmMRAwI,v2IV45MZU8c9FzO9r~GUhQ/shrike-i386-disc1.iso"><i>shrike-i386-disc1.iso</i></a>[<a href="index.php?page=flink&flinkttl=shrike-i386-disc1.iso&key=CHK@JysskVMMHCjer-ITwbDaBmz5bmMRAwI,v2IV45MZU8c9FzO9r~GUhQ/shrike-i386-disc1.iso">?</a>] <a href="index.php?page=flink&flinkttl=shrike-i386-disc2.iso&key=CHK@RXZpvFoh-kLDlIXvjiJVpJiYmFURAwI,CgPF6crA29NN6V~tALiXQg/shrike-i386-disc2.iso"><i>shrike-i386-disc2.iso</i></a>[<a href="index.php?page=flink&flinkttl=shrike-i386-disc2.iso&key=CHK@RXZpvFoh-kLDlIXvjiJVpJiYmFURAwI,CgPF6crA29NN6V~tALiXQg/shrike-i386-disc2.iso">?</a>] <a href="index.php?page=flink&flinkttl=shrike-i386-disc3.iso&key=CHK@sgPeLBJq~onirR95ebadZmIZgYkQAwI,2dAmaGdZPPfeG9GDX~OTEg/shrike-i386-disc3.iso"><i>shrike-i386-disc3.iso</i></a>[<a href="index.php?page=flink&flinkttl=shrike-i386-disc3.iso&key=CHK@sgPeLBJq~onirR95ebadZmIZgYkQAwI,2dAmaGdZPPfeG9GDX~OTEg/shrike-i386-disc3.iso">?</a>]</ul>
+
---
+++ pages/download.php
@@ -0,0 +1,22 @@
+<h2>Download Freenet 0.5.1</h2>
+<b>Note: Firewall/NAT users</b><br>
+If your computer is behind a firewall or NAT you must make some quick changes to your NAT's configuration for Freenet to work property.  Please see <a href="http://freenetproject.org/qcms/index.php?page=faq#firewall">here</a> for instructions.
+
+<h3>Windows</h3>
+Windows users should download and run <a href="http://freenetproject.org/snapshots/freenet-webinstall.exe">freenet-webinstall.exe</a>.  This will download other required components of Freenet, configure, and run the software.  When it is finished, double-click on the small blue rabbit icon in your task-tray.
+<h3>Linux</h3>
+Linux users should first install a recent Java Runtime Environment.  We have experienced best results with Sun which can be obtained from <a href="http://java.sun.com/">http://java.sun.com/</a>.  Once Java is installed, download <a href="http://prdownloads.sourceforge.net/freenet/freenet-0.5.1.tar.gz?download">freenet-0.5.1.tar.gz</a>.  Get started by typing:
+<pre>
+$ tar xfz freenet-0.5.1.tar.gz
+$ cd freenet
+$ sh start-freenet.sh
+</pre>
+After you start Freenet wait a few seconds for it to start-up (on a slow computer, you may need to wait about 30 seconds), and visit http://127.0.0.1:8888/ in your web browser to access Freenet's user interface.
+<h3>Upgrading</h3>
+
+After installing Freenet, Windows users can upgrade to the latest daily Freenet "snapshot" release by clicking on "Update Snapshot" in the Freenet section of their Start menu.
+<p>
+Linux users may similarily upgrade by running the update.sh shell script in the freenet/ directory.
+<h3>Hardware requirements</h3>
+We recommend a processor equivalent to at least a 400MHz Pentium 2/3, with at least 192MB of RAM.
+
