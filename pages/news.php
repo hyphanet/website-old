4c0cf64bb3ca02bfcea3c03b918fa1c6a808d738
Ian Clarke <ian@freenetproject.org>
2003-06-24 04:00:43 +0000

    blatent advert for ian's blog
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@3825 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -2,6 +2,17 @@
 few sentences - create a separate page and link to that (ie. Slashdot
 style). -->
 <h3>News</h3>
+<p><b>23rd June, 2003 - Ian responds to Peacefire paper</b>
+Ian just posted a <a 
+href="http://slashdot.org/~Sanity/journal/37275">response</a> 
+to Bennett Haselton's paper "Why a 
+"distributed cloud" of peer-to-peer systems will not be able to 
+circumvent Internet blocking in the long run" on his <a 
+href="http://slashdot.org/~sanity/journal">weblog</a> today.  In it 
+he argues that the attack Bennett describes, while being a 
+theoretical possibility, would be practically impossible even for the 
+best-funded of government Internet censors.
+</p>
 <p><b>14th June, 2003 - Your support needed</b><br>
 Things have been progressing well, we have almost finished a major 
 architectural improvement (migrating to java.nio) which should lead to 
@@ -20,18 +31,6 @@ prefer.
 <p><b>15th May, 2003 - Freenet 0.5.2-rc2 Released</b><br>
 The second release candidate for Freenet 0.5.2 has been released. This
 is mostly bugfixes over 0.5.2-rc1.
-Download it from <a href="index.php?page=download">the download
-page</a>.
-
-<!-- do not remove the notice below about RC1 without merging the
- features list into this one -->
-
-<p><b>26th April, 2003 - Freenet 0.5.2-rc1 Released</b><br>
-After weeks of work, the first release candidate for Freenet 0.5.2 has
-been released. Major changes include probabilistic caching, other
-network improvements, a zoomable Routing Table density graph, a fix to
-the Heisenbug, further improvements to the large file downloader, and
-many other bugfixes.
 Download it from <a href="index.php?page=download">the download
 page</a>.
 
