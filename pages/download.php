a767443aca4db73af1880c1b01a8ede4a7d566f6
Zlatin Balevsky <zlatinb@users.sourceforge.net>
2003-06-30 19:16:05 +0000

    reworking
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@3923 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/download.php
+++ pages/download.php
@@ -1,22 +1,31 @@
 <h2>Download Freenet 0.5.1</h2>
-<b>Note: Firewall/NAT users</b><br>
-If your computer is behind a firewall or NAT you must make some quick changes to your NAT's configuration for Freenet to work property.  Please see <a href="http://freenetproject.org/index.php?page=faq#firewall">here</a> for instructions.
+
+<i>If you have a firewall or a NAT box click <a href="#NAT">here</a> for some info.</i>
+
 <h3>Windows</h3>
-Windows users should download and run <a href="http://freenetproject.org/snapshots/freenet-webinstall.exe">freenet-webinstall.exe</a>.  This will download other required components of Freenet, configure, and run the software.  When it is finished, double-click on the small blue rabbit icon in your task-tray.
+Download and run <a href="http://freenetproject.org/snapshots/freenet-webinstall.exe">freenet-webinstall.exe</a>.  It will automatically install Freenet and other required components for you.  When done, double-click on the small blue rabbit icon in your task-tray.  Freenet will works much better with Windows 2000 or XP than with 95, 98 and ME.
 <h3>Unix</h3>
-Unix users should first install a recent Java Runtime Environment.  We have experienced best results with Sun which can be obtained from <a href="http://java.sun.com/">http://java.sun.com/</a>.  Once Java is installed, download <a href="http://prdownloads.sourceforge.net/freenet/freenet-0.5.2-rc2.tar.gz?download">freenet-0.5.2-rc2.tar.gz</a>.  Get started by typing:
+You need to have a recent Java Runtime Environment.  We have experienced best results with Sun JRE 1.4.1 which can be obtained from <a href="http://java.sun.com/">http://java.sun.com/</a>.  Once Java is installed, download <a href="http://prdownloads.sourceforge.net/freenet/freenet-0.5.2-rc2.tar.gz?download">freenet-0.5.2-rc2.tar.gz</a>.  Get started by typing:
 <pre>
 $ tar xfz freenet-0.5.2-rc2.tar.gz
 $ cd freenet
 $ sh start-freenet.sh
 </pre>
 After you start Freenet wait a few seconds for it to start-up (on a slow computer, you may need to wait about 30 seconds), and visit http://127.0.0.1:8888/ in your web browser to access Freenet's user interface.
+
+<h3>Hardware requirements</h3>
+Minimum: 400MHz Pentium 2, with at least 192MB of RAM.  Recomended: 1GHz or more processor with 384MB RAM or more (especially if using Windows XP).
+
 <h3>Upgrading</h3>
 
 After installing Freenet, Windows users can upgrade to the latest daily Freenet "snapshot" release by clicking on "Update Snapshot" in the Freenet section of their Start menu.
 <p>
 Linux users may similarily upgrade by running the update.sh shell script in the freenet/ directory.
 <h3>Software requirements</h3>
-As stated above, you will need a Java Runtime Environment. Also, Freenet will probably not work very well with Windows 95/98/ME - if you use Microsoft Windows, use NT 4.0, Microsoft Windows 2000 or Microsoft Windows XP.
-<h3>Hardware requirements</h3>
-We recommend a processor equivalent to at least a 400MHz Pentium 2/3, with at least 192MB of RAM. However, we recommend you run freenet at all times, in the background, for best performance, and some operating systems need more RAM than others. So we recommend around 384MB for Windows XP.
+You will need Java installed on your computer (the webinstall automatically does it for you). Also, 
+
+
+
+<a name="NAT">
+<b>Note: Firewall/NAT users</b><br>
+If your computer is behind a firewall or NAT you must make some quick changes to your NAT's configuration for Freenet to work property.  Please see <a href="http://freenetproject.org/index.php?page=faq#firewall">here</a> for instructions.
