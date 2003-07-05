b244882bf00a6e6996de74d5c44242e7169d37af
fvw <fvw@unknown>
2003-07-05 02:08:39 +0000

    Updates submitted by Nick Tarleton <nickptar@mindspring.com>
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4053 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/tools.php
+++ pages/tools.php
@@ -5,12 +5,14 @@
 <br />
 <br /><h3>Freenet Publishing & Retrieval Tools
 </h3>
-<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@M7yZgrl8gwtAe1xEcR5Xyv4tFsoPAgM/fiw/6//'><b>Freesite Insertion Wizard</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@M7yZgrl8gwtAe1xEcR5Xyv4tFsoPAgM/fiw/7//'><b>Freesite Insertion Wizard</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
 <br />
 <br />FIW is a feature packed Java AWT application for inserting freesites.  It contains a lot of help at each stage of the process, and includes site verification to increase availability of the inserts.
 <br />
-<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@kWu5Osv~VAI3-kH7z8QIVxklv-YPAgM/fishtools/34//'><b>FishTools</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
-
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@kWu5Osv~VAI3-kH7z8QIVxklv-YPAgM/fishtools/52//'><b>FishTools</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@CKesZYUJWn2GMvoif1R4SDbujIgPAgM/fuqid/9//'><b>Freenet Utility for Queued Inserts and Downloads</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br />
+<br />FUQID is a Windows program (but it will run in Wine under Linux) for inserting and downloading large files. It offers fast native CHK calculation and FEC decoding and a simple, highly configurable interface, better than FProxy.
 <br />
 <br />Freesite insertion tools written in Python (standalone binaries are available for Windows). Includes FEC support for alternative handling of large files.
 <br />
@@ -37,12 +39,12 @@
 <br />
 <br />FMB is an excellent Usenet-like tool which allows secure messages to be exchanged via Freenet, it is somewhat similar to Frost, but employs a more sophisticated "outbox" based approach. 
 <br />
-<br />It seems that the original author does not have the time to release new versions of it, some other Freenet users created modified versions. For more information visit <a class='wiki'  href='http://127.0.0.1:8888/SSK@De48eyKvA132GLzM3iIyon30JvoPAgM/wookie//'>Wookie's Freesite</a>. <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br />It seems that the original author does not have the time to release new versions of it, some other Freenet users created modified versions. For more information visit <a class='wiki'  href='http://127.0.0.1:8888/SSK@rjYFfgPHfolmcStiaoxESFfBXz8PAgM/FMBwishlist/11//'>FMB Wishlist</a>. <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
 
 <br />
 <br /><a class='wiki'  href='http://www.storrow.org/Software/FreeNews/'><b>FreeNews</b></a>
 <br />
-<br />FreeNews is an NNTP server that uses Freenet as its storage medium.  It allows you to use your favorite newsreader to read Text Over Freenet messages.
+<br />FreeNews is an NNTP server that uses Freenet as its storage medium.  It allows you to use your favorite newsreader to read Text Over Freenet messages, like frost uses.
 <br />
 <br /><h3>Misc Tools
 </h3>
@@ -53,7 +55,7 @@
 <br /><b>Update:</b> new versions written in OCaml will be released in binary form for Win32, Linux/x86, Linux/PPC, and OSX.
 <br />
 
-<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@fgbuxwSCCjOJHsUI-9-uijD1haQPAgM/flinks/8//'><b>Flinks</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@fgbuxwSCCjOJHsUI-9-uijD1haQPAgM/flinks/11//'><b>Flinks</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
 <br />
 <br />Flinks is an application to help people gather, manage, and share links in freenet.  Currently, several indicies generate flinks data (<a class='wiki'  href='http://127.0.0.1:8888/SSK@-w495UL3mfSlWC2c~nRAuG2fAWwPAgM/TFEE//'><b>The Freedom Engine Engine</b></a>, <a class='wiki'  href='http://127.0.0.1:8888/SSK@Sc6qV~D6iFhaYord6HtbjJ8MaEYPAgM/YoYo//'><b>YoYo</b></a>, and the <a class='wiki'  href='http://127.0.0.1:8888/SSK@rjYFfgPHfolmcStiaoxESFfBXz8PAgM/FreenetHelp//'><b>Freenet Help Index</b></a>, among others (<b><span style='color:#FF0000;'>You must be running Freenet for those links to work.</span></b>)), so you can go to their page and download their links, allowing you to essentially build your own index with sites you like
 
