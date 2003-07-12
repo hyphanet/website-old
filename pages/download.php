36e38d4ebc0d9963069d517935c5e6cfce695a46
Matthew Toseland <toad@amphibian.dyndns.org>
2003-07-12 02:35:14 +0000

    RC4
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4194 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/download.php
+++ pages/download.php
@@ -17,11 +17,11 @@ for some info.</i>
 Download and run    
 <big><a href="http://freenetproject.org/snapshots/freenet-webinstall.exe">freenet-webinstall.exe</a></big><br><br>
 It will automatically install Freenet and other required components for you  (It contains 
-<u><b>no</b> spyware or adware</u> , it's Free Software!). When done, double-click on the small blue rabbit icon in your task-tray.  Freenet works best with <b>Windows 2000 or XP.</b>
+<u><b>no</b> spyware or adware</u> , it's Free Software!). When done, double-click on the small blue rabbit icon in your task-tray.  Freenet works best with <b>Windows NT 4.0, 2000 Professional or XP. Windows 2000 Millennium Edition, Windows 98 (any edition), and Windows 95 work less well.</b>
 <h3>Unix</h3>
-You need to have a recent Java Runtime Environment.  We have experienced best results with Sun JRE 1.4.1 which can be obtained from <a href="http://java.sun.com/">http://java.sun.com/</a>.  Once Java is installed, download <a href="http://prdownloads.sourceforge.net/freenet/freenet-0.5.2-rc3.tar.gz?download">freenet-0.5.2-rc3.tar.gz</a>.  Get started by typing:
+You need to have a recent Java Runtime Environment.  We have experienced best results with Sun JRE 1.4.1 which can be obtained from <a href="http://java.sun.com/">http://java.sun.com/</a>.  Once Java is installed, download <a href="http://prdownloads.sourceforge.net/freenet/freenet-0.5.2-rc4.tar.gz?download">freenet-0.5.2-rc4.tar.gz</a>.  Get started by typing:
 <pre>
-$ tar xfz freenet-0.5.2-rc3.tar.gz
+$ tar xfz freenet-0.5.2-rc4.tar.gz
 $ cd freenet
 $ sh start-freenet.sh
 </pre>
