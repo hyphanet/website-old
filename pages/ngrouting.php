4f8590b5ccb4d8676498bfdfb34c931007b2c7b9
Ian Clarke <ian@freenetproject.org>
2003-07-18 03:26:21 +0000

    another update - not finished
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4271 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/ngrouting.php
+++ pages/ngrouting.php
@@ -12,7 +12,8 @@ in the Freenet network is required to provide a simple service to
 other nodes in the system. When a node receives a request for a
 particular key, it should do its best to retrieve the data
 corresponding to that key as quickly as possible and send it back to
-the node that requested it. 
+the node that requested it.  Provided that every node in the Freenet
+network performs this service reasonably well - Freenet will work.
 </P>
 <P>In the simplest case, the recipients of the request will already
 have the data cached locally, and can immediately send it to the
@@ -40,16 +41,39 @@ side effects of this approach was that nodes would tend to specialize
 in the retrieval of some keys to the exclusion of others. This can be
 seen is somewhat analogous to the way that people specialize in
 particular areas of expertise. This effect has been observed in
-actual Freenet nodes deployed in the Freenet network. This is a good
-indication that Freenet's routing algorithm is performing correctly. 
+actual Freenet nodes deployed in the Freenet network, the following
+image represents the keys stored by an actual Freenet node.  The X
+axis represents the "keyspace", at the left is key 0, across to the
+right which is key 2<sup>160</sup>.  Dark areas indicate where the
+node has better knowledge.  The dark strips indicate areas in which
+the node has detailed knowledge about where requests for those keys
+should be routed:
+</p><p>
+<center><img src="image/rthist.png" width="480" height="100"></center>
+</p><p>
+This is a good
+indication that Freenet's current routing algorithm is performing 
+correctly.
 </P>
+
 <H3>Next generation routing mechanism</H3>
 <P>The fact that it works, of course, does not mean that it cannot be
-improved upon. In fact, by making more effective use of the
+improved upon.   At the simplest level, note that
+the current algorithm does not distinguish between a slow Freenet node
+sitting at the end of a slow modem line in the remote Australian outback,
+and a powerful node connected to a T3 in downtown Los Angeles. Also note
+that while the current algorithm works, the only real way to test
+improvements to the algorithm is to see how they affect a large-scale
+network, either in simulation, or in the real world - this leads to
+a slow and cumbersome development cycle.
+By making more effective use of the
 information available to a Freenet node, we can dramatically improve
 a node's ability to route requests in a manner likely to result in
-the fastest response for that request. This is the goal of Next
-Generation Routing. 
+the fastest response for that request. By avoiding guesswork, and
+striving for a firm basis in statistical reality - we can even accelorate
+our development effort by allowing ourselves to determine the effectiveness
+of a modification based on its effect on a single node, before we deploy it.
+These goals are achieved by Next Generation Routing. 
 </P>
 <P>The core idea behind the Next Generation Routing design is to make
 Freenet nodes much smarter about deciding where to route information.
@@ -90,8 +114,8 @@ evenly distributed across the key-space.  When we have a new routing
 time sample for a particular key &ndash; we move the two points
 closest to our new sample towards it.  The amount they are moved can
 be adjusted to change how &ldquo;forgetful&rdquo; the estimator is.</P>
-<P><IMG SRC="../image/rte_diag.gif" NAME="Graphic1" ALIGN=CENTER WIDTH=335 HEIGHT=256 BORDER=0><BR CLEAR=LEFT><BR><BR>
-</P>
+<P><center><IMG SRC="image/rte_diag.gif" NAME="Graphic1" WIDTH=335 HEIGHT=256 BORDER=0><BR CLEAR=LEFT><BR><BR>
+</center></P>
 <P>In this diagram it can be seen that two of the reference points
 (blue) are being moved towards our new data sample (red).</P>
 <P>When we wish to create an estimate for a new key, we see where the
@@ -99,4 +123,22 @@ line between the reference points at either side of that key
 intersects (green) and this gives us our estimated response time.</P>
 <H3>Handling different data lengths</H3>
 <P>
+Also consider the fact that there isn't just one timing measurement
+that must be taken into account when we receive a DataReply, there
+are two.  The first is the time required to get the beginning
+of the response, the second is the time required to transfer the data.
+This raises the question of how we can combine these two timings
+into a single value which can be reported to the estimator algorithm
+described above.
 </P>
+<p>
+The answer we have settled upon is to use these two numbers to
+estimate what the total time between the request being sent and
+the transfer being <i>completed</i> would have been had the data
+been the average length of data in Freenet (which we in-turn estimate
+by taking the average length of data in the local datastore).  This
+is a single value which can be compared directly with other timing
+measurements even when they were made with requests where the data
+was differing sizes.
+</p>
+
