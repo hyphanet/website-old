561815f564c32b7248d091ddc3ac94692cde94c4
Ian Clarke <ian@freenetproject.org>
2003-08-26 22:30:04 +0000

    outsource archiving
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4550 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/lists.php
+++ pages/lists.php
@@ -9,7 +9,7 @@
 <br /><a
 href='http://mail.freenetproject.org/mailman/listinfo/support/'>Support</a>
 (<a
-href='http://mail.freenetproject.org/pipermail/support/'>archive</a>)
+href='http://www.mail-archive.com/support@freenetproject.org/'>archive</a>)
 <br />Asking for or giving advice relating to getting Freenet working, 
 bug reports, and suggestions on improving the user experience. Requests 
 for help are more likely to be heard here than in the other mailing 
@@ -18,20 +18,10 @@ subscribed to the list must be manually approved, and this can impose
 a delay of several days.  To avoid this delay, please <a 
 href="http://mail.freenetproject.org/mailman/listinfo/support/">subscribe</a>.
 <br />
-<br /><a class='wiki'  href='http://mail.freenetproject.org/mailman/listinfo/tech/'>Technical Discussion</a> (<a class='wiki'  href='http://mail.freenetproject.org/pipermail/tech/'>archive</a>)
-<br />Technical issues related to Freenet, not specifically tied to day-to-day implementation. Cryptography, packet radio, related projects, suggestions for future versions, attacks on the design, etc. Anything which deals with technology and relates to Freenet should go here.
-<br />
-<br /><a class='wiki'  href='http://mail.freenetproject.org/mailman/listinfo/web/'>Web</a> (<a class='wiki'  href='http://mail.freenetproject.org/pipermail/web/'>archive</a>)
-<br />Discussion of the organization and development of the Freenet website and associated translation efforts.
-<br />
-<br /><a class='wiki'  href='http://mail.freenetproject.org/mailman/listinfo/chat/'>Chat</a> (<a class='wiki'  href='http://mail.freenetproject.org/pipermail/chat/'>archive</a>)
-
-<br />General discussion about everything else. It is generally high traffic and low signal. Join at your own risk.
-<br />
-<br /><a class='wiki'  href='http://mail.freenetproject.org/mailman/listinfo/devl/'>Development</a> (<a class='wiki'  href='http://mail.freenetproject.org/pipermail/devl/'>archive</a>)
+<br /><a class='wiki'  href='http://mail.freenetproject.org/mailman/listinfo/devl/'>Development</a> (<a class='wiki'  href='http://www.mail-archive.com/devl@freenetproject.org/'>archive</a>)
 <br />Discussion pertaining to the next release of Freenet.  What features should and shouldn't go in, how things should be implemented, etc. Discussions about how Freenet should be in general (as opposed to in the very next release) should go to the technical list. If you think you have a great idea for a feature or attack, read all of the documentation first, especially the <a href='index.php?page=faq'>FAQ</a>.  The developers have little patience for repeated suggestions.
 <br />
-<br /><a class='wiki'  href='http://mail.freenetproject.org/mailman/listinfo/cvs'>CVS</a>  (<a class='wiki'  href='http://mail.freenetproject.org/pipermail/cvs/'>archive</a>)
+<br /><a class='wiki'  href='http://mail.freenetproject.org/mailman/listinfo/cvs'>CVS</a>  (<a class='wiki'  href='http://www.mail-archive.com/cvs@freenetproject.org/'>archive</a>)
 <br />A read-only mailing list to which CVS checkins are mailed.  This will probably only be of interest to developers.
 
 <br /><br />
