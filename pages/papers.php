d0b8bd258291f3b626b1d3daffbe03b668315f7d
Ian Clarke <ian@freenetproject.org>
2003-08-29 17:40:27 +0000

    doh
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4555 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/papers.php
+++ pages/papers.php
@@ -3,6 +3,7 @@
 This article describes the new mechanism for routing requests in Freenet.
 It holds the promise of dramatically increasing Freenet's speed and 
 reliability.
+<p>
 <a href="/papers/freenet-ieee.pdf">Protecting Freedom of Information 
 Online with Freenet</a><br>
 An IEEE Internet Computing article describing the Freenet architecture 
