561e7dfa1f364abd73c65100e2c1aa5831e9f010
Ian Clarke <ian@freenetproject.org>
2003-08-29 17:35:05 +0000

    add NGR article to papers page
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4554 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/papers.php
+++ pages/papers.php
@@ -1,4 +1,8 @@
 <h3>Papers</h3>
+<a href="index.php?page=ngrouting">Next Generation Routing Algorithm</a><br>
+This article describes the new mechanism for routing requests in Freenet.
+It holds the promise of dramatically increasing Freenet's speed and 
+reliability.
 <a href="/papers/freenet-ieee.pdf">Protecting Freedom of Information 
 Online with Freenet</a><br>
 An IEEE Internet Computing article describing the Freenet architecture 
