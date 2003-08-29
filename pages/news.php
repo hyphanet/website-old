c563e0bf4fd7a721d05db16e52db54c0d3b480f2
Ian Clarke <ian@freenetproject.org>
2003-08-29 16:53:26 +0000

    add patent news
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4553 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -2,6 +2,16 @@
 few sentences - create a separate page and link to that (ie. Slashdot
 style). -->
 <h3>News</h3>
+<p><b>29th August, 2003 - Fight Software Patents in EU</b></p>
+The European Commission has proposed to introduce US-style software patents
+in Europe.  This would be a disaster for innovation and competition in the
+European software industry as it would allow large companies to hoard
+software patents and use them to shut down smaller competitors.  Free software
+projects such as Freenet would be particularly vulnerable.  Visitors to this
+site that live within the EU are encouraged to write to their MEPs to register
+their concerns about this proposal.  Please visit <a href="http://swpat.ffii.org/">
+this page</a> for more information.
+
 <p><b>23rd August, 2003 - Next Gen routing now in experimental branch</b></p>
 The most fundamental improvement to Freenet's core algorithm, "Next Generation
 Routing", is now in the experimental branch of CVS.  NGrouting holds the
@@ -9,10 +19,4 @@ promise of making Freenet much faster than it is today, while improving
 virtually all aspects of Freenet's performance.  To learn more about
 NGrouting see <a href="/index.php?page=ngrouting">here</a>, if you would
 like to try it out see <a href="/index.php?page=tryng">here</a>.
-
-<p><b>21st July, 2003 - Next Generation Routing Paper</b></p>
-Ian just finished a paper describing "Next Generation Routing", a pretty revolutionary
-change to the core of Freenet's operation that has the potential to make Freenet as
-fast, and in some cases even faster, than the World Wide Web.  Read it 
-<a href="/index.php?page=ngrouting">here</a>.
 
