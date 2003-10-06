226e0c50a92e61cf5eb02c7d758616d7480fbd46
Ian Clarke <ian@freenetproject.org>
2003-10-06 16:53:16 +0000

    Added response to FUD
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4724 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -2,7 +2,29 @@
 few sentences - create a separate page and link to that (ie. Slashdot
 style). --> 
 
-<h3>News</h3> <p><b>15th September, 2003 - Ian Clarke selected as top
+<h3>News</h3> 
+<p><b>6th October, 2003 - Misleading Rumors</b></p>
+There have been some misleading 
+<a href="http://slashdot.org/article.pl?sid=03/10/06/1058250&mode=thread&tid=126&tid=158&tid=95&tid=99">
+rumors</a> circulating over the past
+day or two that there is an effort to create a "fork" of Freenet.
+This is not true.  There have also been some rather peculiar
+<a href="http://p2pnet.net/article/8315">reports</a> that the
+Freenet 0.6 release was premature.  This is news to the entire
+Freenet development team as there are no immediate plans to
+release Freenet 0.6, nor have there ever been.  Visitors to 
+<b>p2pnet.net</b> should take
+note of that news source's apparent lack of fact-checking abilities.
+<p>
+There was some discussion among the Freenet developers about how
+we can improve the stability of Freenet's stable branch, without
+hampering experimentation with Freenet's development branch.  We
+have now arrived at a consensus as to how this can be achieved.
+<p>
+In short, despite some recent issues caused by a significant overhaul
+of some core code, Freenet development is healthier than ever and
+progressing well.
+<p><b>15th September, 2003 - Ian Clarke selected as top
 Innovator of 2003</b></p>
 MIT's <a href="http://www.technologyreview.com/">Technology Review</a>
 magazine has selected Freenet's creator, Ian Clarke, as one of the top
@@ -10,16 +32,4 @@ magazine has selected Freenet's creator, Ian Clarke, as one of the top
 TR100) for his work on Freenet and related projects.  See <a 
 href="http://www.technologyreview.com/articles/tr100_1003.asp">here</a> 
 for further details.
-
-<p><b>31st August, 2003 - Next Gen Routing now in Unstable Branch</b></p>
-For the past few months we have been designing and implementing a radical
-new mechanism for finding information in Freenet called "Next Generation
-Routing".  This was described in a recent <a href="/index.php?page=ngrouting">
-article</a> by Ian Clarke.  We have been testing and refining this new code
-in our "experimental" development branch, and now we deem it stable enough
-to be moved into our "unstable" branch where we will test it further before
-moving it to "stable", eventually releasing it as Freenet 0.6.  We have
-put together some simple instructions for Freenet users who would like
-to try it out <a href="index.php?page=tryng">here</a>.
-
 
