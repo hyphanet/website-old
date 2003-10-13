b3783f656a6ea7dc8538329a4c9be73f64211758
newsbyte <newsbyte@unknown>
2003-10-13 15:19:54 +0000

    minor update
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4777 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -2,7 +2,7 @@
 <body>
 <table width="100%" border="0" cellspacing="0" cellpadding="0" height="100%">
 <tr>
-<td align="center" valign="top"><h2><span style='color:#FF0000;'><h4>New!</h4></span> Freenet Sponsorship:</h2>
+<td align="center" valign="top"><h2>Freenet Sponsorship:</h2>
 <h3>The following persons, organisations and companies have generously sponsored this project:</h3>
 <P>
 1) <span style='color:blue'><b>Verbum Vanum</b></span> <a href="http://www.verbumvanum.org">(http://www.verbumvanum.org)</a>  € 115
@@ -10,6 +10,10 @@
 </td>
 <tr>
 <td align="right" valign="bottom">
+<BR>
+<BR>
+<BR>
+<BR>
 Donation inquiries please contact <a href="mailto:ian@locut.us">Ian Clarke</a>
 </td>
 </body>
