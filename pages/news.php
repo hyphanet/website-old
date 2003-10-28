e86ae190a870c4e59217bd6ff0ea015c7df2eb18
Ian Clarke <ian@freenetproject.org>
2003-10-28 20:50:33 +0000

    big news
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@4945 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -9,21 +9,21 @@ After months of work we have finally decided that the "Next Generation
 Routing" code is ready for wider testing and have thus merged it into 
 the "stable" build of Freenet.
 <p>
-Next Generation Routing or NGR represents a MAJOR change to the way 
-freenet searches for data, while still preserving the underlying 
+Next Generation Routing or NGR represents a <b>major</b> change to the 
+way freenet searches for data, while still preserving the underlying 
 principles of its operation.  For example, Freenet now considers actual 
 network latency and bandwidth while making its routing decisions, while 
 also considering many other factors including reliability and the load 
 imposed on other nodes in the Freenet network.  We believe these 
 advances help to keep Freenet on the cutting-edge of P2P research while 
 also providing our users with a useful if far-from complete piece of 
-software.  To learn more about this see 
+software.  To learn more about NGR see 
 <a href="http://freenetproject.org/index.php?page=ngrouting">here</a>.
 <p>
 NGR is far from being the only improvement.  We have continued to 
 migrate Freenet over to Java's new non-blocking IO API.  We have made 
 major changes to Freenet's handling of connections to other nodes, added 
-many new diagnostics and configuration options, fixed many bugs.
+many new diagnostics and configuration options, and fixed many bugs.
 <p>
 We believe that this new build will be a great improvement on build 5028 
 and we urge all users to upgrade, while cautioning that it is unlikely 
