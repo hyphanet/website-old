50f6c21c28ab63ae1a3cdcd1eed6f7da012797ad
Ian Clarke <ian@freenetproject.org>
2003-11-12 11:38:44 +0000

    more bragging
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@5079 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/whatis.php
+++ pages/whatis.php
@@ -1,12 +1,27 @@
-<h3>What is Freenet?</h3>
-Freenet is free software which lets you publish and obtain information on the Internet without fear of censorship.  To achieve this freedom, the network is entirely decentralized and publishers and consumers of information are anonymous. Without anonymity there can never be true freedom of speech, and without decentralization the network will be vulnerable to attack.
+<h3>What is Freenet?</h3> Freenet is free software which lets you
+publish and obtain information on the Internet without fear of
+censorship.  To achieve this freedom, the network is entirely
+decentralized and publishers and consumers of information are anonymous.
+Without anonymity there can never be true freedom of speech, and without
+decentralization the network will be vulnerable to attack.
 
-<p>
-Communications by Freenet nodes are encrypted and are "routed-through" other nodes to make it extremely difficult to determine who is requesting the information and what its content is.
-<p>
-Users contribute to the network by giving bandwidth and a portion of their hard drive (called the "data store") for storing files. Unlike other peer-to-peer file sharing networks, Freenet does not let the user control what is stored in the data store. Instead, files are kept or deleted depending on how popular they are, with the least popular being discarded to make way for newer or more popular content. Files in the data store are encrypted to reduce the likelihood of prosecution by persons wishing to censor Freenet content.
-<p>
-The network can be used in a number of different ways and isn't restricted to just sharing files like other peer-to-peer networks. It acts more like an Internet within an Internet. For example Freenet can be used for:
+<p> Communications by Freenet nodes are encrypted and are
+"routed-through" other nodes to make it extremely difficult to determine
+who is requesting the information and what its content is.
+
+<p> Users contribute to the network by giving bandwidth and a portion of
+their hard drive (called the "data store") for storing files. Unlike
+other peer-to-peer file sharing networks, Freenet does not let the user
+control what is stored in the data store. Instead, files are kept or
+deleted depending on how popular they are, with the least popular being
+discarded to make way for newer or more popular content. Files in the
+data store are encrypted to reduce the likelihood of prosecution by
+persons wishing to censor Freenet content.
+
+<p> The network can be used in a number of different ways and isn't
+restricted to just sharing files like other peer-to-peer networks. It
+acts more like an Internet within an Internet. For example Freenet can
+be used for:
 
 <ul>
 <li>Publishing websites or 'freesites'
@@ -18,16 +33,17 @@ for gosh's sake remove this turn-based idiosyncracy.-->
 </ul>
 <p>
 
-Freenet is not just theoretical, it has been downloaded by over 2
-million users since the project started, and it is used for the
-distribution of censored information all over the world including
-countries such as <a href="http://freenet-china.org">China</a> and the
-Middle East.  Ideas and concepts pioneered in Freenet have had a
-significant impact in the academic world.  Our 2000 paper "Freenet: A
-Distributed Anonymous Information Storage and Retrieval System" was the
-most cited computer science paper of 2000 according to <a
+Unlike many cutting edge projects, Freenet long ago escaped the science
+lab, it has been downloaded by over 2 million users since the project
+started, and it is used for the distribution of censored information all
+over the world including countries such as <a
+href="http://freenet-china.org">China</a> and the Middle East.  Ideas
+and concepts pioneered in Freenet have had a significant impact in the
+academic world.  Our 2000 paper "Freenet: A Distributed Anonymous
+Information Storage and Retrieval System" was the most cited computer
+science paper of 2000 according to <a
 href="http://citeseer.nj.nec.com/articles2000.html">Citeseer</a>, and
-Freenet has also inspired papers in the worlds of law and philosophy.  
-Ian Clarke, Freenet's creator and project coordinator, was selected as 
-one of the top 100 innovators of 2003 my MIT's Technology Review 
+Freenet has also inspired papers in the worlds of law and philosophy. 
+Ian Clarke, Freenet's creator and project coordinator, was selected as
+one of the top 100 innovators of 2003 by MIT's Technology Review
 magazine.
