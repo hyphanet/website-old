5333abec423b551bbd92f917641da9298e887644
Ian Clarke <ian@freenetproject.org>
2003-11-15 19:50:24 +0000

    announce new stable build
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@5109 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -3,37 +3,16 @@ few sentences - create a separate page and link to that (ie. Slashdot
 style). --> 
 
 <h3>News</h3> 
-<p><b>28th October, 2003 - Next Generation Routing now in 
-"stable" build</b></p>
-After months of work we have finally decided that the "Next Generation 
-Routing" code is ready for wider testing and have thus merged it into 
-the "stable" build of Freenet.
+<p><b>15th September, 2003 - New stable build: 5034</b></p>
+A new stable build of Freenet is now available.  Among the core 
+improvements are a rethink of load balancing, and some 
+measures to help nodes route information more effectively from the 
+moment they start.  This should result in a better experience for new 
+users, along with superior overall performance.
 <p>
-Next Generation Routing or NGR represents a <b>major</b> change to the 
-way freenet searches for data, while still preserving the underlying 
-principles of its operation.  For example, Freenet now considers actual 
-network latency and bandwidth while making its routing decisions, while 
-also considering many other factors including reliability and the load 
-imposed on other nodes in the Freenet network.  We believe these 
-advances help to keep Freenet on the cutting-edge of P2P research while 
-also providing our users with a useful if far-from complete piece of 
-software.  To learn more about NGR see 
-<a href="http://freenetproject.org/index.php?page=ngrouting">here</a>.
-<p>
-NGR is far from being the only improvement.  We have continued to 
-migrate Freenet over to Java's new non-blocking IO API.  We have made 
-major changes to Freenet's handling of connections to other nodes, added 
-many new diagnostics and configuration options, and fixed many bugs.
-<p>
-We believe that this new build will be a great improvement on build 5028 
-and we urge all users to upgrade, while cautioning that it is unlikely 
-to be without bugs.
-<p>
-As always, while many have generously donated their time and 
-considerable expertise to these advances, they would not have been 
-possible without the dedication and hard work of Freenet's grossly 
-underpaid full-time developer, Matthew Toseland.  Please help us 
-continue to grossly underpay him by making a donation to the project 
-on our <a 
-href="http://freenetproject.org/index.php?page=donations">donations</a> 
-page.
+As usual with Freenet, the full benefit of these improvements will only 
+really become apparent when the new version of Freenet is widely 
+deployed.  Windows users can update by running the "Update Snapshot" 
+program in the Freenet section of their start menu, and Linux users can 
+update by running the <b>update.sh</b> script in their Freenet 
+directory.
