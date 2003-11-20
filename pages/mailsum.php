e2ddb845743fa653b4bf07716e1ef029dcede01b
newsbyte <newsbyte@unknown>
2003-11-20 19:19:21 +0000

    the same, but different (page summary maillist of nov 2003)
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@5146 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ pages/mailsum.php
@@ -0,0 +1,23 @@
+<table width="100%" border="0" cellspacing="0" cellpadding="0" height="100%">
+<tr>
+<center><h2>Summary of some discussions on the maillist</h2>
+</center>
+<P>
+<BR>
+<P>
+<LI> <span style='color:blue'><a href="http://www.freenetproject.org/index.php?page=mailsum1103"><b>November
+</b></a></span><P>
+<br>
+</td>
+<tr>
+<td align="right" valign="bottom">
+<BR>
+<P>
+<BR>
+<P>
+<BR>
+<P>
+<BR>
+Want to help with this page? Make a summary of a discussion on the freenet maillist and send it to <a href="mailto:newsbyte@freenetproject.org">NewsByte</a>
+</td>
+
