4a27635bfeb946e1a3303a994f9e6bfb53ebf124
Ian Clarke <ian@freenetproject.org>
2003-11-22 17:24:57 +0000

    beg
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@5155 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -3,16 +3,27 @@ few sentences - create a separate page and link to that (ie. Slashdot
 style). --> 
 
 <h3>News</h3> 
-<p><b>15th November, 2003 - New stable build: 5034</b></p>
-A new stable build of Freenet is now available.  Among the core 
-improvements are a rethink of load balancing, and some 
-measures to help nodes route information more effectively from the 
-moment they start.  This should result in a better experience for new 
-users, along with superior overall performance.
+<p><b>22nd November, 2003 - Your support needed</b></p>
+We are making rapid progress towards our <b>0.6</b> release, which will
+contain a variety of fundamental improvements to the Freenet.  As most
+are aware, our development effort has received a significant boost by
+being able to employ Matthew Toseland full-time to work on the project,
+paid for by your generous donations.
 <p>
-As usual with Freenet, the full benefit of these improvements will only 
-really become apparent when the new version of Freenet is widely 
-deployed.  Windows users can update by running the "Update Snapshot" 
-program in the Freenet section of their start menu, and Linux users can 
-update by running the <b>update.sh</b> script in their Freenet 
-directory.
+Unfortunately the fundamental nature of the changes we are working on 
+make it impossible for us to do formal "point" releases of the software, 
+and these have traditionally been when we receive most of our donations.  
+As a result, the project's current funding levels are so low that we can 
+only afford to continue employing Matthew for another couple of days as 
+things stand right now.
+<p>
+Because of this, we are making an appeal for donations so that we can 
+keep Matthew working on the project, as many will know, his involvement 
+as provided valuable momentum to the development effort.  Matthew is 
+paid just a fraction of what he could earn working commercially, so you 
+can be sure that your contribution will be well spent.
+<p>
+If you can spare any money, please visit our <a 
+href="/index.php?page=donate">donations page</a> and make a 
+contribution.  You can donate via Paypal using a credit card, or via 
+E-Gold.  Your support is much appreciated.
