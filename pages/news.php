97fed2e58a2639f62fc585b0d4cd2ace7a72d3b3
Ian Clarke <ian@freenetproject.org>
2004-01-13 11:19:07 +0000

    new news
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@5731 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -3,27 +3,12 @@ few sentences - create a separate page and link to that (ie. Slashdot
 style). --> 
 
 <h3>News</h3> 
-<p><b>22nd November, 2003 - Your support needed</b></p>
-We are making rapid progress towards our <b>0.6</b> release, which will
-contain a variety of fundamental improvements.  As most
-are aware, our development effort has received a significant boost by
-being able to employ Matthew Toseland full time to work on the project,
-paid for by your generous donations.
-<p>
-Unfortunately, the fundamental nature of the changes we are working on 
-make it impossible for us to do formal "point" releases of the software, 
-and these have traditionally been when we receive most of our donations.  
-As a result, the project's current funding levels are so low that we can 
-only afford to continue employing Matthew for another couple of days as 
-things stand right now.
-<p>
-Because of this, we are making an appeal for donations so that we can 
-keep Matthew working on the project, as many will know, his involvement 
-as provided valuable momentum to the development effort.  Matthew is 
-paid just a fraction of what he could earn working commercially, so you 
-can be sure that your contribution will be well spent.
-<p>
-If you can spare any money, please visit our <a 
-href="/index.php?page=donate">donations page</a> and make a 
-contribution.  You can donate via Paypal using a credit card, or via 
-E-Gold.  Your support is much appreciated.
+<p><b>13th January, 2004 - Stable build 5054 released</b></p>
+After discovering a major bug which we suspect to have been the cause of 
+Freenet's recent poor performance, we have back-ported a number of 
+important improvements from Freenet's "unstable" branch to stable.  
+The main improvement, apart from the bug fix, is "multiplexing".  this 
+means that rather than open multiple connections to the same node, 
+multiple streams of data can now be sent across the same inter-node 
+connection.  This will reduce Freenet's bandwidth, CPU, and memory usage 
+requirements.
