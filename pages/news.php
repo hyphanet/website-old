9b8b6df35113f1b26a6f0674bd7f71a23ef4064f
Ian Clarke <ian@freenetproject.org>
2004-02-08 11:36:15 +0000

    new stable release
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@5969 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -3,12 +3,17 @@ few sentences - create a separate page and link to that (ie. Slashdot
 style). --> 
 
 <h3>News</h3> 
-<p><b>13th January, 2004 - Stable build 5054 released</b></p>
-After discovering a major bug which we suspect to have been the cause of 
-Freenet's recent poor performance, we have back-ported a number of 
-important improvements from Freenet's "unstable" branch to stable.  
-The main improvement, apart from the bug fix, is "multiplexing".  this 
-means that rather than open multiple connections to the same node, 
-multiple streams of data can now be sent across the same inter-node 
-connection.  This will reduce Freenet's bandwidth, CPU, and memory usage 
-requirements.
+<p><b>8th February, 2004 - Stable build 5066 released</b></p>
+We have just migrated a number of recent improvements and bugfixes back
+to the stable branch.  The most significant of these are the introduction
+of a new load balancing mechanism which is the first real attempt at load
+balancing that has proved almost entirely successful.  In the unstable
+branch over 95% of queries were being rejected by nodes due to overload,
+but with the new load balancing mechanism this is now less than 5% and
+planned improvements promise to reduce this further.  The second most
+significant change is the fixing of a major bug in the datastore code
+which has gone undetected for over a year, and which was playing havoc
+with Freenet's caching of data.  All users should upgrade immediately
+by visiting our <a href="index.php?page=download">download</a> page,
+selecting "Update Snapshot" in your Windows Start menu, or running
+the <i>update.sh</i> script in your Linux freenet directory.
