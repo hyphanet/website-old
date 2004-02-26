0f0318ff9d19b5d10a950bb6c0d7cb7077a00f95
Ian Clarke <ian@freenetproject.org>
2004-02-26 12:39:30 +0000

    announce new build
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@6144 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -3,6 +3,25 @@ few sentences - create a separate page and link to that (ie. Slashdot
 style). --> 
 
 <h3>News</h3> 
+<p><b>26th February, 2004 - Freenet Stable build 5072 released</b></p>
+This new build incorporates the new "rate limiting" load balancing 
+mechanism discussed <a href="/index.php?page=stateof180204">here</a>.
+Rate limiting has proven
+surprisingly effective in the unstable branch, typically keeping
+a node's load within a few percentage points of where it is supposed
+to be.  This is in stark contrast to previous load balancing attempts
+where load was continuously so high as to render Freenet nodes almost
+completely useless.  
+<p>
+This build also fixes a number of bugs that were causing
+excessive memory usage.  We anticipate more such fixes in the future,
+but users should notice significant improvement already.
+<p>
+Existing users can upgrade to this snapshot.  Linux users should run
+the "update.sh" script in their freenet directory, Windows users
+should use the upgrade option in the Freenet section of their Start
+Menu.
+
 <p><b>18th February, 2004 - State of the Freenet</b></p>
 Freenet Project Coordinator, Ian Clarke, has written an update on the 
 current state of Freenet, giving details of recent challenges and 
