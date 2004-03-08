bcd5df1e78f746fa8751f4a6e35423fcf6dcb576
Ian Clarke <ian@freenetproject.org>
2004-03-08 10:24:03 +0000

    add news about DEF CON donation
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@6207 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -3,32 +3,27 @@ few sentences - create a separate page and link to that (ie. Slashdot
 style). --> 
 
 <h3>News</h3> 
+<p><b>8th March, 2004 - DEF CON donates $1,500 to the Freenet Project</b></p>
+<a href=http://www.defcon.org/">DEF CON</a> Communications, Inc has generously 
+donated $1,500 to the Freenet Project.  
+DEF CON is the world's largest annual <a href="http://en.wikipedia.org/wiki/Hacker">hacker</a> 
+convention, held every year in Las Vegas, Nevada.  
+In the words of DEF CON's Jeff Moss: "Freenet is doing interesting 
+work, and DEF CON likes to support privacy projects. DEF CON is not rich, 
+but Freenet is small enough that our donations hopefully make a difference".
 <p>
-<b>03/03/04 - Freenet Help Site available</b></p>
+These funds will be used to support the continued employment of Freenet's
+grossly underpaid full-time developer, Matthew Toseland.  The Freenet
+Project also gratefully acknowledges the contributions of other
+supporters of the project which have allowed us to raise over $5,000
+in the past few weeks thus providing for Matthew's continued employment
+for the next few months.
+<p>
+Freenet's project coordinator, Ian Clarke, will be speaking at the next
+DEF CON and will provide an update on the project's progress along
+with providing an opportunity for people to ask questions about the
+project.
+<p>
+<b>3rd March, 2004 - Freenet Help Site available</b></p>
 An unofficial, more elaborate <a href="http://www.freenethelp.org">Freenet Help Site</a> was created, to go deeper into the questions newbies may have about the usuage and  inner-workings of Freenet.
 <p>
-
-<b>26th February, 2004 - Freenet Stable build 5072 released</b></p>
-This new build incorporates the new "rate limiting" load balancing 
-mechanism discussed <a href="/index.php?page=stateof180204">here</a>.
-Rate limiting has proven
-surprisingly effective in the unstable branch, typically keeping
-a node's load within a few percentage points of where it is supposed
-to be.  This is in stark contrast to previous load balancing attempts
-where load was continuously so high as to render Freenet nodes almost
-completely useless.  
-<p>
-This build also fixes a number of bugs that were causing
-excessive memory usage.  We anticipate more such fixes in the future,
-but users should notice significant improvement already.
-<p>
-Existing users can upgrade to this snapshot.  Linux users should run
-the "update.sh" script in their freenet directory, Windows users
-should use the upgrade option in the Freenet section of their Start
-Menu.
-
-<p><b>18th February, 2004 - State of the Freenet</b></p>
-Freenet Project Coordinator, Ian Clarke, has written an update on the 
-current state of Freenet, giving details of recent challenges and 
-enhancements, and why the project continues to need your support.  You 
-can read it <a href="/index.php?page=stateof180204">here</a>.
--- pages/sponsors.php
+++ pages/sponsors.php
@@ -3,6 +3,8 @@
 The following persons, organisations and companies have generously
 sponsored this project through donations or discounts on hardware and services:
 <ol>
+<li><a href="http://www.defcon.org/">DEF CON</a><br>
+DEF CON is the world's largest annual hacker convention, held every year in Las Vegas, Nevada.
 <li><a href="http://www.verbumvanum.org/">Verbum Vanum</a><br>
 VerbumVanum is a site for free online storage and distribution of written
 knowledge and art; thesises, books, endworks, researchpapers, etc... 
