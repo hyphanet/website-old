8530ce3f3f03ceb7c9a7f4ac8f9a6ce43f709543
Matthew Toseland <toad@amphibian.dyndns.org>
2004-04-25 01:04:31 +0000

    announce 5077. Massive changes, so justifies a news item.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@6517 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -2,6 +2,24 @@
 few sentences - create a separate page and link to that (ie. Slashdot
 style). --> 
 <h3>News</h3>
+<p><b>25th April, 2004 - Stable build 5077, and immediate future</b></p>
+Please upgrade to stable build 5077. You can do this by using the update.sh
+utility on *nix, or freenet-webinstall.exe on Windows, or by downloading the
+jar from <a href="http://freenetproject.org/snapshots/freenet-latest.jar">here</a>.
+This build merges an enormous quantity of work from the unstable branch, including
+a fix for a very serious bug affecting inserts, optimizations, refactoring, many
+more bug fixes, a substantial reduction in memory usage with the fixing of many memory
+leaks, and a major change to how freenet nodes work together ("bidirectional routing").
+Like previous builds, there will probably be bugs in it, and we would as always 
+welcome bug reports at <a href="support@freenetproject.org">support@freenetproject.org</a>.
+We will not see the full effects of the changes until most of the stable branch 
+network have upgraded, so please upgrade ASAP. If you don't have a Freenet node
+installed, now is a great time to <a href="/index.php?page=download">download</a> one.
+We are trying to work towards getting a 0.6 prerelease out, but recent network tests
+suggest we may need to deal with one more major network level issue first (but 5077 
+should nonetheless be a significant improvement). We will need to do more debugging,
+ of course, and we may or may not implement automatic IP address detection for 
+firewalled nodes before 0.6.
 <p><b>10th April, 2004 - Demonstrate against Software Patents in Europe</b></p>
 A <a href="http://demo.ffii.org/online.php">demonstration</a> is taking place
 on 14th April in Brussels against the introduction of software patents in
