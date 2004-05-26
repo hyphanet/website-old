71e5e6dbe1c39a7da071d2eaef8ae80424636454
Jay Oliveri <ilnero@gmx.net>
2004-05-26 00:37:36 +0000

    tweaked the formatting to fit better on fp.org website
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@6654 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/FCPlib.php
+++ pages/FCPlib.php
@@ -1,68 +1,62 @@
- <h3>Freenet Client Protocol Library</h3>
+ <h1>Freenet Client Protocol Library</h1>
 
- <h4>What is the Freenet Client Protocol Library?</h4>
+ <h2>What is the Freenet Client Protocol Library?</h2>
  <p>FCPLib (Freenet Client Protocol Library) aims to be a platform independent, 
  but natively compiled set of functions for storing and retrieving information 
  to and from Freenet. There are routines for storing documents to Freenet from 
  the local disk, and other routines for moving data in memory to and from 
- Freenet (for more information see the documentation for <a href="http://freenetproject.org/index.php?page=fcptools">
- FCPTools</a>, which is the package containing FCPLib).</p>
- <p>It is written in ANSI C, checked by gecko when compiling the library on a Unix 
+ Freenet (for more information see the documentation
+ for <a href="http://freenetproject.org/index.php?page=fcptools">FCPTools</a>, which is the package containing FCPLib).</p>
+  
+  <p>It is written in ANSI C, checked by gcc when compiling the library on a Unix 
  based system (the flags -ansi and -pedantic). I'm not sure how important this 
  issue is today, but to enable the greatest portability I have kept to these 
  standards (the flags attempt to enforce strict ANSI compliance and the 
  disabling of GNU extensions).</p>
- <h5>Supported Platforms</h5>
-
+ <h3>Supported Platforms</h3>
  <p>FCPLib is now routinely compiled on the following platforms:</p>
  <ul>
    <li>Microsoft Windows (Win32 only). 
  
-   <li>Debian GNU/Linux. 
+   </li><li>Debian GNU/Linux. 
  
-   <li>BSD. 
+   </li><li>BSD. 
  
-   <li>Sun Solaris. 
+   </li><li>Sun Solaris. 
  
-   <li>Mac OS X.</li>
+   </li><li>Mac OS X.</li>
  </ul>
- <h5>Contact Information</h5>
-
- <p>All Freenet and FCPLib related email send to <SCRIPT language="javascript">
- eval(unescape('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%22%6d%61%69%6c%74%6f%3a%4a%61%79%20%4f%6c%69%76%65%72%69%20%3c%69%6c%6e%65%72%6f%40%67%6d%78%2e%6e%65%74%3e%22%3e%4a%61%79%20%4f%6c%69%76%65%72%69%3c%2f%61%3e%27%29%3b'))
- </SCRIPT>. I am currently subscribed to 
- Freenet-dev, Freenet-tech, and Freenet-support. On the Freenode IRC network I can be found in #freenet under the registered nick 
- &quot;Jay&quot;.</p>
+ <h3>Contact Information</h3>
+ <p>All Freenet and FCPLib related email send to <script language="javascript">
+ eval(unescape('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%22%6d%61%69%6c%74%6f%3a%4a%61%79%20%4f%6c%69%76%65%72%69%20%3c%69%6c%6e%65%72%6f%40%67%6d%78%2e%6e%65%74%3e%22%3e%4a%61%79%20%4f%6c%69%76%65%72%69%3c%2f%61%3e%27%29%3b'))</script>. I am currently
+ subscribed to Freenet-dev, Freenet-tech, and Freenet-support. On the Freenode IRC network I can be found in #freenet
+ under the registered nick "Jay".</p>
  
- <h4>FCPLib API Overview</h4>
- <h5>Necessary Files</h5>
+ <h2>FCPLib API Overview</h2>
+ <h3>Necessary Files</h3>
  <p>The necessary files depend on the platform:</p>
  <ul>
-
    <li>Windows: <code>ezFCPlib.lib, ezFCPlib.h</code></li>
    <li>UNIX: <code>ezFCPlib.a, ezFCPlib.h</code></li>
  </ul>
  <p>On both platforms the FCPLib compiles into a static library. You must 
  include this static module in your compiler's linking phase and put ezFCPlib.h 
  in your project's include path.</p>
- <h5>Library Startup and Terminate Functions</h5>
+ <h3>Library Startup and Terminate Functions</h3>
  <ul>
-
    <li><code>fcpStartup(FILE *Logfile, int Verbosity)</code>
-   <li><code>fcpTerminate()</code></li>
+   </li><li><code>fcpTerminate()</code></li>
  </ul>
  <p><code>fcpStartup()</code> allows a <code>FILE</code> pointer (which may be stdout or a locally opened 
  file) where debugging messages may be sent. This must be the first function 
  call to the FCPLib.</p>
  <p><code>fcpTerminate()</code> should be called before exiting.</p>
-
- <h5>Handle Create &amp; Destroy Functions.</h5>
+ <h3>Handle Create &amp; Destroy Functions.</h3>
  <ul>
    <li><code>fcpCreateHFCP(char *Host, int Port, int Htl, int Optmask)</code>
-   <li><code>fcpInheritHFCP(hFCP *Handle)</code>
-   <li><code>fcpDestroyHFCP(hFCP *Handle)</code></li>
+   </li><li><code>fcpInheritHFCP(hFCP *Handle)</code>
+   </li><li><code>fcpDestroyHFCP(hFCP *Handle)</code></li>
  </ul>
-
  <p>These  functions are to be called immediately after <code>fcpStartup()</code>, before 
  any calls to the following functions: <code>fcpOpenKey()</code>, <code>fcpPutKeyFromFile()</code>,
  <code>fcpGetKeyToFile()</code>.</p>
@@ -70,65 +64,57 @@
  to prepare an FCP Handle with the same options as another one.</p>
  <p><code>fcpDestroyHFCP()</code> performs memory and file cleanup, and should 
  be called before calling <code>free()</code> on the Handle.</p>
-
- <h5>File Based Functions</h5>
+ <h3>File Based Functions</h3>
  <p>There are 2 functions for moving data between files and Freenet:</p>
  <ul>
    <li><code>fcpPutKeyFromFile(hFCP *Handle, char *Key, char *KeyFile, char 
    *MetaFile)</code>
-   <li><code>fcpGetKeyToFile(hFCP *Handle, char *Key, char *KeyFile, char 
+   </li><li><code>fcpGetKeyToFile(hFCP *Handle, char *Key, char *KeyFile, char 
    *MetaFile)</code></li>
 </ul>
 <p><code>fcpPutKeyFromFile() </code>supports files larger than 1M and inserts 
 them as splitfiles.</p>
-
 <p><code>fcpGetGeyToFile()</code> only supports single file chunks, which usually vary in 
 Freenet from 200-1,000 kilobytes.</p>
- <h5>Memory Based Functions</h5>
- <h6>Opening a Key</h6>
+ <h3>Memory Based Functions</h3>
+ <h4>Opening a Key</h4>
  <p><code>fcpOpenKey(hFCP *Handle, char *Key, int Mode)</code></p>
  <p>This function opens a key in order to read from or write to, depending on 
  the Mode parameter. Opening a key in Read Mode immediately prompts FCPLib to 
  fetch the key and metadata, so that the next call to <code>fcpReadKey()</code> 
  has data to return.</p>
-
- <h6>Reading &amp; Writing</h6>
+ <h4>Reading &amp; Writing</h4>
  <p><code>fcpWriteKey(hFCP *Handle, char *Buffer, int Length)<br>
  fcpWriteMetadta(hFCP *Handle, char *Buffer, int Length)</code></p>
  <p>Once a key is opened for writing, this function writes a block of data to a 
  temporary file that FCPLib later inserts into Freenet.</p>
  <p><code>fcpReadKey(hFCP *Handle, char *Buffer, int Length)<br>
-
  fcpReadMetadata(hFCP *Handle, char *Buffer, int Length)</code></p>
  <p>Once a key is opened for reading, this function reads from the temporary 
  file that <code>fcpOpenKey()</code> retrieved the key into.</p>
- <h6>Closing a Key</h6>
+ <h4>Closing a Key</h4>
  <p><code>fcpCloseKey(hFCP *Handle)</code></p>
  <p>This function closes a key. When closing a key that has been opened in Write 
  Mode, FCPLib inserts into Freenet the key and metadata stored in the temporary 
  files. </p>
-
- <h4>FCPLib in Detail</h4>
- <h5>Startup &amp; Terminate Functions</h5>
- <h6>fcpStartup</h6>
+ <h2>FCPLib in Detail</h2>
+ <h3>Startup &amp; Terminate Functions</h3>
+ <h4>fcpStartup</h4>
  <p><code>int fcpStartup(FILE *Logfile, int Verbosity)</code></p>
 <p><b>Parameters: </b>Logfile is a pointer to an opened <code>FILE</code> 
-
 structure, or <code>NULL</code> to send logging information to <code>stdout</code>.</p>
 <p><b>Returns:</b> Zero on success, -1 on error.</p>
 <p><b>Remarks</b></p>
 <p>This must be the first function called out of all the FCPLib functions. On 
 Windows it initializes Winsock, and then for all platforms initializes the log.</p>
-<h6>fcpTerminate</h6>
-
+<h4>fcpTerminate</h4>
  <p><code>void fcpTerminate()</code></p>
 <p><b>Remarks</b></p>
 <p>This must be the last function to be called to FCPLib, especially before 
 exiting a program. It closes the log, and on Windows calls the Winsock <code>
 WSACleanup()</code> function.</p>
- <h5>Create and Destroy Functions</h5>
- <h6>fcpCreateHFCP</h6>
-
+ <h3>Create and Destroy Functions</h3>
+ <h4>fcpCreateHFCP</h4>
  <p><code>hFCP *fcpCreateHFCP(char *Host, int Port, int Htl, int Optmask)</code></p>
 <p><b>Parameters:</b> Host is a hostname or IP address of a Freenet node, 
 Port is the node's listen port, Htl the hops to live for the FCP handle, and 
@@ -138,82 +124,73 @@ Optmask a series of possible flags.</p>
    <li><code>FCP_MODE_RAW</code>: 
         Do not follow redirects on retrieve. 
  
-   <li><code>FCP_MODE_DELETE_LOCAL</code>: Delete the key from 
+   </li><li><code>FCP_MODE_DELETE_LOCAL</code>: Delete the key from 
         the local datastore on insert. 
  
-   <li><code>FCP_MODE_SKIP_LOCAL</code>: Skip the local datastore on retrieve.</li>
-
+   </li><li><code>FCP_MODE_SKIP_LOCAL</code>: Skip the local datastore on retrieve.</li>
  </ul>
 <p><b>Returns:</b> A pointer to a malloc'ed <code>hFCP</code> structure.</p>
 <p><b>Remarks</b></p>
 <p>This function creates a new FCP Handle that must be free'd after use. Before 
 freeing however, call <code>fcpDestroyHFCP()</code>.</p>
- <h6>fcpInheritHFCP</h6>
-
+ <h4>fcpInheritHFCP</h4>
  <p><code>hFCP *fcpInheritHFCP(hFCP *Handle)</code></p>
 <p><b>Parameters:</b> Handle is an already created hFCP structure.</p>
 <p><b>Returns: </b>A pointer to a malloc'ed <code>hFCP</code> structure.</p>
 <p><b>Remarks</b></p>
 <p>This functions basically creates a new FCP Handle and fills it with values 
 from the Handle parameter. Before freeing however, call <code>fcpDestroyHFCP()</code>.</p>
-
- <h6>fcpDestroyHFCP</h6>
+ <h4>fcpDestroyHFCP</h4>
  <p><code>void fcpDestroyHFCP(hFCP *Handle)</code></p>
 <p><b>Parameters:</b> Handle is an already created hFCP structure.</p>
 <p><b>Remarks</b></p>
 <p>This function disconnects the socket (if necessary), and destroys everything 
 that's been malloc'ed and not yet free'd within the <code>hFCP</code> structure. 
 It also deletes any temporary files still being held on disk.</p>
-
- <h5>File Based Functions</h5>
- <h6>fcpPutKeyFromFile</h6>
+ <h3>File Based Functions</h3>
+ <h4>fcpPutKeyFromFile</h4>
  <p><code>int fcpPutKeyFromFile(hFCP *Handle, char *Key, char 
  *KeyFile, char *MetaFile)</code></p>
  <p><b>Parameters:</b> Handle is an already created hFCP structure, Key a Freenet URI, 
  KeyFile the local filename to insert into Freenet, and MetaFile is the 
  local filename to insert as metadata.</p>
  <p><b>Returns:</b> Zero on success, -1 on error.</p>
-
  <p><b>Remarks</b></p>
  <p>Handle must already be created from a prior call to any of the fcpCreate 
  functions. If the file is larger than <code>EZFCP_DEFAULT_SPLITSIZE</code>, the 
  file is inserted into Freenet as an FEC encoded splitfile.</p>
- <h6>fcpGetKeyToFile</h6>
+ <h4>fcpGetKeyToFile</h4>
  <p><code>int fcpGetKeyToFile(hFCP *Handle, char *Key, char *KeyFile, char 
  *MetaFile)</code></p>
 <p><b>Parameters:</b> Handle is an already created hFCP structure, Key a 
 Freenet URI, KeyFile the local filename to write the Freenet document, and 
 MetaFile is the local filename to write the document's metadata.</p>
-
 <p><b>Returns:</b> Zero on success, -1 on error.</p>
 <p><b>Remarks</b></p>
 <p>Handle must already be created from a prior call to any of the fcpCreate 
 functions. The retrieving of splitfiles is currently not supported, which means 
 this function assumes every URI parameter it receives is not FEC Encoded (in 
 practical terms this means only the mapfile is retrieved).</p>
- <h5>Memory Based Functions</h5>
- <h6>fcpOpenKey</h6>
+ <h3>Memory Based Functions</h3>
+ <h4>fcpOpenKey</h4>
  <p><code>int fcpOpenKey(hFCP *Handle, char *Key, int Mode)</code></p>
-
 <p><b>Parameters:</b> Handle is an already created hFCP structure, Key a 
 Freenet URI, and Mode indicates either read or write.</p>
 <p><b>MODE VALUES:</b></p>
  <ul>
    <li><code>FCP_MODE_O_READ.</code>
-   <li><code>FCP_MODE_O_WRITE.</code></li>
+   </li><li><code>FCP_MODE_O_WRITE.</code></li>
  </ul>
 <p><b>Returns: Zero on success, -1 on error.</b></p>
-
 <p><b>Remarks</b></p>
 <p>When a key is opened with Mode=FCP_MODE_O_READ, 
       the key specified in Key is immediately retrieved and stored internally 
       by FCPLib. Subsequent calls to <code>fcpReadKey()</code> return data from the retrieved 
       temporary file.</p>
-      <P>When a key is opened with Mode=FCP_MODE_O_WRITE, calls to 
+      <p>When a key is opened with Mode=FCP_MODE_O_WRITE, calls to 
       <code>fcpWriteKey()</code> write the key data to an internal temporary file. The key is 
-      written after a call to <code>fcpCloseKey()</code>.</P>
- <h6>fcpReadKey</h6>
-
+      written after a call to <code>fcpCloseKey()</code>.</p>
+ <h4>fcpReadKey</h4>
  <p><code>int fcpReadKey(hFCP *Handle, char *Buffer, int Length)<br>
  int fcpReadMetadata(hFCP *Handle, char *Buffer, int Length)</code></p>
 <p><b>Parameters:</b> Handle 
@@ -222,11 +199,10 @@ Freenet URI, and Mode indicates either read or write.</p>
 <p><b>Returns:</b> Bytes read 
       into Buffer, -1 on error.</p>
 <p><b>Remarks</b></p>
-      <P>Function reads from the internal temporary file 
+      <p>Function reads from the internal temporary file 
       (which contains the data from the call to <code>fcpOpenKey()</code>) and returns no 
-      more than Length bytes.</P>
-
- <h6>fcpWriteKey</h6>
+      more than Length bytes.</p>
+ <h4>fcpWriteKey</h4>
  <p><code>int fcpWriteKey(hFCP *Handle, char *Buffer, int Length)<br>
  int fcpWriteMetadata(hFCP *Handle, char *Buffer, int Length)</code></p>
 <p><b>Parameters:</b> Handle 
@@ -234,26 +210,23 @@ Freenet URI, and Mode indicates either read or write.</p>
       block of memory. The function will write at most Length bytes.</p>
 <p><b>Returns:</b> Bytes read 
       into Buffer, -1 on error.</p>
-
 <p><b>Remarks</b></p>
-      <P>Function writes to the internal temporary file no more than Length 
+      <p>Function writes to the internal temporary file no more than Length 
       bytes. Nothing is inserted into Freenet until the call to 
-      <code>fcpCloseKey()</code>.</P>
- <h6>fcpCloseKey</h6>
+      <code>fcpCloseKey()</code>.</p>
+ <h4>fcpCloseKey</h4>
  <p><code>int fcpCloseKey(hFCP *Handle)</code></p>
 <p><b>Parameters:</b> Handle is an already created hFCP structure.</p>
-
 <p><b>Returns:</b> Zero on 
       success, -1 on error.</p>
 <p><b>Remarks</b>  </p>
-      <P>When a key is opened in Write mode, this function will attempt to store 
+      <p>When a key is opened in Write mode, this function will attempt to store 
       the key into Freenet. The functions returns after the key is stored or if 
-      an error is encountered.</P>
-      <P>When a key is opened in Read mode, <code>fcpCloseKey()</code> simply cleans up 
-      temporary files and returns to the caller.</P>
- <h5>Other</h5>
-
- <h6>fcpMakeSvkKeypair</h6>
+      an error is encountered.</p>
+      <p>When a key is opened in Read mode, <code>fcpCloseKey()</code> simply cleans up 
+      temporary files and returns to the caller.</p>
+ <h3>Other</h3>
+ <h4>fcpMakeSvkKeypair</h4>
  <p><code>int fcpMakeSvkKeypair(hFCP *Handle, char *PubKey, char *PrivKey, 
  char *Entropy)</code></p>
 <p><b>Parameters:</b> Handle 
@@ -262,17 +235,15 @@ Freenet URI, and Mode indicates either read or write.</p>
       allocated block of memory to hold the private key. Entropy is not currently used.</p>
 <p><b>Returns:</b> Zero on 
       success, -1 on error.</p>
-
 <p><b>Remarks</b>  </p>
 <p>PubKey and PrivKey must be allocated with 
       at least&nbsp;28 bytes (27 for the key, 1 for the NULL character). 
       Entropy should be set to <code>NULL</code> for now, until it is implemented.</p>
 <p><code>/********************************************************************/</code></p>
- <h4>Examples</h4>
+ <h2>Examples</h2>
  <p>For the sake of completion, each of the 4 different methods for moving data 
  to and from Freenet is illustrated with the minimal C code required.</p>
-
- <h5>Storing Files To Freenet with fcpPutKeyFromFile</h5>
+ <h3>Storing Files To Freenet with fcpPutKeyFromFile</h3>
  
 <pre><code>#include "ezFCPlib.h"
 
@@ -305,7 +276,7 @@ int main(int argc, char* argv[])
   return 0;
 }</code></pre>
 
-<h5>Storing Files To Freenet with fcpWriteKey</h5>
+<h3>Storing Files To Freenet with fcpWriteKey</h3>
 
 <pre><code>#include "ezFCPlib.h"
 
@@ -347,7 +318,7 @@ int main(int argc, char* argv[])
   return 0;
 }</code></pre>
 
- <h5>Retrieving Files From Freenet with fcpGetKeyToFile</h5>
+ <h3>Retrieving Files From Freenet with fcpGetKeyToFile</h3>
 
 <pre><code>#include "ezFCPlib.h"
 
@@ -380,7 +351,7 @@ int main(int argc, char* argv[])
   return 0;
 }</code></pre>
 
-<h5>Retrieving Files From Freenet with fcpReadKey</h5>
+<h3>Retrieving Files From Freenet with fcpReadKey</h3>
 
 <pre><code>#include "ezFCPlib.h"
 
@@ -406,8 +377,8 @@ int main(int argc, char* argv[])
                  FCP_MODE_O_READ) != 0)
     return -1;
     
-  /* ReadKey returns >0 whenever there's data left */
-  while ((bytes = fcpReadKey(hfcp, buf,  512)) > 0) {
+  /* ReadKey returns &gt;0 whenever there's data left */
+  while ((bytes = fcpReadKey(hfcp, buf,  512)) &gt; 0) {
     buf[bytes] = 0;
 
     /* hope this is text only! */
@@ -427,3 +398,5 @@ int main(int argc, char* argv[])
 
   return 0;
 }</code></pre>
+
+<h1>*</h1>
--- pages/fcptools.php
+++ pages/fcptools.php
@@ -1,4 +1,4 @@
- <h3>Freenet Client Protocol Tools (FCPTools)</h3>
+ <h1>Freenet Client Protocol Tools (FCPTools)</h1>
 
  <p>First check the documentation for <a href="http://freenetproject.org/index.php?page=FCPlib">FCPLib</a>. Everything 
  that applies to FCPLib also applies to FCPTools. To repeat a few things: </p>
@@ -6,13 +6,14 @@
  Admittedly there are missing features here and there, and these will get most 
  of my attention in the near future. For example:</p>
  <ul>
+
    <li>Metadata support is spotty, so not yet 
         supported. Further, where metadata is actually stored needs to change 
         (metadata stored in CHKs). 
  
-   </li><li>FEC inserts supported, but not FEC retrieves. 
+   <li>FEC inserts supported, but not FEC retrieves. 
  
-   </li><li>Multithreading not supported, nor even desirable for the library itself. 
+   <li>Multithreading not supported, nor even desirable for the library itself. 
    Any scheduler should be built on <i>top</i> of FCPLib.</li>
 </ul>
  <p>Some things have been improved:</p>
@@ -20,6 +21,7 @@
    <li>Thoroughly checked and fixed most, if not all memory leaks. This should 
    allow FCPLib to loaded into RAM for extended periods of time without 
    problems.</li>
+
    <li>Insert and retrieve logic is not redundant; it is shared. For example, 
    the same code that inserts data into Freenet for splitfiles is used later to 
    insert metadata, meaning each operation works equally well within the chain 
@@ -31,7 +33,8 @@
    <li>The functions for inserting and retrieving single files is identical for 
    splitfiles.</li>
  </ul>
- <h4>What are the FCPTools?</h4>
+ <h2>What are the FCPTools?</h2>
+
  <p>The FCPTools are command-line driven programs for inserting and retrieving 
  files with Freenet. Included separately is <a href="http://freenetproject.org/index.php?page=FCPlib">FCPLib</a>, the 
  Freenet Client Protocol Library. The FCPTools are linked against FCPLib and 
@@ -44,25 +47,27 @@
    <li><b>fcpget</b>: Retrieves single files (no splitfiles) from Freenet (you 
    can of course retrieve the key in rawmode and manually follow the Blockmap 
    data).</li>
+
    <li><b>fcpx</b>: A general informational utility.</li>
  </ul>
  <p>There is currently unmaintained code for fcpputsite which needs to be 
  rewritten to use FCPLib exclusively. I believe that a shell script written 
  around fcpput could work nicely as a short term substitute.</p>
- <h5>Status of Development</h5>
+ <h3>Status of Development</h3>
  <p>The FCPTools (and FCPLib) are works in progress, but beginning to take solid form in 
  preparation for a 0.9 release, which will not include a command-line fcpputsite.</p>
  <p>The version in CVS is 0.4.9 and works well for what it claims to support. 
  Admittedly there are missing features here and there, and these will get most 
  of my attention in the near future. For example:</p>
+
  <ul>
    <li>Metadata support is spotty, so not yet 
         supported. Further, where metadata is actually stored needs to change 
         (metadata stored in CHKs). 
  
-   </li><li>FEC inserts supported, but not FEC retrieves. 
+   <li>FEC inserts supported, but not FEC retrieves. 
  
-   </li><li>Multithreading not supported, nor even desirable for the library itself. 
+   <li>Multithreading not supported, nor even desirable for the library itself. 
    Any scheduler should be built on <i>top</i> of FCPLib.</li>
 </ul>
  <p>Some things have been improved:</p>
@@ -70,6 +75,7 @@
    <li>Thoroughly checked and fixed most, if not all memory leaks. This should 
    allow FCPLib to loaded into RAM for extended periods of time without 
    problems.</li>
+
    <li>Insert and retrieve logic is not redundant; it is shared. For example, 
    the same code that inserts data into Freenet for splitfiles is used later to 
    insert metadata, meaning each operation works equally well within the chain 
@@ -81,19 +87,18 @@
    <li>The functions for inserting and retrieving single files is identical for 
    splitfiles.</li>
  </ul>
- <h5>Contact Information</h5>
- <p>All Freenet and FCPLib related email send to <script language="javascript">
- eval(unescape('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%22%6d%61%69%6c%74%6f%3a%4a%61%79%20%4f%6c%69%76%65%72%69%20%3c%69%6c%6e%65%72%6f%40%67%6d%78%2e%6e%65%74%3e%22%3e%4a%61%79%20%4f%6c%69%76%65%72%69%3c%2f%61%3e%27%29%3b'))
- </script><a href="mailto:Jay%20Oliveri%20%3Cilnero@gmx.net%3E">Jay Oliveri</a>.
-I am currently subscribed to Freenet-dev, Freenet-tech, and
-Freenet-support. On the Freenode IRC network I can be found in #freenet
-under the registered nick "Jay".</p>
+ <h3>Contact Information</h3>
+
+ <p>All Freenet and FCPLib related email send to <SCRIPT language="javascript">eval(unescape('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%22%6d%61%69%6c%74%6f%3a%4a%61%79%20%4f%6c%69%76%65%72%69%20%3c%69%6c%6e%65%72%6f%40%67%6d%78%2e%6e%65%74%3e%22%3e%4a%61%79%20%4f%6c%69%76%65%72%69%3c%2f%61%3e%27%29%3b'))</SCRIPT>. I am currently subscribed to 
+ Freenet-dev, Freenet-tech, and Freenet-support. On the Freenode IRC network I can be found in #freenet under the registered nick 
+ &quot;Jay&quot;.</p>
  
- <h4>Source Code</h4>
+ <h2>Source Code</h2>
  <p>There are two options for obtaining the source:</p>
- <h5>CVS</h5>
+ <h3>CVS</h3>
  <p>To be on the 
  bleeding edge, access the source from CVS as follows (all on one line):</p>
+
  <p><code>cvs -z3 _<br>
 &nbsp;&nbsp;&nbsp; -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/freenet _<br>
 &nbsp;&nbsp;&nbsp;&nbsp; co Contrib/fcptools</code></p>
@@ -103,7 +108,8 @@ under the registered nick "Jay".</p>
  details).</p>
  <p><a href="http://cvs.sourceforge.net/viewcvs.py/freenet/Contrib/fcptools/">
  Follow this link</a> to browse the  code at SourceForge using ViewCVS.</p>
- <h5>Tarballs</h5>
+
+ <h3>Tarballs</h3>
  <p>Occasionally when I feel a quasi-stable point is reached in the project's 
  cycle, I post a tarball to the <a href="http://freenetproject.org/snapshots/">
  freenet snapshots directory</a>. The filename itself will be 
@@ -111,6 +117,7 @@ under the registered nick "Jay".</p>
  <ul>
    <li>fcptools-yyyymmdd.tgz (source code)</li>
    <li>fcptools-yyyymmdd-bin.tgz (Win32 pre-compiled binaries)</li>
+
  </ul>
  <p>You can use these links as well:</p>
  <ul>
@@ -118,20 +125,21 @@ under the registered nick "Jay".</p>
    fcptools-unstable.tgz</a> (latest source code)</li>
    <li><a href="http://freenetproject.org/snapshots/fcptools-unstable-bin.tgz">
    fcptools-unstable-bin.tgz</a> (latest Win32 pre-compiled binaries)</li>
+
  </ul>
  <p>I recommend this method over the others, since CVS at times may not even 
  compile. The tarballs are fully tested before uploaded (SourceForge 
  compile-farm among other tests) and should function as advertised. The binaries 
  will also contain (in addition to fcpget, fcpput, and fcpx) the FCPLib static 
  library and C header file.</p>
- <h4>Using FCPTools</h4>
+ <h2>Using FCPTools</h2>
  <p>Some common parameters are used by both fcpget and fcpput, but for the sake 
  of completeness everything is listed for all 3 programs.</p>
- <h5>FCP-Put Usage:</h5>
+ <h3>FCP-Put Usage:</h3>
 
 <pre>fcpput [-n hostname] [-p port] [-l hops to live] [-s]  [-D]
        [-v verbosity] [-g] [-V] [-h] freenet_uri [FILE]...
-       
+
 Options:
 
   -n, --address host     Freenet node address
@@ -143,8 +151,10 @@ Options:
   -D, --delete-local     Delete key from local datastore on insert
 
   -v, --verbosity num    Verbosity of log messages (default 2)
-                         0=silent, 1=critical, 2=normal, 3=verbose, 4=debug
-  -f, --logfile file     Full pathname for the output log file (default stdout)
+                         0=silent, 1=critical, 2=normal,
+                         3=verbose, 4=debug
+  -f, --logfile file     Full pathname for the output log file
+                         (default stdout)
 
   -g, --genkeys          Generate a keypair then exit
 
@@ -154,6 +164,7 @@ Options:
   uri                    URI to give newly inserted key; variations:
                            CHK@
                            KSK@&lt;routing key&gt;
+
                            SSK@&lt;private key&gt;[/&lt;docname&gt;]
 
   file                   Read key data from local file
@@ -166,7 +177,7 @@ To insert a Content Hash Key (CHK) with file "gpl.txt":
 
 To insert a Keyword Signed Key (KSK) with file "gpl.txt" against a
 freenet node at address raven.cp.net with hops to live 10:
-  fcpput --htl 10 --address raven.cp.net KSK@gpl.txt /home/hapi/gpl.txt
+  fcpput --htl 10 --address raven.cp.net KSK@gpl.txt gpl.txt
 
 To insert a Subspace Signed Key (SSK) with file "gpl.txt":
   fcpput SSK@LNlEaG7L24af-OH~CKmyPOvJ~EM/ gpl.txt
@@ -174,7 +185,7 @@ To insert a Subspace Signed Key (SSK) with file "gpl.txt":
 To insert an SSK within named document "licenses":
   fcpput SSK@LNlEaG7L24af-OH~CKmyPOvJ~EM/licenses gpl.txt</pre>
 
-<h5>FCP-GET Usage:</h5>
+<h3>FCP-GET Usage:</h3>
 
 <pre>fcpget [-n hostname] [-p port] [-l hops to live] [-s] [-S]
        [-v verbosity] [-V] [-h] freenet_uri [FILE]
@@ -189,8 +200,10 @@ Options:
   -s, --stdout           Write key data to stdout
   -S, --skip-local       Skip key in local datastore on retrieve
   -v, --verbosity num    Verbosity of log messages (default 2)
-                         0=silent, 1=critical, 2=normal, 3=verbose, 4=debug
-  -f, --logfile file     Full pathname for the output log file (default stdout)
+                         0=silent, 1=critical, 2=normal,
+                         3=verbose, 4=debug
+  -f, --logfile file     Full pathname for the output log file
+                         (default stdout)
 
   -V, --version          Output version information and exit
   -h, --help             Display this help and exit
@@ -203,7 +216,7 @@ Options:
   file                   Write key data to local file
                          (cannot be used with --stdout)</pre>
 
- <h5>FCP-X Usage:</h5>
+ <h3>FCP-X Usage:</h3>
 
 <pre>fcpx [-n hostname] [-p port] [-v verbosity] [-f filename] COMMAND
 
@@ -218,5 +231,9 @@ Options:
   -p, --port num         Freenet node port
 
   -v, --verbosity num    Verbosity of log messages (default 2)
-                         0=silent, 1=critical, 2=normal, 3=verbose, 4=debug
-  -f, --logfile file     Full pathname for the output log file (default stdout)</pre>
+                         0=silent, 1=critical, 2=normal,
+                         3=verbose, 4=debug
+  -f, --logfile file     Full pathname for the output log file
+                         (default stdout)</pre>
+
+<p><h1>*</h1></p>
