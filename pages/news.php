2dc03d0f130160795956486fe3367a610da04d29
Matthew Toseland <toad@amphibian.dyndns.org>
2004-08-06 19:17:43 +0000

    formatting
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@6872 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -2,8 +2,8 @@
 few sentences - create a separate page and link to that (ie. Slashdot
 style). --> 
 <h3>News</h3>
-<b>6th August, 2004 : Stable build 5090, Stable network reset</b><br>
-Stable build 5090 has now been released, all users running the stable
+<b>6th August, 2004 : Stable build 5090, Stable network reset</b>
+<p>Stable build 5090 has now been released, all users running the stable
 branch should upgrade as this build contains numerous important bugfixes,
 including a fix for a 4-month-old bug that was causing lots more Route
 Not Found errors than there should be. Also, the stable network has been
@@ -16,16 +16,18 @@ the announcement</a>, or for the real Freenet geeks out there, the low
 level changelog is on the 
 <a href="http://news.gmane.org/gmane.network.freenet.cvs">CVS list archive</a>.
 If you have any problems upgrading don't hesitate to contact us on #freenet on
-irc.freenode.net, or by email at <a href="support@freenetproject.org">support@freenetproject.org</a>.
+irc.freenode.net, or by email at <a href="support@freenetproject.org">support@freenetproject.org</a>.</p>
 <b>23rd July, 2004 : Stable build 5085</b><br>
+<p>
 Stable build 5085 has now been released, all users running the stable
 branch should upgrade as this build contains important bugfixes and
 many fundamental improvements.  Instructions for installing or upgrading
 are available <a href="/index.php?page=download">
 here</a>.  A more detailed list of changes may be found 
 <a href="/index.php?page=whatsnew">here</a>.
-<p>
+</p>
 <b>12th July, 2004 : Status Update</b><br>
+<p>
 Things have been progressing rapidly in the unstable branch over the
 past few weeks.  Along with the usual flow of bug fixes, Matthew
 has made some interesting discoveries by setting up a local "testnet",
@@ -34,6 +36,6 @@ and is in the process of integrating these lessons into the
 We have continued to experience problems with excessive "route not
 found" errors, particularly when a node has just started up, and
 are working on a variety of solutions to this problem.
-<p>
+</p>
 If you are feeling adventurous, <a href="?page=tryunstable">this page</a>
 explains how you can try out the unstable branch of development.
