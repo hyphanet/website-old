2e747998e354643ec1a94781e6b4753ae59b234c
Ian Clarke <ian@freenetproject.org>
2004-08-08 15:15:48 +0000

    clean up news
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@6883 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -1,6 +1,7 @@
-<!-- Please keep this to at-most 5 news items, and if it is more than a
+<!-- Please keep this to at-most 3 news items, and if it is more than a
 few sentences - create a separate page and link to that (ie. Slashdot
-style). --> 
+style).  Additionally, please remove obsolete news items (such as
+old new build announcements) --> 
 <h3>News</h3>
 <b>6th August, 2004 : Stable build 5090, Stable network reset</b>
 <p>Stable build 5090 has now been released, all users running the stable
@@ -18,25 +19,3 @@ level changelog is on the
 If you have any problems upgrading don't hesitate to contact us on IRC on 
 #freenet on irc.freenode.net, or by email at 
 <a href="support@freenetproject.org">support@freenetproject.org</a>.</p>
-<b>23rd July, 2004 : Stable build 5085</b><br>
-<p>
-Stable build 5085 has now been released, all users running the stable
-branch should upgrade as this build contains important bugfixes and
-many fundamental improvements.  Instructions for installing or upgrading
-are available <a href="/index.php?page=download">
-here</a>.  A more detailed list of changes may be found 
-<a href="/index.php?page=whatsnew">here</a>.
-</p>
-<b>12th July, 2004 : Status Update</b><br>
-<p>
-Things have been progressing rapidly in the unstable branch over the
-past few weeks.  Along with the usual flow of bug fixes, Matthew
-has made some interesting discoveries by setting up a local "testnet",
-and is in the process of integrating these lessons into the 
-<a href="/index.php?page=ngrouting">next-gen routing</a> algorithm.
-We have continued to experience problems with excessive "route not
-found" errors, particularly when a node has just started up, and
-are working on a variety of solutions to this problem.
-</p>
-If you are feeling adventurous, <a href="?page=tryunstable">this page</a>
-explains how you can try out the unstable branch of development.
