ca7df7676e437f51d67bc29f391c869fa2258d90
Matthew Toseland <toad@amphibian.dyndns.org>
2004-12-20 18:35:26 +0000

    slight improvement to demo
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@7124 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -13,9 +13,9 @@ our <a href="/index.php?page=donate">donations page</a>.
 
 <h3>News</h3>
 <b>20th November, 2004 : Software patents protest</b>
-<p>Click the logo for details.</p>
-<p><a href="http://demo.ffii.org/letter.html">
-<img src="http://demo.ffii.org/minister_banner90.png" width="728" height="90" border="0" alt="Dear Minister, please click here!"></a></p>
+<p>The Council is trying to push through the software patents directive without discussion at a fisheries meeting despite the opposition of national parliaments, and there being no qualified majority. Click the logo for details.</p>
+<a href="http://demo.ffii.org/letter.html">
+<img src="http://demo.ffii.org/minister_banner90.png" width="728" height="90" border="0" alt="Dear Minister, please click here!"></a><
 <b>8th November, 2004 : Unofficial Freenet RPMs now available</b>
 <p>Freenet developer Mika Hirvonen has put together some RPMs of the 
 Freenet stable and unstable builds which Fedora users can install and 
