bbd7689756f6a948762ce382e3e6262a5dd14ffd
Ian Clarke <ian@freenetproject.org>
2005-10-06 10:43:59 +0000

    Fix mailing list links
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@7417 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/lists.php
+++ pages/lists.php
@@ -13,13 +13,13 @@ information and tips for getting the most from Freenet.
 <i><b>Note</b>: We constantly get requests of people who want to become unsubscribed.  </i><span style='text-decoration:underline;'>YOU</span><i> have to do that yourself! Just click on the www link provided at the bottom of every mail, enter your e-mail address in the text field in section "Subscribers" and provide your password under the unsubscribe option.  (You can get your password there as well, in case you forgot it.)</i>
 <br />
 <br /><a class='wiki'  
-href='http://mail.freenetproject.org/mailman/listinfo/announce/'>Announcements</a> (<a 
+href='http://mail.freenetproject.org/cgi-bin/mailman/listinfo/announce/'>Announcements</a> (<a 
 class='wiki'  href='http://news.gmane.org/gmane.network.freenet.announce'>archive</a>)
 <br />Please sign up here to be notified of major Freenet developments, such as announcements of new releases or important bugfixes.  This list is moderated and is very low traffic (less than one message per month).
 <br />
 
 <br /><a
-href='http://mail.freenetproject.org/mailman/listinfo/support/'>Support</a>
+href='http://mail.freenetproject.org/cgi-bin/mailman/listinfo/support/'>Support</a>
 (<a
 href='http://news.gmane.org/gmane.network.freenet.support'>archive</a>)
 <br />Asking for or giving advice relating to getting Freenet working, 
@@ -28,10 +28,10 @@ for help are more likely to be heard here than in the other mailing
 lists.  <b>Note: </b> Emails sent to this mailing list from those not 
 subscribed to the list must be manually approved, and this can impose 
 a delay of several days.  To avoid this delay, please <a 
-href="http://mail.freenetproject.org/mailman/listinfo/support/">subscribe</a>.
+href="http://mail.freenetproject.org/cgi-bin/mailman/listinfo/support/">subscribe</a>.
 <br />
 <br /><a
-href='http://mail.freenetproject.org/mailman/listinfo/tech/'>Tech</a>
+href='http://mail.freenetproject.org/cgi-bin/mailman/listinfo/tech/'>Tech</a>
 (<a
 href='http://news.gmane.org/gmane.network.freenet.technical'>archive</a>)
 <br />
@@ -39,19 +39,19 @@ This is for general technical discussion of Freenet.  If you want to discuss
 longer-term technical ideas about Freenet, please use this list.
 <br />
 <br /><a class='wiki'  
-href='http://mail.freenetproject.org/mailman/listinfo/devl/'>Development</a> (<a 
+href='http://mail.freenetproject.org/cgi-bin/mailman/listinfo/devl/'>Development</a> (<a 
 class='wiki'  href='http://news.gmane.org/gmane.network.freenet.devel'>archive</a>)
 <br />
 This list is for active developers to discuss bugs, and the implementation 
 of near-term new features.  Please only post to this list if you know what 
 you are talking about, otherwise use the tech mailing list (see above).
 <br />
-<br /><a class='wiki'  href='http://mail.freenetproject.org/mailman/listinfo/cvs'>CVS</a>  
+<br /><a class='wiki'  href='http://mail.freenetproject.org/cgi-bin/mailman/listinfo/cvs'>CVS</a>  
 (<a class='wiki'  href='http://news.gmane.org/gmane.network.freenet.cvs'>archive</a>)
 <br />A read-only mailing list to which CVS checkins are mailed.  This will probably only be of interest to developers.
 <br />
 <br /><a class='wiki'  
-href='http://mail.freenetproject.org/mailman/listinfo/chat/'>Chat</a> (<a 
+href='http://mail.freenetproject.org/cgi-bin/mailman/listinfo/chat/'>Chat</a> (<a 
 class='wiki'  href='http://news.gmane.org/gmane.network.freenet.general'>archive</a>)
 <br />
 This list is for general discussion of Freenet, topics that are not suitable 
