04e69232aebec19af4c870bda93b61c0d3248a6c
bob <bob@freenetproject.org>
2005-12-16 00:30:34 +0000

    Recommends use of Java 1.5.0_04 or higher due to yet more applet privellege escalation exploits. Other minor changes.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@7714 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/download.php
+++ pages/download.php
@@ -13,16 +13,27 @@ improve with use.
 href="http://freenetproject.org/index.php?page=faq#firewall"><b>here</b></a> 
 for some info.</i>
 
-
-<h3>Windows</h3>- Download and run <big><a href="http://downloads.freenetproject.org/freenet-java-webinstall.exe">freenet-java-webinstall.exe</a> </big>or<br>- If you already have Java 1.4.1 or later installed <big><a href="http://downloads.freenetproject.org/freenet-webinstall.exe">freenet-webinstall.exe</a></big><br><br>It will automatically install Freenet and other required components for you. When done, double-click on the small blue rabbit icon in your task-tray. <br>
-(Freenet contains <u><b>NO spyware or adware</b></u> , it's Free Software! The source code is publicly available for review)
-<p>Freenet works best with <b>Windows 2000, XP Professional or NT.
-</b>Windows ME, 98 and 95 work less well.</p>
+<BR>&nbsp;<BR>
+<h3>Windows</h3>- Download and run 
+<big><a href="http://downloads.freenetproject.org/freenet-java-webinstall.exe">freenet-java-webinstall.exe</a></big> which includes Java <B>or</B>
+<br>- If you already have Java 5 update 4 or greater (<B>strongly</B> recommended for security, although v1.4.1 or greater works)
+<big><a href="http://downloads.freenetproject.org/freenet-webinstall.exe">freenet-webinstall.exe</a></big><br>
+<br>It will automatically install Freenet and other required components for you. When done, double-click on the small blue rabbit icon in your system-tray. 
+<br>(Freenet contains <u><b>NO spyware or adware</b></u> , it's Free Software! The source code is publicly available for review)
+<BR>&nbsp;<BR>
+Freenet works best with <b>Windows 2000, XP Professional or NT.
+</b>Windows ME, 98 and 95 work less well.
 <!-- 2000 and Millenium Edition are different; all 2k's work good.  Millenium is also a bit borked.  The borked one is XP Home-->
+
+<BR>&nbsp;<BR>
 <h3>Unix/Linux</h3>
 You need to have a recent Java Runtime Environment.  We have experienced
-best results with Sun's Java Runtime Environment (versions 1.4.1 and later) 
-which can be obtained from <a href="http://java.sun.com/">http://java.sun.com/</a>. Currently Freenet does not run on Kaffe or GCJ, due to bugs in Classpath. This is being worked on.
+best results with Sun's Java Runtime Environment
+which can be obtained from <a href="http://www.java.com/">http://www.java.com/</a>.
+<BR>Java version 1.4.1 and later will work. However, be aware that there are applet security vulnerabilities in all versions prior to
+Java 5 update 4. Generally, we recommend using at least Java 1.4.2.
+
+<P>Currently Freenet does not run on Kaffe or GCJ, due to bugs in Classpath. This is being worked on.</P>
 Once Java is installed, download 
 <a 
 href="http://downloads.freenetproject.org/freenet-stable-latest.tgz">freenet-stable-latest.tgz</a>. 
@@ -38,13 +49,16 @@ computer, you may need to wait about 30 seconds), and visit
 http://127.0.0.1:8888/ in your web browser to access Freenet's user
 interface.
 
+
+<BR>&nbsp;<BR>
 <h3>Hardware requirements</h3>
 <b>Minimum:</b> 400MHz Pentium 2, with at least 192MB of RAM.<br>
 <b>Recommended:</b> 1GHz or more processor with 256MB <!-- decreasing agaisnt my will :(-->RAM or 
 more (especially if using Windows XP).
 
-<h3>Upgrading</h3>
 
+<BR>&nbsp;<BR>
+<h3>Upgrading</h3>
 After installing Freenet, Windows users can upgrade to the latest daily Freenet
 "snapshot" release by clicking on "Update Snapshot" in the Freenet section of their
 Start menu.
@@ -56,6 +70,8 @@ To see the latest changes of the builds in the stable branch, you can go to the
 <b><a
 href="http://www.freenetproject.org/index.php?page=whatsnew">Changelog</b></a>.
 
+
+<BR>&nbsp;<BR>
 <h3>Source Code</h3>
 You can obtain the latest source code from SVN from <a href="https://emu.freenetproject.org/svn/trunk/">https://emu.freenetproject.org/svn/</a>, or you can download a daily snapshot:<ul>
 <li><a href="http://downloads.freenetproject.org/freenet-stable-latest.src.tbz">Stable</a> - 
@@ -64,5 +80,5 @@ current stable branch source code</li>
 href="http://downloads.freenetproject.org/freenet-unstable-latest.src.tbz">Unstable</a> 
 - current unstable branch source code</li>
 <li><a href="http://downloads.freenetproject.org/freenet-contrib-latest.src.tar.bz2">Contrib</a> - separate module needed to compile freenet-ext.jar; unpack this and one of the above in a common parent directory, and then run "ant distclean dist" to build everything.</li>
-</ul>
+</ul><BR>
 <!--end of empty space--></body>
