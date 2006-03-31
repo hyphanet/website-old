1d8943122a657c933b035a6751c72d19f23b3eb3
Matthew Toseland <toad@amphibian.dyndns.org>
2006-03-31 15:36:34 +0000

    Add download-new page. This will be the download freenet 0.7 page. Thanks to sich.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@8397 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ pages/download-new.php
@@ -0,0 +1,49 @@
+<body><h2>Download Freenet</h2>
+
+<h3>Important note for first time users</h3>
+
+When you first start Freenet your node will not know other node on the network, you need to connect to other node. To find other node you can connect on irc server <big>irc.freenode.com</big> and join the room <big>#freenet</big>.
+<p> <i>If you have a firewall or a NAT box click <a 
+href="http://freenetproject.org/index.php?page=faq#firewall"><b>here</b></a> for some info.</i> <BR>
+&nbsp;<BR>
+<h3>Windows</h3>
+- Run <big><a href="http://downloads.freenetproject.org/freenet-pre-07-alpha.exe">freenet-webinstall</a></big><br>
+<br>
+It will automatically install Freenet and other required components for you. When done, he launch your browser directly on Freenet. <br>
+(Freenet contains <u><b>NO spyware or adware</b></u> , it's Free Software! The source code is publicly available for review) <BR>
+&nbsp;<BR>
+Freenet works best with <b>Windows 2000 Professional, XP Professional or NT. </b> Windows 95, 98 and 2000 Millennium Edition work rather
+less well; in particular, the installer won't run on those operating systems.
+<BR>
+&nbsp;<BR>
+<h3>Unix/Linux</h3>
+You need to have a recent Java Runtime Environment. We have experienced best results with Sun's Java Runtime Environment which can be obtained from <a href="http://www.java.com/">http://www.java.com/</a>. <BR>
+Java version 1.4.1 and later will work. However, be aware that there are applet security vulnerabilities in all versions prior to Java 5 update 4. Generally, we recommend using at least Java 1.4.2.
+<P>- Run <big><a href="http://downloads.freenetproject.org/alpha/installer/freenet.jnlp">freenet-webinstall</a></big></P>
+<P>If you encounter some problems try this : javaws http://downloads.freenetproject.org/alpha/installer/freenet.jnlp</P>
+<P>After you start Freenet wait a few seconds for it to start-up (on a slow computer, you may need to wait about 30 seconds), and visit http://127.0.0.1:8888/ in your web browser to access Freenet's user interface. <BR>
+&nbsp;<BR>
+</P>
+<h3>Hardware requirements</h3>
+<b>Minimum:</b> 400MHz Pentium 2, with at least 192MB of RAM.<br>
+<b>Recommended:</b> 1GHz or more processor with 256MB
+<!-- decreasing agaisnt my will :(-->
+RAM or more (especially if using Windows XP). <BR>
+&nbsp;<BR>
+<h3>Upgrading</h3>
+After installing Freenet, Windows users can upgrade to the latest daily Freenet "snapshot" release by clicking on "update.cmd" in the Freenet directory.
+<p> Linux users may similarily upgrade by running the update.sh shell script in the freenet/ directory.
+<p>
+<h3>Source Code</h3>
+You can obtain the latest source code from SVN from https://emu.freenetproject.org/svn/, or you can download a daily snapshot:
+<ul>
+  <li><a href="http://downloads.freenetproject.org/freenet-stable-latest.src.tbz">Stable</a> - current stable branch source code</li>
+  <li><a 
+href="http://downloads.freenetproject.org/freenet-unstable-latest.src.tbz">Unstable</a> - current unstable branch source code</li>
+  <li><a href="http://downloads.freenetproject.org/freenet-contrib-latest.src.tar.bz2">Contrib</a> - separate module needed to compile freenet-ext.jar; unpack this and one of the above in a common parent directory, and then run "ant distclean dist" to build everything.</li>
+</ul>
+<BR>
+<h3>
+  <!--end of empty space-->
+</h3>
+</body>
