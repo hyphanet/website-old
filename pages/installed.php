fabc8f47d893b09daed1fc19b613a712635c78a7
Matthew Toseland <toad@amphibian.dyndns.org>
2006-03-31 19:15:58 +0000

    add the installed-a-node page
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@8414 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ pages/installed.php
@@ -0,0 +1,15 @@
+<h2>Successful installation of Freenet 0.7!</h2>
+
+Congratulations, you have successfully installed a Freenet 0.7 alpha-test node.<br />
+<br />
+You can access it <a href="http://127.0.0.1:8888/">here</a>.<br />
+<br />
+If you installed a testnet node you can access it <a href="http://127.0.0.1:8889/">here</a>.<br />
+<br />
+These links will take you to FProxy, the web interface to Freenet. You can also run Frost, a message boards program, which may have been installed; there may be an icon on your desktop, otherwise have a look at the directory the node was installed to.<br />
+<br />
+You will need to get some darknet (or testnet) connections. If you know people using Freenet, ask them for their noderefs (this is ideal), otherwise please join us on IRC on #freenet on irc.freenode.net, and we will get you wired in to the network.<br />
+<br />
+Once again thanks for using Freenet!<br />
+<br />
+If you are feeling especially generous, you can <a href="http://freenetproject.org/index.php?page=donate">donate here</a> so that we can continue to employ a full time coder.
