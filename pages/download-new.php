369c26aa029b0c54d238ee9188c0ae6b29cffbeb
ian <ian@freenetproject.org>
2006-04-02 17:45:20 +0000

    minor fixes
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@8437 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/download-new.php
+++ pages/download-new.php
@@ -2,16 +2,23 @@
 
 <h3>Important note for first time users</h3>
 
-Freenet 0.7 is different to most other P2P networks in that, to protect your security, you only connect directly to other people you trust, but through them you gain access to the 
-entire Freenet network.
+Freenet 0.7 is different to most other P2P networks in that, to protect
+your security, you only connect directly to other people you trust, but
+through them you gain access to the entire Freenet network.
 <p>
-When you first start Freenet 0.7 your node will not know any other nodes on the network, you need to connect to other nodes, at least three.  Ideally you should find people you trust 
-that are already part of the Freenet 0.7 network and connect to them, but if that isn't possible in the early stages of the Freenet 0.7 network you can try connecting to the irc server
-<big>irc.freenode.net</big> and join the channel <big>#freenet</big>, to see if anyone will connect to you.
 
-<p> <i>If you have a firewall or a NAT box click <a 
-href="http://freenetproject.org/index.php?page=faq#firewall"><b>here</b></a> for some info.</i> <BR>
-&nbsp;<BR>
+When you first start Freenet 0.7 your node will not know any other nodes
+on the network, you need to connect to other nodes, at least three. 
+Ideally you should find people you trust that are already part of the
+Freenet 0.7 network and connect to them, but if that isn't possible in
+the early stages of the Freenet 0.7 network you can try connecting to
+the irc server <big>irc.freenode.net</big> and join the channel
+<big>#freenet</big>, to see if anyone will connect to you.
+
+<p> <i>If you have a firewall or a NAT box click <a
+href="http://freenetproject.org/index.php?page=faq#firewall"><b>here</b></a>
+for some info.</i> <BR> &nbsp;<BR>
+
 <h3>Windows</h3>
 - Run <big><a href="http://downloads.freenetproject.org/freenet-pre-07-alpha.exe">freenet-webinstall</a></big><br>
 <br>
@@ -20,16 +27,26 @@ It will automatically install Freenet and other required components for you. Whe
 &nbsp;<BR>
 Freenet works best with <b>Windows 2000 Professional, XP Professional or NT. </b> Windows 95, 98 and 2000 Millennium Edition work rather
 less well; in particular, the installer won't run on those operating systems.
-<BR>
-&nbsp;<BR>
-<h3>Unix/Linux</h3>
+
+<h3>Mac OSX, Unix, and Linux</h3>
 You need to have a recent Java Runtime Environment. We have experienced best results with Sun's Java Runtime Environment which can be obtained from <a href="http://www.java.com/">http://www.java.com/</a>. <BR>
 Java version 1.4.1 and later will work. However, be aware that there are applet security vulnerabilities in all versions prior to Java 5 update 4. Generally, we recommend using at least Java 1.4.2.
-<P>- Run <big><a href="http://downloads.freenetproject.org/alpha/installer/freenet.jnlp">freenet-webinstall</a></big></P>
-<P>If you encounter some problems try this : javaws http://downloads.freenetproject.org/alpha/installer/freenet.jnlp</P>
-<P>After you start Freenet wait a few seconds for it to start-up (on a slow computer, you may need to wait about 30 seconds), and visit http://127.0.0.1:8888/ in your web browser to access Freenet's user interface. <BR>
-&nbsp;<BR>
-</P>
+
+<P>- Run <big><a
+href="http://downloads.freenetproject.org/alpha/installer/freenet.jnlp">freenet-webinstall</a></big></P>
+
+<P>If you encounter some problems try typing the following at the 
+command line : 
+
+<pre>
+javaws http://downloads.freenetproject.org/alpha/installer/freenet.jnlp</P>
+</pre>
+
+<P>After you start Freenet wait a few seconds for it to start-up (on a
+slow computer, you may need to wait about 30 seconds), and visit
+http://127.0.0.1:8888/ in your web browser to access Freenet's user
+interface. <BR> &nbsp;<BR> </P>
+
 <h3>Hardware requirements</h3>
 <b>Minimum:</b> 400MHz Pentium 2, with at least 192MB of RAM.<br>
 <b>Recommended:</b> 1GHz or more processor with 256MB
