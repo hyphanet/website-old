3a0e6b30bde023165c345ce49f581500aba160b8
Florent Daignière <nextgens@freenetproject.org>
2006-04-04 06:36:41 +0000

    webpages: should fix 'people'
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@8451 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/people.php
+++ pages/people.php
@@ -7,23 +7,22 @@
 <br />Storage and Retrieval System".  Ian started the Freenet Project around July of
 <br />1999, and continues to coordinate the project.
 <br />
-<br /><b>Oskar Sandberg</b>
 <br /><b>Matthew Toseland</b>
-Matthew has been working on Freenet since before the 0.5 release.  His work
-and that of others has resulted in dramatic improvements to the performance
-and stability of the network.
-<p>
-Oskar was also one of the earliest contributors to the Freenet project, and
-
-<br />continues to play a core role in ongoing development.
+<br />Matthew has been working on Freenet since before the 0.5 release.  His work
+<br />and that of others has resulted in dramatic improvements to the performance
+<br />and stability of the network.
 <br />
-<br /><b>Scott Miller</b>
-<br />Scott is responsible for the implementation of much of the cryptography
-<br />elements within Freenet.
+<br /><b>Oskar Sandberg</b>
+<br />Oskar was also one of the earliest contributors to the Freenet project, and
+<br />continues to play a core role in ongoing development.
 <br />
 <br /><b>Florent Daignière</b>
 <br />Florent has been our system administrator since around the time 0.7 was started,
 <br />and has helped on other matters like the current installer.
+<br />
+<br /><b>Scott Miller</b>
+<br />Scott is responsible for the implementation of much of the cryptography
+<br />elements within Freenet.
 <br />
 <br /><b>Steven Starr</b>
 <br />Steven is the coordinator of the Freenet Project non-profit corporation.<br />
