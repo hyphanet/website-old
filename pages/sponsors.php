25d63a4f14d2354c899218283187d8cae88a03b6
ian <ian@freenetproject.org>
2006-05-24 17:56:44 +0000

    Add sponsor to sponsors page
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@8845 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -5,6 +5,9 @@ sponsored this project through donations or discounts on hardware and services:
 <ol>
 <li><a href="http://www.defcon.org/">DEF CON</a><br>
 DEF CON is the world's largest annual hacker convention, held every year in Las Vegas, Nevada.
+<li><a href="http://www.seocompany.ca/">SEO Company</a><br>
+SEO Company provides Directory Submission Services and Website 
+Optimization & Marketing to its Canada, US, and UK clients.
 <P>
 <li><a href="http://www.verbumvanum.org/">Verbum Vanum</a><br>
 VerbumVanum is a site for free online storage and distribution of written
@@ -13,9 +16,6 @@ knowledge and art; thesises, books, endworks, researchpapers, etc...
 <li><a href="http://bytemark.co.uk/">Bytemark Hosting</a><br>
 Bytemark provides discounted hosting facilities to Freenet and comes with 
 our highest recommendation.
-<P>
-<li><a href="http://www.freesofty.org/">FreeSofty</a><br>
-Freesofty is a site where you can download freeware as much as you want, but without forgetting the developers.
 </ol>
 <p>
 
