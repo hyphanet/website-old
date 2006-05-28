04a531c11ee3973adf0bfb7cf0576fe7e46a8037
ian <ian@freenetproject.org>
2006-05-28 01:24:48 +0000

    Another minor fix
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@8894 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -12,30 +12,30 @@ support the Freenet Project, you can make a convenient donation through
 our <a href="/index.php?page=donate">donations page</a>.
 
 <h3>News</h3>
-<b><u>27th May, 2006 - Freenet selects four students to work on Freenet-related projects in Google's Summer of Code</u></b><br>
+<b>27th May, 2006 - Freenet selects four students to work on Freenet-related projects in Google's Summer of Code</b><br>
 We have selected four students to work on Freenet-related projects this summer, sponsored by Google.  These are:
 <ul>
-	<li><b>Jerome Flesch (jflesch): A file upload and download utility.</b><br>
+	<li><u>Jerome Flesch (jflesch): A file upload and download utility.</u><br>
 	For those familiar with it this will be a cross-platform replacement for Fuqid, with added search capabilities.  It will provide a user-friendly interface to the uploading and downloading of large files.
 
-	<li><b>Michael Rogers (mrogers): Congestion Control and Load Balancing for Freenet 0.7.</b><br>
+	<li><u>Michael Rogers (mrogers): Congestion Control and Load Balancing for Freenet 0.7.</u><br>
 	Michael will be working on simulations, theoretical work, and maybe implementation, to sort out Freenet's current load issues. Our present load limiting/balancing algorithm not only does not work as well as it should, but it also has some rather serious security issues.
 
-	<li><b>Dave Baker (dbkr): Secure, email-like messaging over Freenet.</b><br>
+	<li><u>Dave Baker (dbkr): Secure, email-like messaging over Freenet.</u><br>
 	The Freemail project lives again! Dave will code a portable Freenet-based email system, with strong spam prevention features and hopefully an optional web interface (for those who don't use SMTP/POP directly). This will probably be bundled with future versions of Freenet. It will be especially useful for human rights organizations and distributed development (bringing the "darknet" freenet community closer to the developers).
 
-	<li><b>Florent Daigniere (nextgens): Installer and related components.</b><br>
+	<li><u>Florent Daigniere (nextgens): Installer and related components.</u><br>
 
 	Florent's proposal encompasses creating an uninstaller, sorting out some serious issues with the installer, creating a systray icon, fixing some issues with the website, and especially sorting out the remaining issues with free java implementations (some recent JVM-related bugs have shown why this is important) and packaging Freenet for Linux distributions.
 </ul>
 <p>
-<b><u>18th May, 2006 - New stable build: 5107</u></b><br>
+<b>18th May, 2006 - New stable build: 5107</b><br>
 A new stable build of Freenet is now available. This is a maintainance release:<br>
 	* The updating script has been updated to use new mirrors<br>
 	* A fix has been merged from 0.7 (regarding NativeLibraries which were preventing the node from starting up)<br>
 <p>
-<b><u>27th April, 2006: Freenet particpates in Google Summer of 
-Code</u></b><br>
+<b>27th April, 2006: Freenet particpates in Google Summer of 
+Code</b><br>
 Wondering how to spend your time this Summer?  Interested in being paid 
 $4,500 by Google to work on a Freenet-related project?  Well, this may 
 just be your lucky day, as this year Freenet a Mentoring 
@@ -43,7 +43,7 @@ Organization for Google's Summer of Code.  Please <a
 href="http://wiki.freenetproject.org/SummerOfCode">look here</a> for 
 more details.
 <p>
-<b><u>21st April, 2006: New Frost released with 0.7 support</u></b><br>
+<b>21st April, 2006: New Frost released with 0.7 support</b><br>
 A new version of <a href="http://jtcfrost.sf.net/">Frost</a>, the 
 Freenet message board system, has been released, and it now supports 
 Freenet 0.7!  Note that Frost requires that a copy of Freenet is up and 
