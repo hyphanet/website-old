8f1e1c37333ab15dc49d62830f5be6c1c122196a
sbc <sbc@freenetproject.org>
2006-06-28 13:44:49 +0000

    Hopefully brings tools.php a bit up to date.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@9394 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/tools.php
+++ pages/tools.php
@@ -1,19 +1,39 @@
-<h1>Applications
+<h1>Tools
 
 </h1>
+<br /><a class='wiki'  href='http://jtcfrost.sf.net/'><b>Frost</b></a>
+<br />
+<br />
+Frost is a filesharing and message board application for Freenet. Visit the <a class='wiki'  href='http://127.0.0.1:8888/SSK@XeMBryjuEaxqazEuxwnn~G7wCUOXFOZlVWbscdCOUFs,209eycYVidlZvhgL5V2a3INFxrofxzQctEZvyJaFL7I,AQABAAE/frost-1/'>Frost freesite</a>. <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br />
+<br /><a class='wiki'  href='http://wiki.freenetproject.org/FreenetJsite'><b>jSite</b></a>
+<br />
+<br />
+jSite is a graphical application that you can use to create, insert and manage your own Freenet sites.
+<br />
+<br /><a class='wiki'  href='http://127.0.0.1:8888/USK@DWB7AD1mBr5HNatdWvbqBe5FdZ3Zt~eS3nPHdp1Yg~g,HK1~x4rUgH6NyIT87CymSWcIHLP2Yc6dPQ417n1gnBY,AQABAAE/fuqid-1/'><b>FUQID</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br />
+<br />
+FUQID is the Freenet Utility for Queued Inserts and Downloads. It is a Windows application for adding stuff to, and downloading stuff from, Freenet. It will also work on Linux using <a href="http://www.winehq.com/">Wine</a>.
+<br />
+<hr/>
+<h2>Freenet 0.5 applications
+</h2>
+<br />
+Pleas notice that many of these tools are outdated and unmaintained. All development efforts are currently focused on freenet 0.7.
 <br />A variety of people have written some excellent tools and clients for use with Freenet.
 <br /><p>
 <h3><a href="http://www.freenetproject.org/index.php?page=fphowto">Development Aides</a></h3>
 <p>
 <br /><h3>Freenet Publishing & Retrieval Tools</h3>
 
-<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@M7yZgrl8gwtAe1xEcR5Xyv4tFsoPAgM/fiw/9//'><b>Freesite Insertion Wizard</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@M7yZgrl8gwtAe1xEcR5Xyv4tFsoPAgM/fiw/9//'><b>Freesite Insertion Wizard</b></a> <b><span style='color:#FF0000;'>You must be running Freenet 0.5 for this link to work.</span></b>
 <br />
 <br />FIW is a feature packed Java AWT application for inserting freesites.  It contains a lot of help at each stage of the process, and includes site verification to increase availability of the inserts.
 <br />
-<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@kWu5Osv~VAI3-kH7z8QIVxklv-YPAgM/fishtools/52//'><b>FishTools</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@kWu5Osv~VAI3-kH7z8QIVxklv-YPAgM/fishtools/52//'><b>FishTools</b></a> <b><span style='color:#FF0000;'>You must be running Freenet 0.5 for this link to work.</span></b>
 <p>
-<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@CKesZYUJWn2GMvoif1R4SDbujIgPAgM/fuqid/12//'><b>Freenet Utility for Queued Inserts and Downloads</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@CKesZYUJWn2GMvoif1R4SDbujIgPAgM/fuqid/12//'><b>Freenet Utility for Queued Inserts and Downloads</b></a> <b><span style='color:#FF0000;'>You must be running Freenet 0.5 for this link to work.</span></b>
 <br />
 <br />FUQID is a Windows program (but it will run in Wine under Linux) for inserting and downloading large files. It offers fast native CHK calculation and FEC decoding and a simple, highly configurable interface, better than FProxy.
 <br />
@@ -29,11 +49,11 @@
 <br />
 <br />Frost is a file-sharing tool for Freenet, it also has chat forums which allow people to chat anonymously over Freenet. It now supports FEC (which makes downloads more reliable).
 <br />
-<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@h~ixmz11-tDOox9O1gQyjkzAUCcPAgM/fmb/5//'><b>Freenet Message Board</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@h~ixmz11-tDOox9O1gQyjkzAUCcPAgM/fmb/5//'><b>Freenet Message Board</b></a> <b><span style='color:#FF0000;'>You must be running Freenet 0.5 for this link to work.</span></b>
 <br />
 <br />FMB is an excellent Usenet-like tool which allows secure messages to be exchanged via Freenet, it is somewhat similar to Frost, but employs a more sophisticated "outbox" based approach. 
 <br />
-<br />It seems that the original author does not have the time to release new versions of it, some other Freenet users created modified versions. For more information visit <a class='wiki'  href='http://127.0.0.1:8888/SSK@rjYFfgPHfolmcStiaoxESFfBXz8PAgM/FMBwishlist/11//'>FMB Wishlist</a>. <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br />It seems that the original author does not have the time to release new versions of it, some other Freenet users created modified versions. For more information visit <a class='wiki'  href='http://127.0.0.1:8888/SSK@rjYFfgPHfolmcStiaoxESFfBXz8PAgM/FMBwishlist/11//'>FMB Wishlist</a>. <b><span style='color:#FF0000;'>You must be running Freenet 0.5 for this link to work.</span></b>
 
 <br />
 <br /><a class='wiki'  href='http://www.storrow.org/Software/FreeNews/'><b>FreeNews</b></a>
@@ -45,7 +65,7 @@
 <br /><hr><h3>Misc Tools
 </h3>
 
-<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@UjHShzjg6iS5UNGGb4IpFPhHGeoPAgM,p7qut8fPyia~CgO7noQdLA/pm4pigs/10//'><b>pm4pigs</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@UjHShzjg6iS5UNGGb4IpFPhHGeoPAgM,p7qut8fPyia~CgO7noQdLA/pm4pigs/10//'><b>pm4pigs</b></a> <b><span style='color:#FF0000;'>You must be running Freenet 0.5 for this link to work.</span></b>
 <br />
 <br />
 pm4pigs is a freenet client written in python for sending and receiving encrypted messages.
@@ -58,9 +78,9 @@ pm4pigs is a freenet client written in python for sending and receiving encrypte
 <br /><b>Update:</b> new versions written in OCaml will be released in binary form for Win32, Linux/x86, Linux/PPC, and OSX.
 <br />
 
-<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@fgbuxwSCCjOJHsUI-9-uijD1haQPAgM/flinks/11//'><b>Flinks</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+<br /><a class='wiki'  href='http://127.0.0.1:8888/SSK@fgbuxwSCCjOJHsUI-9-uijD1haQPAgM/flinks/11//'><b>Flinks</b></a> <b><span style='color:#FF0000;'>You must be running Freenet 0.5 for this link to work.</span></b>
 <br />
-<br />Flinks is an application to help people gather, manage, and share links in freenet.  Currently, several indicies generate flinks data (<a class='wiki'  href='http://127.0.0.1:8888/SSK@-w495UL3mfSlWC2c~nRAuG2fAWwPAgM/TFEE//'><b>The Freedom Engine Engine</b></a>, <a class='wiki'  href='http://127.0.0.1:8888/SSK@Sc6qV~D6iFhaYord6HtbjJ8MaEYPAgM/YoYo//'><b>YoYo</b></a>, and the <a class='wiki'  href='http://127.0.0.1:8888/SSK@rjYFfgPHfolmcStiaoxESFfBXz8PAgM/FreenetHelp//'><b>Freenet Help Index</b></a>, among others (<b><span style='color:#FF0000;'>You must be running Freenet for those links to work.</span></b>)), so you can go to their page and download their links, allowing you to essentially build your own index with sites you like.<p>
+<br />Flinks is an application to help people gather, manage, and share links in freenet.  Currently, several indicies generate flinks data (<a class='wiki'  href='http://127.0.0.1:8888/SSK@-w495UL3mfSlWC2c~nRAuG2fAWwPAgM/TFEE//'><b>The Freedom Engine Engine</b></a>, <a class='wiki'  href='http://127.0.0.1:8888/SSK@Sc6qV~D6iFhaYord6HtbjJ8MaEYPAgM/YoYo//'><b>YoYo</b></a>, and the <a class='wiki'  href='http://127.0.0.1:8888/SSK@rjYFfgPHfolmcStiaoxESFfBXz8PAgM/FreenetHelp//'><b>Freenet Help Index</b></a>, among others (<b><span style='color:#FF0000;'>You must be running Freenet 0.5 for those links to work.</span></b>)), so you can go to their page and download their links, allowing you to essentially build your own index with sites you like.<p>
 
 <br /><hr/>
 <br /><h2>Other tools? Want to contribute?
