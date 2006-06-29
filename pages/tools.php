bf5d9781e1c6934956c56c93751ee0b71d721086
Florent Daignière <nextgens@freenetproject.org>
2006-06-29 16:34:00 +0000

    website: fix #444 : updating the tools page
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@9403 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/tools.php
+++ pages/tools.php
@@ -1,24 +1,28 @@
-<h1>Tools
-
-</h1>
-<br /><a class='wiki'  href='http://jtcfrost.sf.net/'><b>Frost</b></a>
+<h1>Tools</h1>
+<h2>Freenet 0.7 applications</h2>
 <br />
+<p><a class='wiki'  href='http://jtcfrost.sf.net/'><b>Frost</b></a><br />
 <br />
 Frost is a filesharing and message board application for Freenet. Visit the <a class='wiki'  href='http://127.0.0.1:8888/SSK@XeMBryjuEaxqazEuxwnn~G7wCUOXFOZlVWbscdCOUFs,209eycYVidlZvhgL5V2a3INFxrofxzQctEZvyJaFL7I,AQABAAE/frost-1/'>Frost freesite</a>. <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+</p>
+
+<p><a class='wiki'  href='http://freenet.org.nz/python/pyFreenet/'><b>pyFreenet</b></a><br />
 <br />
-<br /><a class='wiki'  href='http://wiki.freenetproject.org/FreenetJsite'><b>jSite</b></a>
-<br />
+pyFreenet is an API and a set of tools to access freenet from the python programing language. Tools are currently command-line only.
+</p>
+
+<p><a class='wiki'  href='http://wiki.freenetproject.org/FreenetJsite'><b>jSite</b></a><br />
 <br />
 jSite is a graphical application that you can use to create, insert and manage your own Freenet sites.
-<br />
-<br /><a class='wiki'  href='http://127.0.0.1:8888/USK@DWB7AD1mBr5HNatdWvbqBe5FdZ3Zt~eS3nPHdp1Yg~g,HK1~x4rUgH6NyIT87CymSWcIHLP2Yc6dPQ417n1gnBY,AQABAAE/fuqid-1/'><b>FUQID</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
-<br />
+</p>
+
+<p><a class='wiki'  href='http://127.0.0.1:8888/USK@DWB7AD1mBr5HNatdWvbqBe5FdZ3Zt~eS3nPHdp1Yg~g,HK1~x4rUgH6NyIT87CymSWcIHLP2Yc6dPQ417n1gnBY,AQABAAE/fuqid/1/'><b>FUQID</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b><br />
 <br />
 FUQID is the Freenet Utility for Queued Inserts and Downloads. It is a Windows application for adding stuff to, and downloading stuff from, Freenet. It will also work on Linux using <a href="http://www.winehq.com/">Wine</a>.
+</p>
 <br />
 <hr/>
-<h2>Freenet 0.5 applications
-</h2>
+<h2>Freenet 0.5 applications</h2>
 <br />
 Pleas notice that many of these tools are outdated and unmaintained. All development efforts are currently focused on freenet 0.7.
 <br />A variety of people have written some excellent tools and clients for use with Freenet.
