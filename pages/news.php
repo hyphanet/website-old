904e2a6de1c4610d362799afcd13edba79d82897
ian <ian@freenetproject.org>
2006-07-03 23:07:57 +0000

    add status report to news page
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@9457 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -12,36 +12,52 @@ support the Freenet Project, you can make a convenient donation through
 our <a href="/index.php?page=donate">donations page</a>.
 
 <h3>News</h3>
+<b>3rd July, 2006 - Freenet Status report</b><br>
+The pace of coding on Freenet has increased dramatically since the
+release of the 0.7 alpha. We now have an active group of core developers
+working with Matthew Toseland (our full-time dev), and a growing
+community of developers working on third-party applications through the
+Freenet Client Protocol. After the rather stagnant period before the
+decision to switch to a darknet model, Freenet is now more active and
+vibrant than it has ever been.
+<p>
+Current estimates put the size of the darknet between 300-400 nodes, not
+bad for a hard-to-use alpha release, and we are seeing an average of
+about 50 commits per week from developers. We have 4 Google Summer of
+Code projects working on everything from a new load-balancing mechanism,
+to a cross-platform file upload and download utility (called "Thaw"),
+and a flexible user-friendly installer.
+<p>
+We have also been thinking hard about how to minimize the inherent
+usability challenges posed by a darknet approach, and with this in mind,
+have implemented support for third party applications to add and remove
+darknet connections via FCP. This means we can do things like write IM
+plugins to make it very easy to establish connections to friends.
+<p>
+We will also be deploying "opennet" functionality, so that users who
+don't need the security offered by the darknet, can just start up a node
+and get going immediately, as with previous versions of Freenet, but
+this time with the benefits of NAT circumvention and a UDP-based
+transport.
+<p>
+On the more administrative side, Florent Daigniere has been a big help,
+setting up a bug tracker, and a
+variety of other tools to help streamline Freenet's development process.
+<p>
+Its not all good news though, despite the generosity of many of our
+users, donations to the project have been unable to keep-up with the
+$2300 per month (a meager salary for a software developer in the UK)
+needed by Matthew Toseland, and right now we are on the verge of not
+being able to continue to pay him - which would obviously be terrible
+for the project. In the past, users and supporters of the project have
+been extremely generous when we have asked them to help us out of a
+financial hole, and I am hoping that you can be similarly generous on
+this occasion.
+<p>
+With this in mind, I would ask that you visit our donations page on the
+website, it can be found <a href="http://freenetproject.org/donate.html">here</a>.
+<p>
+And please donate anything you can spare.
 <b>2nd June, 2006 - Freenet lecture available on Google Video</b><br>
 You can now see the talk given by Ian and Oskar in Berlin last December conveniently embedded <a href="/22c3vid.html">on this website</a> thanks to Google Video.
 <p>
-<b>27th May, 2006 - Freenet selects four students to work on Freenet-related projects in Google's Summer of Code</b><br>
-We have selected four students to work on Freenet-related projects this summer, sponsored by Google.  These are:
-<ul>
-	<li><u>Jerome Flesch: A file upload and download utility.</u><br>
-	Jerome will be creating a user-friendly cross-platform interface to the searching for, uploading and downloading of large files.  It will be a cross-platform replacement for Fuqid (with the addition of search).  
-
-	<li><u>Michael Rogers: Congestion Control and Load Balancing for Freenet 0.7.</u><br>
-	Michael will be working on simulations, theoretical work, and maybe implementation, to sort out Freenet's current load issues. Our present load limiting/balancing algorithm not only does not work as well as it should, but it also has some rather serious security issues.
-
-	<li><u>Dave Baker: Secure, email-like messaging over Freenet.</u><br>
-	Dave will code a portable Freenet-based email system, with strong spam prevention features and hopefully an optional web interface (for those who don't use SMTP/POP directly). This will probably be bundled with future versions of Freenet. It will be especially useful for human rights organizations and distributed development (bringing the "darknet" freenet community closer to the developers).
-
-	<li><u>Florent Daigniere: Installer and related components.</u><br>
-
-	Florent's proposal encompasses creating an uninstaller, sorting out some serious issues with the installer, creating a systray icon, fixing some issues with the website, and especially sorting out the remaining issues with free java implementations (some recent JVM-related bugs have shown why this is important) and packaging Freenet for Linux distributions.
-</ul>
-<p>
-<b>18th May, 2006 - New stable build: 5107</b><br>
-A new stable build of Freenet is now available. This is a maintainance release:<br>
-	* The updating script has been updated to use new mirrors<br>
-	* A fix has been merged from 0.7 (regarding NativeLibraries which were preventing the node from starting up)<br>
-<p>
-<b>27th April, 2006: Freenet particpates in Google Summer of 
-Code</b><br>
-Wondering how to spend your time this Summer?  Interested in being paid 
-$4,500 by Google to work on a Freenet-related project?  Well, this may 
-just be your lucky day, as this year Freenet a Mentoring 
-Organization for Google's Summer of Code.  Please <a 
-href="http://wiki.freenetproject.org/SummerOfCode">look here</a> for 
-more details.
