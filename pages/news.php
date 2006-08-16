6c2d7341a2c898c4dea817d1345f668e5f780cfd
ian <ian@freenetproject.org>
2006-08-16 07:50:20 +0000

    remove status update, which is now unduely negative
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@10106 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -14,53 +14,6 @@ support the Freenet Project, you can make a convenient donation through
 our <a href="/index.php?page=donate">donations page</a>.
 -->
 <h3>News</h3>
-<b>3rd July, 2006 - Freenet Status report</b><br>
-The pace of coding on Freenet has increased dramatically since the
-release of the 0.7 alpha. We now have an active group of core developers
-working with Matthew Toseland (our full-time dev), and a growing
-community of developers working on third-party applications through the
-Freenet Client Protocol. After the rather stagnant period before the
-decision to switch to a darknet model, Freenet is now more active and
-vibrant than it has ever been.
-<p>
-Current estimates put the size of the darknet between 300-400 nodes, not
-bad for a hard-to-use alpha release, and we are seeing an average of
-about 50 commits per week from developers. We have 4 Google Summer of
-Code projects working on everything from a new load-balancing mechanism,
-to a cross-platform file upload and download utility (called "Thaw"),
-and a flexible user-friendly installer.
-<p>
-We have also been thinking hard about how to minimize the inherent
-usability challenges posed by a darknet approach, and with this in mind,
-have implemented support for third party applications to add and remove
-darknet connections via FCP. This means we can do things like write IM
-plugins to make it very easy to establish connections to friends.
-<p>
-We will also be deploying "opennet" functionality, so that users who
-don't need the security offered by the darknet, can just start up a node
-and get going immediately, as with previous versions of Freenet, but
-this time with the benefits of NAT circumvention and a UDP-based
-transport.
-<p>
-On the more administrative side, Florent Daigniere has been a big help,
-setting up a bug tracker, and a
-variety of other tools to help streamline Freenet's development process.
-<p>
-Its not all good news though, despite the generosity of many of our
-users, donations to the project have been unable to keep-up with the
-$2300 per month (a meager salary for a software developer in the UK)
-needed by Matthew Toseland, and right now we are on the verge of not
-being able to continue to pay him - which would obviously be terrible
-for the project. In the past, users and supporters of the project have
-been extremely generous when we have asked them to help us out of a
-financial hole, and I am hoping that you can be similarly generous on
-this occasion.
-<p>
-With this in mind, I would ask that you visit our donations page on the
-website, it can be found <a href="http://freenetproject.org/donate.html">here</a>.
-<p>
-And please donate anything you can spare.
-<p>
 <b>2nd June, 2006 - Freenet lecture available on Google Video</b><br>
 You can now see the talk given by Ian and Oskar in Berlin last December conveniently embedded <a href="/22c3vid.html">on this website</a> thanks to Google Video.
 <p>
