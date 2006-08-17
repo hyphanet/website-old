8a1a6c40dde881c4ccc27c5c44601be623bb05a8
ian <ian@freenetproject.org>
2006-08-17 03:02:43 +0000

    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@10153 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -5,6 +5,7 @@ sponsored this project through donations or discounts on hardware and services:
 <ol>
 <li><a href="http://www.toad.com/gnu/">John Gilmore</a><br>
 John Gilmore is one of the founders of the Electronic Frontier Foundation, the Cypherpunks mailing list, and Cygnus Solutions. He created the alt.* hierarchy in Usenet and is a major contributor to the GNU project.
+<p>
 <li><a href="http://www.defcon.org/">DEF CON</a><br>
 DEF CON is the world's largest annual hacker convention, held every year in Las Vegas, Nevada.
 <p>
