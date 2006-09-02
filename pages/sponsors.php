5946bdf2a21b03b1080e6cc2ca617e16b92259fc
ian <ian@freenetproject.org>
2006-09-02 15:58:34 +0000

    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@10356 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -6,17 +6,6 @@ sponsored this project through donations or discounts on hardware and services:
 <li><a href="http://www.toad.com/gnu/">John Gilmore</a><br>
 John Gilmore is one of the founders of the Electronic Frontier Foundation, the Cypherpunks mailing list, and Cygnus Solutions. He created the alt.* hierarchy in Usenet and is a major contributor to the GNU project.
 <p>
-<li><a href="http://www.defcon.org/">DEF CON</a><br>
-DEF CON is the world's largest annual hacker convention, held every year in Las Vegas, Nevada.
-<p>
-<li><a href="http://www.seocompany.ca/">SEO Company</a><br>
-SEO Company provides Directory Submission Services and Website 
-Optimization & Marketing to its Canada, US, and UK clients.
-<P>
-<li><a href="http://www.verbumvanum.org/">Verbum Vanum</a><br>
-VerbumVanum is a site for free online storage and distribution of written
-knowledge and art; thesises, books, endworks, researchpapers, etc... 
-<P>
 <li><a href="http://bytemark.co.uk/">Bytemark Hosting</a><br>
 Bytemark provides discounted hosting facilities to Freenet and comes with 
 our highest recommendation.
