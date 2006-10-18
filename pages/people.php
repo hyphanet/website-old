bebed8257b87e9866cd0b1bd696f15e7a94e90a4
Florent Daignière <nextgens@freenetproject.org>
2006-10-18 20:13:24 +0000

    website: test commit: replace <br> with <p> on the people page
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@10672 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/people.php
+++ pages/people.php
@@ -1,26 +1,25 @@
 <h3>People</h3>
-<b>Note:</b> This is an incomplete list, a large number of people have
-<br />contributed to the project. 
-<br />
-<br /><b>Ian Clarke</b>
-<br />Freenet is based on Ian's paper "A Distributed Decentralised Information
-<br />Storage and Retrieval System".  Ian started the Freenet Project around July of
-<br />1999, and continues to coordinate the project.
-<br />
-<br /><b>Matthew Toseland</b>
-<br />Matthew has been working on Freenet since before the 0.5 release.  His work
-<br />and that of others has resulted in dramatic improvements to the performance
-<br />and stability of the network.
-<br />
-<br /><b>Oskar Sandberg</b>
-<br />Oskar was also one of the earliest contributors to the Freenet project, and
-<br />continues to play a core role in ongoing development.
-<br />
-<br /><b>Florent Daignière</b>
-<br />Florent has been our system administrator since around the time 0.7 was started,
-<br />and has helped on other matters like the current installer.
-<br />
-<br /><b>Scott Miller</b>
-<br />Scott is responsible for the implementation of much of the cryptography
-<br />elements within Freenet.
+<p><b>Note:</b> This is an incomplete list, a large number of people have
+contributed to the project. </p>
 
+<p><b>Ian Clarke</b><br>
+Freenet is based on Ian's paper "A Distributed Decentralised Information
+Storage and Retrieval System".  Ian started the Freenet Project around July of
+1999, and continues to coordinate the project.</p>
+
+<p><b>Matthew Toseland</b><br>
+Matthew has been working on Freenet since before the 0.5 release.  His work
+and that of others has resulted in dramatic improvements to the performance
+and stability of the network.</p>
+
+<p><b>Oskar Sandberg</b><br>
+Oskar was also one of the earliest contributors to the Freenet project, and
+continues to play a core role in ongoing development.</p>
+
+<p><b>Florent Daignière</b><br>
+Florent has been our system administrator since around the time 0.7 was started,
+and has helped on other matters like the current installer.</p>
+
+<p><b>Scott Miller</b><br>
+Scott is responsible for the implementation of much of the cryptography
+elements within Freenet.</p>
