5025ed05483af913376b43f1ea667ceb6f90e768
Matthew Toseland <toad@amphibian.dyndns.org>
2006-11-29 18:52:36 +0000

    Full names
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@11114 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -20,14 +20,14 @@ as part of their <a href="http://code.google.com/soc/">Summer of Code</a> projec
 (to get students into open source). Here's a summary of all the good things that 
 came out of this:
 <ul>
-<li><b>Freemail</b> (dbkr)<br>
+<li><b>Freemail</b> David Baker (dbkr)<br>
 dbkr wrote an email server which uses Freenet as a back-end. This is one
 of the features which is most asked-for by groups in repressive regimes,
 and fills an important hole in Freenet's current functionality (we have
 web sites, we have discussion boards, we don't really have private
 messaging). This is now working, its architecture is complete, and it
 will be bundled with Freenet soon.</li>
-<li><b>Load simulations</b> (mrogers)<br>
+<li><b>Load simulations</b> - Michael Rogers (mrogers)<br>
 mrogers wrote us an event-based simulator for evaluating proposed
 changes to Freenet routing, load balancing and data storage. In the past
 we have generally deployed changes in a fairly haphazard way, and it has
@@ -39,7 +39,7 @@ produced a detailed simulation which has already yielded insights into
 congestion control, has confirmed some of our recent decisions on load
 balancing, and is being used to prototype a new load management
 algorithm which should radically improve Freenet's performance.</li>
-<li><b>Installer improvements and related stuff</b> (nextgens)<br>
+<li><b>Installer improvements and related stuff</b> Florent Daigniere (nextgens)<br>
 Over the summer nextgens, an experienced freenet developer already,
 worked on what was most urgently needed for Freenet, and which he had
 most expertise in, but wasn't necessarily most interesting. His work
@@ -48,7 +48,7 @@ updating system, the mirror network, native libraries and general
 bugfixing. All of this has been deployed already and has enhanced
 Freenet's usability. He also did some work on getting Freenet to work
 on free JVMs, which has yielded some results.</li>
-<li><b>Thaw (filesharing utility and upload/download manager)</b> (jflesch)<br>
+<li><b>Thaw (filesharing utility and upload/download manager)</b> Jerome Flesch (jflesch)<br>
 Over the summer jflesch developed Thaw. Thaw's goals were firstly to
 provide a user-friendly, cross platform interface to the built-in
 Freenet download manager engine (replacing an older Windows-only tool
@@ -57,7 +57,7 @@ application via sharing file indexes. The first goal has been 100%
 fulfilled and Thaw is now the preferred tool. The second goal has been
 partially achieved; Thaw has working functionality for searching file
 indexes, but this is not yet used very much.</li>
-</ul><p>All in all, a great success!</p>
+</ul><p>All in all, a great success, many thanks to Google!</p>
 <b>17th August, 2006 - John Gilmore donates US$15,000 to the Freenet Project</b> [<a href="http://digg.com/software/John_Gilmore_donates_US_15_000_to_the_Freenet_Project">digg it</a>]<br>
 John Gilmore, one of the founders of the Electronic Frontier Foundation, the Cypherpunks mailing list, and Cygnus Solutions, has donated US$15,000 to the Freenet Project, to support the ongoing development of the Freenet software.
 <p>
