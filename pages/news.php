dff5a2c3c8bc1ea882bacee8a55f237dac363d92
Matthew Toseland <toad@amphibian.dyndns.org>
2006-11-29 18:54:25 +0000

    Formatting fix, Dave Baker
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@11115 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -15,12 +15,12 @@ our <a href="/donate.html">donations page</a>.
 -->
 <h3>News</h3>
 <b>29 November, 2006 - Summer of Code roundup</b><br>
-As you know, Google paid for four students to work for us full-time over the summer,
+As you may know, Google paid for four students to work for us full-time over the summer,
 as part of their <a href="http://code.google.com/soc/">Summer of Code</a> project
 (to get students into open source). Here's a summary of all the good things that 
 came out of this:
 <ul>
-<li><b>Freemail</b> David Baker (dbkr)<br>
+<li><b>Freemail</b> - Dave Baker (dbkr)<br>
 dbkr wrote an email server which uses Freenet as a back-end. This is one
 of the features which is most asked-for by groups in repressive regimes,
 and fills an important hole in Freenet's current functionality (we have
@@ -39,7 +39,7 @@ produced a detailed simulation which has already yielded insights into
 congestion control, has confirmed some of our recent decisions on load
 balancing, and is being used to prototype a new load management
 algorithm which should radically improve Freenet's performance.</li>
-<li><b>Installer improvements and related stuff</b> Florent Daigniere (nextgens)<br>
+<li><b>Installer improvements and related stuff</b> - Florent Daigniere (nextgens)<br>
 Over the summer nextgens, an experienced freenet developer already,
 worked on what was most urgently needed for Freenet, and which he had
 most expertise in, but wasn't necessarily most interesting. His work
@@ -48,7 +48,7 @@ updating system, the mirror network, native libraries and general
 bugfixing. All of this has been deployed already and has enhanced
 Freenet's usability. He also did some work on getting Freenet to work
 on free JVMs, which has yielded some results.</li>
-<li><b>Thaw (filesharing utility and upload/download manager)</b> Jerome Flesch (jflesch)<br>
+<li><b>Thaw (filesharing utility and upload/download manager)</b> - Jerome Flesch (jflesch)<br>
 Over the summer jflesch developed Thaw. Thaw's goals were firstly to
 provide a user-friendly, cross platform interface to the built-in
 Freenet download manager engine (replacing an older Windows-only tool
