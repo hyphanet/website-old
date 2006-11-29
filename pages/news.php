1b7c2b5fb66984e5072884921a452f32edecd822
Florent Daignière <nextgens@freenetproject.org>
2006-11-29 19:37:25 +0000

    website: improve the presentation a bit; add some accentuated characters
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@11116 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/news.php
+++ pages/news.php
@@ -20,44 +20,16 @@ as part of their <a href="http://code.google.com/soc/">Summer of Code</a> projec
 (to get students into open source). Here's a summary of all the good things that 
 came out of this:
 <ul>
-<li><b>Freemail</b> - Dave Baker (dbkr)<br>
-dbkr wrote an email server which uses Freenet as a back-end. This is one
-of the features which is most asked-for by groups in repressive regimes,
-and fills an important hole in Freenet's current functionality (we have
-web sites, we have discussion boards, we don't really have private
-messaging). This is now working, its architecture is complete, and it
-will be bundled with Freenet soon.</li>
-<li><b>Load simulations</b> - Michael Rogers (mrogers)<br>
-mrogers wrote us an event-based simulator for evaluating proposed
-changes to Freenet routing, load balancing and data storage. In the past
-we have generally deployed changes in a fairly haphazard way, and it has
-often not been clear whether a particular change has been beneficial.
-Since we started work on 0.7 we have been more systematic, but the
-simulations we have built were not able to simulate many of the more
-complex processes, such as load balancing / load limiting. mrogers has
-produced a detailed simulation which has already yielded insights into
-congestion control, has confirmed some of our recent decisions on load
-balancing, and is being used to prototype a new load management
-algorithm which should radically improve Freenet's performance.</li>
-<li><b>Installer improvements and related stuff</b> - Florent Daigniere (nextgens)<br>
-Over the summer nextgens, an experienced freenet developer already,
-worked on what was most urgently needed for Freenet, and which he had
-most expertise in, but wasn't necessarily most interesting. His work
-related mostly to deployment: The installer, the uninstaller, the
-updating system, the mirror network, native libraries and general
-bugfixing. All of this has been deployed already and has enhanced
-Freenet's usability. He also did some work on getting Freenet to work
-on free JVMs, which has yielded some results.</li>
-<li><b>Thaw (filesharing utility and upload/download manager)</b> - Jerome Flesch (jflesch)<br>
-Over the summer jflesch developed Thaw. Thaw's goals were firstly to
-provide a user-friendly, cross platform interface to the built-in
-Freenet download manager engine (replacing an older Windows-only tool
-called FUQID), and secondly to evolve into a complete file sharing
-application via sharing file indexes. The first goal has been 100%
-fulfilled and Thaw is now the preferred tool. The second goal has been
-partially achieved; Thaw has working functionality for searching file
-indexes, but this is not yet used very much.</li>
+<li><b>Freemail</b> (<i>dbkr</i> - Dave Baker)<br>
+dbkr wrote an email server which uses Freenet as a back-end. This is one of the features which is most asked-for by groups in repressive regimes, and fills an important hole in Freenet's current functionality (we have web sites, we have discussion boards, we don't really have private messaging). This is now working, its architecture is complete, and it will be bundled with Freenet soon.</li>
+<li><b>Load simulations</b> (<i>mrogers</i> - Michael Rogers)<br>
+mrogers wrote us an event-based simulator for evaluating proposed changes to Freenet routing, load balancing and data storage. In the past we have generally deployed changes in a fairly haphazard way, and it has often not been clear whether a particular change has been beneficial. Since we started work on 0.7 we have been more systematic, but the simulations we have built were not able to simulate many of the more complex processes, such as load balancing / load limiting. mrogers has produced a detailed simulation which has already yielded insights into congestion control, has confirmed some of our recent decisions on load balancing, and is being used to prototype a new load management algorithm which should radically improve Freenet's performance.</li>
+<li><b>Installer improvements and related stuff</b> (<i>nextgens</i> - Florent Daigni&egrave;re)<br>
+Over the summer nextgens, an experienced freenet developer already, worked on what was most urgently needed for Freenet, and which he had most expertise in, but wasn't necessarily most interesting. His work related mostly to deployment: The installer, the uninstaller, the updating system, the mirror network, native libraries and general bugfixing. All of this has been deployed already and has enhanced Freenet's usability. He also did some work on getting Freenet to work on free JVMs, which has yielded some results.</li>
+<li><b>Thaw</b> (<i>jflesch</i> - J&eacute;r&ocirc;me Flesch)<br>
+Over the summer jflesch developed Thaw (a filesharing utility and upload/download manager). Thaw's goals were firstly to provide a user-friendly, cross platform interface to the built-in Freenet download manager engine (replacing an older Windows-only tool called FUQID), and secondly to evolve into a complete file sharing application via sharing file indexes. The first goal has been 100% fulfilled and Thaw is now the preferred tool. The second goal has been partially achieved; Thaw has working functionality for searching file indexes, but this is not yet used very much.</li>
 </ul><p>All in all, a great success, many thanks to Google!</p>
+
 <b>17th August, 2006 - John Gilmore donates US$15,000 to the Freenet Project</b> [<a href="http://digg.com/software/John_Gilmore_donates_US_15_000_to_the_Freenet_Project">digg it</a>]<br>
 John Gilmore, one of the founders of the Electronic Frontier Foundation, the Cypherpunks mailing list, and Cygnus Solutions, has donated US$15,000 to the Freenet Project, to support the ongoing development of the Freenet software.
 <p>
