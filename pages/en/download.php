12e93245274eccaf09c4f8185f0acd544d3bae3b
Florent Daignière <nextgens@freenetproject.org>
2007-07-14 11:04:01 +0000

    website: update jvm requirements
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@14099 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/download.php
+++ pages/en/download.php
@@ -38,7 +38,7 @@ If the installer doesn't work (something seriously wrong) then please let us kno
 
 <h3>Unix, and Linux</h3>
 You need to have a recent <b>Java Runtime Environment</b> (JRE). We have experienced best results with Sun's Java Runtime Environment which can be obtained from <a href="http://www.java.com/">http://www.java.com/</a>. <BR>
-Java version 1.4.1 and later will work. However, be aware that there are applet security vulnerabilities in all versions prior to Java 1.5 update 4. Generally, we recommend using Java 1.5.
+Java version 1.4.2 and later will work. However, be aware that there are applet security vulnerabilities in all versions prior to Java 1.5 update 4. Generally, we recommend using latest.
 
 <p>Type on a command line : 
 <pre>
