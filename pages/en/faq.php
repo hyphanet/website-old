52e33e9390de608fe3a03476a7fb375b8ab9e1b5
Matthew Toseland <toad@amphibian.dyndns.org>
2007-12-11 15:49:30 +0000

    against them, format
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@16485 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/faq.php
+++ pages/en/faq.php
@@ -414,9 +414,9 @@ traffic analysis, compromising any given majority of the nodes, etc.)
 that these were designed to counter would probably be successful in
 identifying someone making requests on Freenet.</p>
 <p>Your identity is always visible to the nodes you are actually connected to.
-They know what keys your node sends requests for: your anonymity consists in a
-limited level of plausible deniability, that maybe you are forwarding these 
-requests for some other node. Unfortunately, your peers can do 
+They know what keys your node sends requests for: your anonymity against them
+consists in a limited level of plausible deniability, that maybe you are 
+forwarding these requests for some other node. Unfortunately, your peers can do 
 <a href="http://wiki.freenetproject.org/CorrelationAttacks">correlation 
 attacks</a> to figure out which requests are from you and which requests are from
 your peers or somebody else. One key thing you can do to protect yourself is
