7491cc0f8d21610bc2d7f8fdad512880a02f25ea
Florent Daignière <nextgens@freenetproject.org>
2007-12-16 20:46:27 +0000

    website: keeping outdated files here is for the least ... confusing
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@16605 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/donatethanks.php
+++
@@ -1,6 +0,0 @@
-<h3>Thanks for your donation!</h3>
-We have received your generous donation.  It will be used at the
-direction of the Freenet Board to advance the interests of the
-Freenet Project.  If you have any questions about how your donation
-will be used please contact <a href="mailto:ian@locut.us">Ian Clarke</a>.
-
