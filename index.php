cf4309dbb9fde340f882e49440c51283ccd444f6
David Sowder <freenet@david.sowder.com>
2007-12-17 01:53:01 +0000

    Fix a closing P tag order problem
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@16645 67a373e5-eb02-0410-a15c-ee090a768436

--- index.php
+++ index.php
@@ -79,9 +79,9 @@ include 'includes/common.inc.php';
 <!--<p><? //echo otherLanguages(); ?></p>-->
 <p>
 This website is licensed under the <a href="http://www.gnu.org/licenses/fdl.html">GNU Free Documentation License</a>
-</div>
-</div>
 </p>
+</div>
+</div>
 	<div class="hideit">Send spam to <a href="mailto:catchme@freenetproject.org">catchme@freenetproject.org</a> ! :)</div>
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 </script><script type="text/javascript">_uacct = "UA-354970-1";urchinTracker();</script>
