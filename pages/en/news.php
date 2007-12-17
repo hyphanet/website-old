d5fb24b5823f692a6c8c0bbac4d76e4687acf64a
David Sowder <freenet@david.sowder.com>
2007-12-17 01:58:31 +0000

    Fix some UTF-8 characters in our ISO-8859-1 encoded page.  Fix a closing P tag location problem (also: UL can't be nested in a P)
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@16646 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/news.php
+++ pages/en/news.php
@@ -19,7 +19,7 @@ our <a href="/donate.html">donations page</a>.
 John Pozadzides, founder of <a href="http://HTMLHelp.com">HTMLHelp.com</a> and former Vice President of Sales for <a href="http://savvis.net">SAVVIS Communications</a>, has donated $10,000 to the Freenet Project to fund ongoing
 development of the Freenet software.  John says:
 <blockquote>
-More than a decade ago when I founded HTMLHelp.com I had a vision of the Internet as the “great equalizer”. I believed that it would free us from the content filtering imposed by mega-media companies and governments. It turns out this has been true for most of us - but not all.
+More than a decade ago when I founded HTMLHelp.com I had a vision of the Internet as the &#8220;great equalizer&#8221;. I believed that it would free us from the content filtering imposed by mega-media companies and governments. It turns out this has been true for most of us - but not all.
 <p/>
 I view supporting the Freenet Project as a logical extension of that original dream, this time to include people who were previously unable to freely participate.
 </blockquote>
@@ -32,14 +32,14 @@ You can read John's blog entry announcing this donation <a href="http://onemansb
 <p><b>12 March, 2007 - Google Summer of Code update</b></p>
 <p>We have selected 6 students to work for us over the summer, paid for by 
 <a href="http://code.google.com/soc/freenet/about.html">Google Summer of Code</a>.<p>
-<p>These are:
+<p>These are:</p>
 <ul>
 <li><b>Swati Goyal</b> will be working on improving searching in Freenet.</li>
 <li><b>Fr&eacute;d&eacute;ric Rechtenstein</b> will be building us a blogging plugin.</li>
 <li><b>Alberto Bacchelli</b> will be building a test framework and many unit tests.</li>
 <li><b>Vilhelm Verendel</b> will be working on simulating the growth of the network.</li>
 <li><b>Srivatsan</b> will be working on improving Freenet's connection encryption and possibly on darknet introductions.</li>
-<li><b>Mladen Kolar</b> will be building a definitive C/C++ library for the <a href="http://wiki.freenetproject.org/FreenetFCPSpec2Point0">Freenet Client Protocol</a>.</li></p>
+<li><b>Mladen Kolar</b> will be building a definitive C/C++ library for the <a href="http://wiki.freenetproject.org/FreenetFCPSpec2Point0">Freenet Client Protocol</a>.</li>
 </ul><p>Congratulations to all of these students. Last year's Summer of Code yielded results including
 <a href="http://wiki.freenetproject.org/Thaw">Thaw</a>, <a href="http://wiki.freenetproject.org/Freemail">Freemail</a>,
 new low-level simulations and a great deal of work on the installer, packaging and general bugfixing. Even better,
