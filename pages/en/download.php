6ce3c3e458553907948d4172a8ab8cd3a1d223b3
David Sowder <freenet@david.sowder.com>
2007-12-17 02:33:35 +0000

    More HTML validation-related tweaks, mostly of P tags and nesting PRE tags in them
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@16648 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/download.php
+++ pages/en/download.php
@@ -10,9 +10,8 @@ nodes for Freenet to be usable, and performance will likely improve over
 time after that. You will get best performance if you can leave your node
 running 24x7.</p>
 
-<p> <i>If you have a firewall or a NAT box click <a
-href="/faq.html#firewall"><b>here</b></a>
-for some info.</i> <BR> &nbsp;<BR>
+<p><i>If you have a firewall or a NAT box click <a href="/faq.html#firewall"><b>here</b></a>
+for some info.</i></p>
 
 <div id="jws">
 <h2>Installation Instructions</h2>
@@ -53,26 +52,24 @@ If this doesn't work, try the linux instructions below.
 You need to have a recent <b>Java Runtime Environment</b> (JRE). We have experienced best results with Sun's Java Runtime Environment which can be obtained from <a href="http://www.java.com/">http://www.java.com/</a>. <BR>
 Java version 1.4.2 and later will work. However, be aware that there are applet security vulnerabilities in all versions prior to Java 1.5 update 4. Generally, we recommend using latest.
 
-<p>Type on a command line : 
+<p>Type on a command line :</p>
 <pre>
-wget <a href="http://downloads.freenetproject.org/alpha/installer/new_installer.jar" alt="new_installer.jar">http://downloads.freenetproject.org/alpha/installer/new_installer.jar</a>
+wget <a href="http://downloads.freenetproject.org/alpha/installer/new_installer.jar">http://downloads.freenetproject.org/alpha/installer/new_installer.jar</a>
 java -jar new_installer.jar
 </pre>
-</p>
 
-<p> Or do the following on a headless system:
+<p>Or do the following on a headless system:</p>
 <pre>
-wget <a href="http://downloads.freenetproject.org/alpha/installer/freenet07.tar.gz" alt="freenet07.tar.gz">http://downloads.freenetproject.org/alpha/installer/freenet07.tar.gz</a>
+wget <a href="http://downloads.freenetproject.org/alpha/installer/freenet07.tar.gz">http://downloads.freenetproject.org/alpha/installer/freenet07.tar.gz</a>
 cat freenet07.tar.gz | gzip -d | tar xv
 cd freenet
 ./bin/1run.sh
 ./run.sh start
 </pre>
-</p>
 <p>You might find the <a href="http://www.minihowto.org/freenet_howto/Freenet%2520-%2520a%2520very%2520short%2520howto_3077.html">mini-howto</a> useful (it might be of interest to Windows users too).</p>
 </div>
 
-<script language="javascript"><!--
+<script type="text/javascript"><!--
 	// Try to detect if Sun Java 1.4.2 or higher is installed
 	var Java = PluginDetect.isMinVersion('Java', '1,4,2');
 
@@ -104,9 +101,10 @@ cd freenet
 <h3>After installing</h3>
 
 <P>Afer you start Freenet, wait a few seconds for it to start up (on a
-slow computer, you may need to wait about 30 seconds), and visit
-<pre>http://127.0.0.1:8888/</pre> in your web browser to access Freenet's user
-interface. <BR> &nbsp;<BR> </P>
+slow computer, you may need to wait about 30 seconds), and visit</p>
+<pre>http://127.0.0.1:8888/</pre>
+<p>in your web browser to access Freenet's user
+interface.</p>
 
 <h3>Offline installation</h3>
 Some people might encounter difficulties connecting to our server because of censorship. We do have an offline version of the installer available : <a href="http://downloads.freenetproject.org/alpha/installer/">Freenet 0.7 offline installer</a>. You shouldn't use it unless you have to (it's bigger to download and might not be as up to date as the online one).
@@ -114,8 +112,9 @@ Some people might encounter difficulties connecting to our server because of cen
 <h3>So it's running, what do I do?</h3>
 
 <p>You need to get connected.  If you know anyone running Freenet, ask them 
-for their reference, and give them yours.  If you visit <pre>http://127.0.0.1:8888/friends/</pre> 
-in your browser, you can see your reference at the bottom and you can add
+for their reference, and give them yours.  If you visit</p>
+<pre>http://127.0.0.1:8888/friends/</pre>
+<p>in your browser, you can see your reference at the bottom and you can add
 other people's references, either by cutting and pasting the reference
 itself, or by pointing Freenet to a URL or a file containing their
 reference.</p>
@@ -128,10 +127,10 @@ start working by itself.</p>
 
 <h3>So I'm connected, what do I do?</h3>
 
-You can find links to some pages in the index on the FProxy homepage at:
+<p>You can find links to some pages in the index on the FProxy homepage at:</p>
 <pre>http://127.0.0.1:8888/</pre>
-You can find links to some <a href="http://wiki.freenetproject.org/FreenetZeroPointSevenApps">third party 
-applications for freenet</a>.
+<p>You can find links to some <a href="http://wiki.freenetproject.org/FreenetZeroPointSevenApps">third party 
+applications for freenet</a>.</p>
 
 <h3>It doesn't work, now what?</h3>
 <p>If you have problems installing or running Freenet, please contact us on
@@ -141,11 +140,12 @@ applications for freenet</a>.
 on <a href="irc://irc.freenode.net/freenet">#freenet</a> on irc.freenode.net.</p>
 	
 <h3>Hardware requirements</h3>
+<p>
 <b>Minimum:</b> 400MHz Pentium 2, with at least 192MB of RAM.<br>
 <b>Recommended:</b> 1GHz or more processor with 256MB
 <!-- decreasing agaisnt my will :(-->
-RAM or more (especially if using Windows XP). <BR>
-&nbsp;<BR>
+RAM or more (especially if using Windows XP).
+</p>
 <h3>Upgrading</h3>
 <p>Freenet provides now an update-over-freenet mechanism ; We recommend people to use it in favour of the updating scripts. However, you may also update your node manually:</p>
 <ul><li>Windows users can upgrade to the latest-stable Freenet release by clicking on "update.cmd" in the Freenet directory.</li>
@@ -155,5 +155,5 @@ RAM or more (especially if using Windows XP). <BR>
 You can obtain the latest source code from SVN from <a href="http://freenet.googlecode.com/svn/trunk/freenet/">http://freenet.googlecode.com/svn/trunk/freenet/</a>.
 <BR>
 <p>
-<small><b>Note: </b> You can still find the download page for Freenet 0.5 <a href="/download-old.html">here</a>, however 0.5 is no longer officially maintained or supported and 0.7 has many radical improvements not least being that it is significantly faster.</small>
+<small><b>Note:</b> You can still find the download page for Freenet 0.5 <a href="/download-old.html">here</a>, however 0.5 is no longer officially maintained or supported and 0.7 has many radical improvements not least being that it is significantly faster.</small>
 </p>
