aa5e639e7f491020a9d0260d258bcea9a0959b2a
Dave Baker <dbkr@freenetproject.org>
2008-01-24 22:32:05 +0000

    A few tweaks to the Freemail web page, most notably asserting that people need to put one of their Freemail addresses in the their mail client (although you have to wonder how they figured out regular email...)
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@17251 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/freemail.php
+++ pages/en/freemail.php
@@ -1,7 +1,7 @@
 <h2>Freemail</h2>
 
 <p>Freemail is an email system for Freenet. It allows you to send anonymous emails to other Freenet users using your standard email
-client. It is currently not bundled in the Freenet installer, and has to be downloaded separately.<p>
+client. It is currently not bundled in the Freenet installer, and has to be downloaded separately. You can also try the Freemail plugin by selecting it from the list on the plugins page of your Freenet node. Beware, though, that the plugin is still in an early stage of development, and users should be confident about looking in log files and reporting problems if they find them.<p>
 
 <p>You can download the latest pre-compiled version from <a href="http://downloads.freenetproject.org/alpha/plugins/Freemail/">http://downloads.freenetproject.org/alpha/plugins/Freemail/</a></p>
 
@@ -43,7 +43,7 @@ You will probably want to add an account by running Freemail with arguments --ne
 </pre>
 
 <p>
-So do what it says. The username you create here is used to authenticate towards the Freemail-service and is only seen by you, 
+So do what it says. The username you create here is used to authenticate to the Freemail-service and is only seen by you, 
 it isn't part of your freemail address:
 </p>
 <pre>
@@ -70,7 +70,7 @@ The next step is to create a password for your account. The syntax to create a p
 java -jar Freemail.jar --passwd [username] [password]
 </pre>
 <p>
-To create the password <span style="font-weight:bold;">secretpass</span> for the user <span style="font-weight:bold;">john</span>, write:
+To create the password <span style="font-weight:bold;">secretpass</span> for the user <span style="font-weight:bold;">john</span>, type:
 </p>
 <pre>
 java -jar Freemail.jar --passwd john secretpass
@@ -150,10 +150,10 @@ If you use Thunderbird as your email client:
 <li> From the Edit menu, select Account Settings.</li>
 <li> Click the Add Account... button.</li>
 <li>Select Email Account and click Next.</li>
-<li>Type in the name and email address you want to use e.g. John and me@jsmith.freemail, and click Next.</li>
+<li>Type in the name and either the long Freemail address you were given, or your short address if you created one. Check this carefully as an incorrect address here will not only mean people won't be able to reply to you, but your messages will appear as 'Spoofed'. Once you've done this, click Next.</li>
 <li>Set the type of incoming server to IMAP and the incoming server name to localhost. Then click Next.</li>
-<li>For the Incoming User Name, use the original account name you used (john in this example), which may not be the same as your email 
-address name. Click Next.</li>
+<li>For the Incoming User Name, use the account name you chose earlier ('john' in this example), which may not be the same as your email 
+address. Click Next.</li>
 <li>Enter an arbitrary Account Name and click Next and then Finish.</li>
 <li>Nw we have to change the IMAP port number from the default: On the left panel click on Server Settings under the new account. 
 Change the Port to 3143 from the default of 143.</li>
@@ -167,10 +167,13 @@ Now you should be able to read incoming freemails. To send out emails:
 <li>You probably already have at least one SMTP server set up already for your normal emails. So click on the Add... button to create 
 one specially for freemails.</li>
 <li>Under Description put anything you want - Freemail might be a good choice. Set Server Name to localhost and change Port to 3025. 
-Make sure Use name and password is checked and put your original account name as the User Name (john in our example). Use secure 
-connection should be set to No (don't worry, it's only the local connection that is unencrypted). Click OK.</li>
+Make sure 'Use name and password' is checked and put your original account name as the User Name ('john' in our example). 'Use secure 
+connection' should be set to No (don't worry, it's only the local connection that is unencrypted). Click OK.</li>
 <li>The final thing is to set your new Freemail account to use this outgoing server instead of the default one. So in the left panel 
 find and click on the top line of the new incoming mail account you added. In our example this would be something like 
 me@jsmith.freemail. There should be a drop-down box called Outgoing Server (SMTP). Set this to the new setup we just added: 
 something like Freemail - localhost. And click OK.</li>
 </ol>
+<p>
+Congratulations - you're now set up to send and receive email over Freenet!
+</p>
