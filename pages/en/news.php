33211d333fa28d6d1551351e2c366fc8a0c4592a
Matthew Toseland <toad@amphibian.dyndns.org>
2008-01-29 18:12:09 +0000

    Announce Freenet 0.7 alpha 2.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@17397 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/news.php
+++ pages/en/news.php
@@ -15,6 +15,33 @@ support the Freenet Project, you can make a convenient donation through
 our <a href="/donate.html">donations page</a>.
 -->
 <h3>News</h3>
+<p><b>29th Jan, 2008 - Freenet 0.7.0a2 alphatest released</b></p>
+<p>Freenet version 0.7a2 is now available for public testing.</p>
+<p>Freenet is a global peer-to-peer network designed to allow users to publish 
+and consume information without fear of censorship.  To use it, you must download 
+the Freenet software, available for Windows, Mac, Linux and other operating systems.  
+Your computer will then form part of a global, decentralized P2P network, and you 
+will be able to publish and consume information anonymously, either through your 
+web browser, or through a variety of third party applications, such as Frost (see 
+<a href="http://jtcfrost.sf.net/">http://jtcfrost.sf.net/</a>).</p>
+<p>Since the first alpha there have been vast numbers of changes, in particular 
+significant improvements in user friendliness.  For example, it is no-longer 
+necessary to find other people to connect to, Freenet can find other people for you. 
+However, you can still connect to your friends for maximum security. It is also a 
+good deal faster, and many bugs have been fixed.</p>
+<p>There have been many many other changes, and we will release a feature complete 
+beta followed by 0.7.0 itself in the coming months (we hope to have regular point 
+releases after that). But we thought it would be best to get some wider testing at 
+this point. There is more to do before 0.7.0, and one or two features which need to 
+be completed before that.</p>
+<p>Freenet 0.7a2 can be downloaded from 
+<a href="http://freenetproject.org/download.html">here</a>.</p>
+<p>This release would not have been possible without the release of numerous 
+volunteers, and Matthew Toseland, Freenet's full time developer. Matthew's work is 
+funded through donations via our website (as well as a few larger sponsors from time 
+to time), we ask that anyone who can help us to ensure Matthew's continued 
+employment by visiting our donations page and making a contribution at 
+<a href="/donate.html">the donation page</a>.</p>
 <p><b>15th May, 2007 - John Pozadzides donates $10,000 to Freenet Project</b></p>
 <p>
 John Pozadzides, founder of <a href="http://HTMLHelp.com">HTMLHelp.com</a> and former Vice President of Sales for <a href="http://savvis.net">SAVVIS Communications</a>, has donated $10,000 to the Freenet Project to fund ongoing
