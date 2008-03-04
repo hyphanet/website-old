6131650591c5815696ff815366f5f9c503440b82
Florent Daignière <nextgens@freenetproject.org>
2008-03-04 03:04:27 +0000

    website: add a link to the svn
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@18324 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/developer.php
+++ pages/en/developer.php
@@ -1,1 +1,9 @@
 <h2>Developer</h2>
+
+<h3>Source Code</h3>
+<p>You can obtain the latest source code from SVN:</p>
+<pre>svn co --ignore-externals http://freenet.googlecode.com/svn/trunk/freenet/</pre>
+<br>
+<h3>Build Instructions</h3>
+<p>To build the source code you will need <a href="http://ant.apache.org/">Apache ANT</a>.
+<br>
