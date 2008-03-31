8c1290d7569a2cc2fac994fe893e2cecec597058
Michael Tänzer <NEOatNHNG@users.sourceforge.net>
2008-03-31 17:09:53 +0000

    Update the German version of the news section
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@18874 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/de/news.php
+++ pages/de/news.php
@@ -15,10 +15,49 @@ support the Freenet Project, you can make a convenient donation through
 our <a href="/donate.html">donations page</a>.
 -->
 <h3>Neuigkeiten</h3>
-<!-- Translation-Disclaimer - do not remove. Modify date of Translation! -->
-<p>Die deutsche &Uuml;bersetzung der Neuigkeiten (Stand vom 30.3.2008)
+<!-- Translation disclaimer - do not remove. Modify date of translation! -->
+<p>Die deutsche &Uuml;bersetzung der Neuigkeiten (Stand vom 31.3.2008)
 ist evtl. veraltet, die <a href="/index.php?page=news&lang=en">Englische Version</a>
 sollte aber auf dem neusten Stand sein.</p>
+<<!-- End of translation disclaimer -->
+
+<p><b>31. März 2008 - Freenet 0.7.0 Release Candidate 1 veröffentlicht</b></p>
+<p>Die Freenet-Version 0.7.0 Release Candidate 1 steht nun zum öffentlichen Test
+zur Verfügung.</p>
+<p>
+Freenet ist ein weltweites Peer-to-Peer Netzwerk, entworfen um es den Benutzern
+zu ermöglichen Informationen zu lesen und zu veröffentlichen ohne Zensur
+befürchten zu müssen. Um es zu benutzen, müssen Sie die Freenet-Software
+herunterladen, diese ist für Windows, MacOS, Linux und andere Betriebssysteme
+verfügbar. Sobald Sie Freenet installiert und gestartet haben, wird Ihr Computer
+einem weltweiten, dezentralen P2P Netzwerk beitreten. Sie werden in der Lage
+sein Informationen anonym zu lesen und zu veröffentlichen, entweder über Ihren
+Web-Browser oder über eine Vielfalt von Programmen von Drittanbietern.
+</p>
+<p>
+Freenet 0.7 ist eine komplett neu geschriebene Version von Freenet. Das
+Hauptmerkmal von 0.7, welches der Benutzer zu Gesicht bekommt, ist die
+Fähigkeit, Freenet in einem &bdquo;Darknet&ldquo;-Modus zu betreiben, in dem Ihr
+Freenet-Knoten mit anderen Freenet-Benutzern, denen Sie vertrauen, kommuniziert.
+Dies erschwert es einem Angreifer erheblich, herauszufinden, dass Sie Freenet
+benutzen, ganz zu schweigen davon, was Sie damit machen. 0.7 enthält außerdem
+wesentliche Verbesserungen der Sicherheit und Geschwindigkeit.
+</p>
+<p>Hier kann Freenet 0.7 RC1 heruntergeladen werden:</p>
+<p><a href="http://freenetproject.org/download.html">http://freenetproject.org/download.html</a></p>
+<p>Diese Veröffentlichung wäre ohne die zahlreichen Freiwilligen und Matthew
+Toseland, Freenets Vollzeit-Entwickler, nicht möglich gewesen. Matthews Arbeit
+wird durch Spenden über unsere Webseite finanziert (und einigen größeren Spenden
+von Zeit zu Zeit). Wir rufen jeden, der uns helfen kann, dazu auf Matthews
+Anstellung sicherzustellen. Besuchen Sie unsere Spenden-Seite und spenden Sie
+auf:</p>
+<p><a href="http://freenetproject.org/donate.html">http://freenetproject.org/donate.html</a></p>
+<p>Das Freenet-Projekt nimmt dieses Jahr auch am &bdquo;Google Summer of
+Code&ldquo;-Projekt teil. Wenn Sie daran interessiert sind mitzumachen, besuchen
+Sie so schnell wie möglich die &bdquo;Summer of Code&ldquo;-Seite:</p>
+<p><a href="http://code.google.com/soc/2008/">http://code.google.com/soc/2008/</a></p>
+
+
 <p><b>17. M&auml;rz 2008 - Freenet im Google Summer of Code 2008 aufgenommen</b></p>
 <p>Google hat uns zum dritten Mal in ihrem
 <a href="http://code.google.com/soc/2008/">Summer of Code</a> aufgenommen. Die
@@ -33,38 +72,3 @@ einen Blick auf die
 (sie enth&auml;lt auch eine Liste mit Ideen, aber tun Sie sich keinen Zwang an etwas
 vorzuschlagen das dort nicht aufgelistet ist) und
 <a href="mailto:devl@freenetproject.org">kontaktieren Sie uns</a>.</p>
-
-<p><b>29. Januar 2008 - Freenet 0.7.0a2 alphatest ver&ouml;ffentlicht</b></p>
-<p>Freenet Version 0.7a2 steht nun zum &ouml;ffentlichen Test zur Verf&uuml;gung</p>
-<p>Freenet ist ein weltweites Peer-to-Peer Netzwerk, entworfen um es den
-Benutzern zu erm&ouml;glichen Informationen zu lesen und zu ver&ouml;ffentlichen
-ohne Zensur bef&uuml;rchten zu m&uuml;ssen. Um es zu benutzen, m&uuml;ssen Sie die
-Freenet-Software herunterladen, diese ist f&uuml;r Windows, MacOS, Linux und andere
-Betriebssysteme verf&uuml;gbar. Ihr Computer wird damit zu einem Teil eines
-weltweiten, dezentralen P2P Netzwerks und Sie werden in der Lage sein
-Informationen anonym zu lesen und zu ver&ouml;ffentlichen, entweder &uuml;ber Ihren
-Web-Browser oder &uuml;ber eine Vielfalt von Programmen von Drittanbietern, so wie
-Frost (siehe <a href="http://jtcfrost.sf.net/">http://jtcfrost.sf.net/</a>).</p>
-<p>Seit der ersten Alpha-Version sind eine gro&szlig;e Anzahl von &Auml;nderungen
-vorgenommen worden, im speziellen erhebliche Verbesserungen der
-Benutzerfreundlichkeit. Zum Beispiel ist es nicht mehr n&ouml;tig andere Leute zu
-finden mit denen man sich verbinden kann, Freenet kann andere Leute f&uuml;r Sie
-finden. Sie k&ouml;nnen sich jedoch immenoch mit Ihren Freunden verbinden um
-maximale Sicherheit zu erlangen. Weiterhin ist Freenet nun eine ganze Ecke
-schneller und viele Fehler wurden behoben.</p>
-<p>Es wurden zahlreiche weitere Verbesserungen vorgenommen und wir werden in den
-kommenden Monaten eine funktionskomplette Beta-Version gefolgt von 0.7.0 selbst
-ver&ouml;ffentlichen (wir hoffen danach regelm&auml;&szlig;ige
-Ver&ouml;ffentlichungen anbieten zu k&ouml;nnen). Aber wir dachten es w&auml;re
-zu diesem Zeitpunkt das Beste einen gr&ouml;&szlig;eren Test zu machen. Es gibt
-vor 0.7.0 noch mehr zu tun und ein oder zwei Funktionen welche bis dahin fertig
-gestellt werden m&uuml;ssen.</p>
-<p>Freenet 0.7a2 kann <a href="http://freenetproject.org/download.html">hier</a>
-heruntergeladen werden.</p>
-<p>Diese Ver&ouml;ffentlichung w&auml;re ohne die zahlreichen Freiwilligen und Matthew
-Toseland, Freenets Vollzeit-Entwickler, nicht m&ouml;glich gewesen. Matthews Arbeit
-wird durch Spenden &uuml;ber unsere Webseite finanziert (und einigen
-gr&ouml;&szlig;eren Spenden von Zeit zu Zeit), wir rufen jeden der uns helfen
-kann dazu auf Matthews Anstellung durch den Besuch unserer
-<a href="/donate.html">Spenden-Seite</a> (und einer Zuwendung) sicher zu
-stellen.</p>
