ad4e0c838a2554557cfebecc25c4f905b8b53b4d
Florent Daignière <nextgens@freenetproject.org>
2008-04-01 03:20:14 +0000

    website: re-enable google-analytics it's the best opportunity we have to get *usefull* stats
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@18884 67a373e5-eb02-0410-a15c-ee090a768436

--- index.php
+++ index.php
@@ -83,5 +83,7 @@ This website is licensed under the <a href="http://www.gnu.org/licenses/fdl.html
 </div>
 </div>
 	<div class="hideit">Send spam to <a href="mailto:catchme@freenetproject.org">catchme@freenetproject.org</a> ! :)</div>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script><script type="text/javascript">_uacct = "UA-354970-1";urchinTracker();</script>
 </body>
 </html>
