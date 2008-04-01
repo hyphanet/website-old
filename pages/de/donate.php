f42750b8e4b8d59fbaccb90dd326d3f927fbd917
Michael Tänzer <NEOatNHNG@users.sourceforge.net>
2008-04-01 14:48:22 +0000

    Do the paypal-fix on the german website too and translate donate.php
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@18890 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ pages/de/donate.php
@@ -0,0 +1,173 @@
+<h2>Spenden</h2>
+
+<h4>Warum braucht das Freenet-Projekt Spenden?</h4>
+<p>
+Obwohl zahlreiche Menschen seit seinem Bestehen ihre Zeit und ihre Fähigkeiten
+in das Freenet-Projekt eingebracht haben, ist das Freenet-Projekt auf Ihre
+Spenden angewiesen um die Entwickler zu bezahlen, die vollzeit an diesem immer
+komplexer werdenden Projekt arbeiten und um die Hardware- (z.B. die
+Entwicklungs-Computer des Projekts) und administrativen Kosten (z.B.
+Domain-Registrierungen) zu decken. Ein weiteres Ziel ist es, in der Lage zu sein
+eine Kapital zur rechtlichen Verteidigung aufzubauen um dabei zu helfen die
+Zukunft der Entwicklung von Freenet sicherzustellen.<br>
+Alle Beteiligten sind sehr an der Zukunft des Freenet-Projekts interessiert und
+Sie können sicher sein, dass Spenden weise eingesetzt wurden und werden. Wir
+sind dankbar für jede Unterstützung.
+</p>
+
+<h4>Wie kann ich spenden?</h4>
+Sie haben vier Möglichkeiten:
+<ul>
+<li>Sie können für <b>wiederkehrende Zahlungen</b> von 5$, 10$ oder 20$ pro
+Monat ein ein Mitglied des Freenet-Projekts werden. Der Vorteil hiervon ist,
+dass es dem Projekt stabilere und verlässlichere Bezüge bietet, was es
+erleichtert potenzielle Entwickler langfristig zu engagieren - zurzeit ist es
+schwierig vorherzusagen ob wir einen Entwickler im nächsten Monat noch bezahlen
+können, obwohl wir bis jetzt Glück hatten. Sie können ein Mitglied werden indem
+Sie diesen Knopf anklicken (Sie brauchen ein
+<a href="http://www.paypal.de/">PayPal</a>-Konto):
+
+<center>
+<table><tr><td bgcolor="#DDDDFF" style="{padding:4px; border:1px solid silver}">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_xclick-subscriptions">
+<input type="hidden" name="business" value="ian@freenetproject.org">
+<input type="hidden" name="item_name" value="Freenet Project Membership">
+<input type="hidden" name="return" value="http://freenetproject.org/donatethanks.html">
+<input type="hidden" name="cancel_return" value="http://freenetproject.org/donate.html">
+<input type="hidden" name="no_shipping" value="1">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="currency_code" value="USD">
+Betrag (US-Dollar):
+<p>
+<span style="{border:1px solid black}">&nbsp;20$ <input type="radio" name="a3" value="20.00"></span>&nbsp; 
+<span style="{border:1px solid black}">&nbsp;10$ <input type="radio" name="a3" checked="true" 
+value="10.00"></span>&nbsp; 
+<span style="{border:1px solid black}">&nbsp;5$ <input type="radio" name="a3" value="5.00"></span>&nbsp;
+</p>
+<input type="image" src="https://www.paypal.com/images/x-click-but20.gif" border="0" name="submit"
+alt="Regelmäßig per PayPal spenden">
+<input type="hidden" name="p3" value="1">
+<input type="hidden" name="t3" value="M">
+<input type="hidden" name="src" value="1">
+<input type="hidden" name="sra" value="1">
+</form>
+</td></tr></table>
+</center>
+<p>
+<hr width="90%">
+</p>
+</li>
+
+<li>Sie können über Google Checkout spenden:<br>
+<center>
+<form action="https://checkout.google.com/cws/v2/Merchant/828420057535417/checkout" id="BB_BuyButtonForm930897499" method="post" name="BB_BuyButtonForm930897499">
+    <input name="o" type="hidden" value="shoppingcart"/>
+    <input name="signature" type="hidden" value="Pl1x6t9lcHiNBRMEBXo9AXOyEl8="/>
+    <table cellpadding="5" cellspacing="0" width="1%">
+        <tr>
+            <td align="right" width="1%">
+                <select name="cart" onChange="document.getElementById('BB_BuyButtonForm930897499').signature.value=document.getElementById('BB_BuyButtonForm930897499').itemSignature[this.selectedIndex].value">
+                    <option value="PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4NCjxjaGVja291dC1zaG9wcGluZy1jYXJ0IHhtbG5zPSJodHRwOi8vY2hlY2tvdXQuZ29vZ2xlLmNvbS9zY2hlbWEvMiI+DQogIDxzaG9wcGluZy1jYXJ0Pg0KICAgIDxpdGVtcz4NCiAgICAgIDxpdGVtPg0KICAgICAgICA8cXVhbnRpdHk+MTwvcXVhbnRpdHk+DQogICAgICAgIDx1bml0LXByaWNlIGN1cnJlbmN5PSJVU0QiPjIwLjA8L3VuaXQtcHJpY2U+DQogICAgICAgIDxpdGVtLW5hbWU+QnJvbnplPC9pdGVtLW5hbWU+DQogICAgICAgIDxpdGVtLWRlc2NyaXB0aW9uPkEgJDIwIGRvbmF0aW9uIHRvIHRoZSBGcmVlbmV0IFByb2plY3QuPC9pdGVtLWRlc2NyaXB0aW9uPg0KICAgICAgPC9pdGVtPg0KICAgIDwvaXRlbXM+DQogIDwvc2hvcHBpbmctY2FydD4NCiAgPGNoZWNrb3V0LWZsb3ctc3VwcG9ydD4NCiAgICA8bWVyY2hhbnQtY2hlY2tvdXQtZmxvdy1zdXBwb3J0IC8+DQogIDwvY2hlY2tvdXQtZmxvdy1zdXBwb3J0Pg0KPC9jaGVja291dC1zaG9wcGluZy1jYXJ0Pg0KDQo=">20$ - Bronze</option>
+                    <option value="PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4NCjxjaGVja291dC1zaG9wcGluZy1jYXJ0IHhtbG5zPSJodHRwOi8vY2hlY2tvdXQuZ29vZ2xlLmNvbS9zY2hlbWEvMiI+DQogIDxzaG9wcGluZy1jYXJ0Pg0KICAgIDxpdGVtcz4NCiAgICAgIDxpdGVtPg0KICAgICAgICA8cXVhbnRpdHk+MTwvcXVhbnRpdHk+DQogICAgICAgIDx1bml0LXByaWNlIGN1cnJlbmN5PSJVU0QiPjUwLjA8L3VuaXQtcHJpY2U+DQogICAgICAgIDxpdGVtLW5hbWU+U2lsdmVyPC9pdGVtLW5hbWU+DQogICAgICAgIDxpdGVtLWRlc2NyaXB0aW9uPkEgJDUwIGRvbmF0aW9uIHRvIHRoZSBGcmVlbmV0IFByb2plY3QuPC9pdGVtLWRlc2NyaXB0aW9uPg0KICAgICAgPC9pdGVtPg0KICAgIDwvaXRlbXM+DQogIDwvc2hvcHBpbmctY2FydD4NCiAgPGNoZWNrb3V0LWZsb3ctc3VwcG9ydD4NCiAgICA8bWVyY2hhbnQtY2hlY2tvdXQtZmxvdy1zdXBwb3J0IC8+DQogIDwvY2hlY2tvdXQtZmxvdy1zdXBwb3J0Pg0KPC9jaGVja291dC1zaG9wcGluZy1jYXJ0Pg0KDQo=">50$ - Silber</option>
+                    <option value="PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4NCjxjaGVja291dC1zaG9wcGluZy1jYXJ0IHhtbG5zPSJodHRwOi8vY2hlY2tvdXQuZ29vZ2xlLmNvbS9zY2hlbWEvMiI+DQogIDxzaG9wcGluZy1jYXJ0Pg0KICAgIDxpdGVtcz4NCiAgICAgIDxpdGVtPg0KICAgICAgICA8cXVhbnRpdHk+MTwvcXVhbnRpdHk+DQogICAgICAgIDx1bml0LXByaWNlIGN1cnJlbmN5PSJVU0QiPjEwMC4wPC91bml0LXByaWNlPg0KICAgICAgICA8aXRlbS1uYW1lPkdvbGQ8L2l0ZW0tbmFtZT4NCiAgICAgICAgPGl0ZW0tZGVzY3JpcHRpb24+QSAkMTAwIGRvbmF0aW9uIHRvIHRoZSBGcmVlbmV0IFByb2plY3QuPC9pdGVtLWRlc2NyaXB0aW9uPg0KICAgICAgPC9pdGVtPg0KICAgIDwvaXRlbXM+DQogIDwvc2hvcHBpbmctY2FydD4NCiAgPGNoZWNrb3V0LWZsb3ctc3VwcG9ydD4NCiAgICA8bWVyY2hhbnQtY2hlY2tvdXQtZmxvdy1zdXBwb3J0IC8+DQogIDwvY2hlY2tvdXQtZmxvdy1zdXBwb3J0Pg0KPC9jaGVja291dC1zaG9wcGluZy1jYXJ0Pg0KDQo=">100$ - Gold</option>
+                    <option value="PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4NCjxjaGVja291dC1zaG9wcGluZy1jYXJ0IHhtbG5zPSJodHRwOi8vY2hlY2tvdXQuZ29vZ2xlLmNvbS9zY2hlbWEvMiI+DQogIDxzaG9wcGluZy1jYXJ0Pg0KICAgIDxpdGVtcz4NCiAgICAgIDxpdGVtPg0KICAgICAgICA8cXVhbnRpdHk+MTwvcXVhbnRpdHk+DQogICAgICAgIDx1bml0LXByaWNlIGN1cnJlbmN5PSJVU0QiPjUwMC4wPC91bml0LXByaWNlPg0KICAgICAgICA8aXRlbS1uYW1lPlBsYXRpbnVtPC9pdGVtLW5hbWU+DQogICAgICAgIDxpdGVtLWRlc2NyaXB0aW9uPkEgJDUwMCBkb25hdGlvbiB0byB0aGUgRnJlZW5ldCBQcm9qZWN0LjwvaXRlbS1kZXNjcmlwdGlvbj4NCiAgICAgIDwvaXRlbT4NCiAgICA8L2l0ZW1zPg0KICA8L3Nob3BwaW5nLWNhcnQ+DQogIDxjaGVja291dC1mbG93LXN1cHBvcnQ+DQogICAgPG1lcmNoYW50LWNoZWNrb3V0LWZsb3ctc3VwcG9ydCAvPg0KICA8L2NoZWNrb3V0LWZsb3ctc3VwcG9ydD4NCjwvY2hlY2tvdXQtc2hvcHBpbmctY2FydD4NCg0K">500$ - Platin</option>
+                </select>
+            </td>
+            <td align="left" width="1%">
+                <input alt="Jetzt kaufen!" src="https://checkout.google.com/buttons/buy.gif?merchant_id=828420057535417&amp;w=121&amp;h=44&amp;style=white&amp;variant=text&amp;loc=de_DE" type="image"/>
+            </td>
+        </tr>
+    </table>
+    <input name="itemSignature" type="hidden" value="Pl1x6t9lcHiNBRMEBXo9AXOyEl8="/>
+    <input name="itemSignature" type="hidden" value="bJ5/nuKFXCwbgC2P5HObDWOBBb4="/>
+    <input name="itemSignature" type="hidden" value="vNL3QaknB7C0ytaxyX21OWzzSr4="/>
+    <input name="itemSignature" type="hidden" value="O+qrZNdUMaBQFmJnycC3DI3IV/0="/>
+</form>
+</center>
+<hr width="90%">
+
+<li>Über diesen Knopf können Sie <b>einmalig</b> über Paypal spenden:<br>
+<center>
+      <form action="https://www.paypal.com/de/cgi-bin/webscr" method="post">
+      <input type="hidden" name="cmd" value="_xclick">
+      <input type="hidden" name="business" value="ian@freenetproject.org">
+      <input type="hidden" name="no_shipping" value="1">
+<input type="hidden" name="cancel_return" value="http://freenetproject.org/donate.html">
+      <input type="hidden" name="return" value="http://freenetproject.org/">
+      <input type="image" 
+src="http://www.paypal.com/de_DE/i/btn/x-click-butcc-donate.gif" border="0" 
+name="submit" alt="Einmalig spenden per Paypal"></form>
+</center>
+<p>
+<hr width="90%">
+</p>
+</li>
+<li>Mit diesem Formular können Sie über
+<a href="http://www.e-gold.com/e-gold.asp?cid=767764">E Gold</a> Spenden:<br>
+<center>
+<table><tr><td bgcolor="#DDDDFF" style="{padding:4px; border:1px solid silver}">
+<form action="https://www.e-gold.com/sci_asp/payments.asp" method="POST" 
+target=_top>
+<input type="hidden" name="PAYEE_ACCOUNT" value="767764">
+<input type="hidden" name="PAYEE_NAME" value="Freenet Project Inc">Enter 
+Betrag den Sie spenden möchten:<br>USD$<input type=text 
+name="PAYMENT_AMOUNT" value="20" size=4>
+<input type=hidden name="PAYMENT_UNITS" value=1>in E-Gold
+<input type=hidden name="PAYMENT_METAL_ID" value=1>
+<input type="hidden" name="STATUS_URL" 
+value="mailto:ian@freenetproject.org"><input type="hidden" 
+name="NOPAYMENT_URL" value="http://freenetproject.org/">
+<input type="hidden" name="NOPAYMENT_URL_METHOD" value="LINK">
+<input type="hidden" name="PAYMENT_URL" 
+value="http://freenetproject.org/">
+<input type="hidden" name="PAYMENT_URL_METHOD" value="LINK">
+<input type="hidden" name="BAGGAGE_FIELDS" value="CUSTOMERID">
+<input type="hidden" name="CUSTOMERID" value="0">
+<input type="hidden" name="SUGGESTED_MEMO" value='Donation to Freenet Project'><br>
+<input type="submit" name="PAYMENT_METHOD" value="Donate Now">
+</form>
+</td></tr></table>
+</center>
+<p>
+<hr width="90%">
+</p>
+</li>
+<li>Sie können auch spenden indem Sie Dinge aus dem
+<a href="http://cafepress.com/freenetproject/">Freenet-Store</a> kaufen.
+</li>
+</ul>
+<h4>Wie kann ich mir sicher sein, dass meine Spende nicht missbraucht wird?</h4>
+<p>Alle Spenden gehen an die Freenet Project Inc., eine gemeinnützige Organisation
+(nach 501c3) mit dem folgenden Leitsatz:</p>
+<blockquote>
+The specific purpose of this corporation is to assist in developing and 
+disseminating technological solutions to further the open and democratic 
+distribution of information over the Internet or its successor 
+electronic communication networks or organizations. It is also the 
+purpose of this organization to guarantee consenting individuals the 
+free, unmediated, and unimpeded reception and impartation of all 
+intellectual, scientific, literary, social, artistic, creative, human 
+rights, and cultural expressions, opinions and ideas without 
+interference or limitation by or service to state, private, or special 
+interests. It is also the purpose of this organization to educate the 
+world community and be an advocate of these purposes.
+</blockquote>
+<p>Deutsche Übersetzung:</p>
+<blockquote>
+Der Zweck dieser Firma ist es die Entwicklung und Verbreitung technischer
+Lösungen zu unterstützen um die offene und demokratische Verbreitung von
+Informationen über das Internet oder nachfolgende elektronische
+Kommunikations-Netzwerke oder -Organisationen voranzutreiben.  Auch ist es ein
+Zweck dieser Organisation zustimmenden Personen die freie, unbeeinflusste und
+ungehinderte Aufnahme und Mitteilung von allen intellektuellen,
+wissenschaftlichen, literarischen, sozialen, künstlerischen, kreativen,
+menschenrechtlichen und kulturellen Äußerungen, Meinungen und Ideen ohne
+Einmischungen oder Beschränkungen durch staatliche, private oder besondere
+Interessen zu garantieren. Ein weiterer Zweck dieser Organisation ist es, die
+Welt-Gemeinschaft zu schulen, ein Verfechter dieser Ziele zu werden.
+</blockquote>
+<p>
+Alle gespendeten Gelder werden nur dazu verwendet um unseren Leitsatz
+umzusetzen und werden im Sitz des Freenet-Projekt-Vorstands verwaltet.
+</p>
--- pages/de/news.php
+++ pages/de/news.php
@@ -2,18 +2,7 @@
 few sentences - create a separate page and link to that (ie. Slashdot
 style).  Additionally, please remove obsolete news items (such as
 old new build announcements) --> 
-<!-- removed due to large donation - its unfair to imply that the project
-	is at death's door
-<h3>Financial Status</h3>
-The project's current Paypal balance is <b>
-<? 
-#include("paypaltotal.txt"); 
-?></b>.  The project requires approximately $2,300 per month to pay for 
-its 
-full time developer, Matthew Toseland.  If you would like to help 
-support the Freenet Project, you can make a convenient donation through 
-our <a href="/donate.html">donations page</a>.
--->
+
 <h3>Neuigkeiten</h3>
 <!-- Translation disclaimer - do not remove. Modify date of translation! -->
 <p>Die deutsche &Uuml;bersetzung der Neuigkeiten (Stand vom 31.3.2008)
