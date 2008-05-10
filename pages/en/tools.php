14e73b509e348be47eaf26b4162503a24ca589ef
Florent Daignière <nextgens@freenetproject.org>
2008-05-10 07:29:25 +0000

    website: update the tools page; fix broken links and get rid of the unmaintained fuqid there
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@19884 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/tools.php
+++ pages/en/tools.php
@@ -1,14 +1,14 @@
 <h2>Tools</h2>
 <h3>Freenet 0.7 applications</h3>
 <br />
-<p><a class='wiki'  href='http://jtcfrost.sf.net/'><b>Frost</b></a><br />
-<br />
-Frost is a filesharing and message board application for Freenet. Visit the <a class='wiki'  href='http://127.0.0.1:8888/SSK@XeMBryjuEaxqazEuxwnn~G7wCUOXFOZlVWbscdCOUFs,209eycYVidlZvhgL5V2a3INFxrofxzQctEZvyJaFL7I,AQABAAE/frost-1/'>Frost freesite</a>. <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
-</p>
-
 <p><a class='wiki'  href='http://downloads.freenetproject.org/alpha/Thaw/'><b>Thaw</b></a><br />
 <br />
 Thaw is a filesharing application, to add stuff to, and download stuff from, Freenet. New versions are featuring "index" support, to learn about it, I suggest you visit the <a class='wiki'  href='http://wiki.freenetproject.org/Thaw'>Thaw's wiki page</a>.
+</p>
+
+<p><b>Freemail</b><br />
+<br />
+Freemail provides anonymous e-mail over Freenet. Visit the <a class='wiki'  href='http://127.0.0.1:8888/USK@xOg49GNltumTJJzj0fVzuGDpo4hJUsy2UsGQkjE7NY4,EtUH5b9gGpp8JiY-Bm-Y9kHX1q-yDjD-9oRzXn21O9k,AQACAAE/freemail/2/'>Freemail freesite</a>. <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
 </p>
 
 <p><a class='wiki'  href='http://www.freenet.org.nz/pyfcp/'><b>pyFreenet</b></a><br />
@@ -21,15 +21,21 @@ pyFreenet is an API and a set of tools to access freenet from the python program
 jSite is a graphical application that you can use to create, insert and manage your own Freenet sites.
 </p>
 
-<p><a class='wiki'  href='http://127.0.0.1:8888/USK@DWB7AD1mBr5HNatdWvbqBe5FdZ3Zt~eS3nPHdp1Yg~g,HK1~x4rUgH6NyIT87CymSWcIHLP2Yc6dPQ417n1gnBY,AQABAAE/fuqid/1/'><b>FUQID</b></a> <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b><br />
-<br />
-FUQID is the Freenet Utility for Queued Inserts and Downloads. It is a Windows application for adding stuff to, and downloading stuff from, Freenet. It will also work on Linux using <a href="http://www.winehq.com/">Wine</a>.
-</p>
-
 <p><a class='wiki'  href="http://nostromo.joeh.org/content/fn_curses/..html"><b>fn_curses</b></a><br />
 <br />
 fn_curses is an ncurses (text mode) Freenet download queue manager written in perl.
 </p>
+
+<p><a class='wiki'  href='http://jtcfrost.sf.net/'><b>Frost</b></a><br />
+<br />
+Frost is a filesharing and message board application for Freenet. Visit the <a class='wiki'  href='http://127.0.0.1:8888/USK@QRZAI1nSm~dAY2hTdzVWXmEhkaI~dso0OadnppBR7kE,wq5rHGBI7kpChBe4yRmgBChIGDug7Xa5SG9vYGXdxR0,AQACAAE/frost/7/'>Frost freesite</a>. <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+</p>
+
+<p><b>FMS</b><br />
+<br />
+FMS is a spam-resistent message board application for Freenet. Visit the <a class='wiki'  href='http://127.0.0.1:8888/freenet:USK@F-BKUq9vNTrvp7D90r5edQ4MfZCrx3pP7TofdDlCRoU,YFUeosm79Z3KZxC62qQCteCCUI3D-LYyRk6P9auVpPA,AQACAAE/freenetapps/1/'>FMS freesite</a>. <b><span style='color:#FF0000;'>You must be running Freenet for this link to work.</span></b>
+</p>
+
 <br /><h2>Other tools? Want to contribute?
 </h2>
 <br />If you want to develop a tool or client for Freenet please publish your idea <a href="/freeapps.html">here</a>. There you can also find a list of projects related to Freenet in their early stages of development.<br />
