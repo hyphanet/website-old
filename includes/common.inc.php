8d9a64cb20c9488018a97713e7a73549242b9069
Florent Daignière <nextgens@freenetproject.org>
2008-06-17 18:37:45 +0000

    website: add the missing ';'
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@20413 67a373e5-eb02-0410-a15c-ee090a768436

--- includes/common.inc.php
+++ includes/common.inc.php
@@ -2,7 +2,7 @@
 
 if (!isset($_REQUEST["rewritten"])) {
         $currentURI = htmlentities($_SERVER['REQUEST_URI']);
-        $prefix = "http://freenetproject.org"
+        $prefix = "http://freenetproject.org";
                                                             
         if ($c=preg_match_all ("/\\/index\\.php\\?page=[\\/]*([^&]+)/is", $currentURI, $matches)) {
                 $page_name = $matches[1][0].".html";
