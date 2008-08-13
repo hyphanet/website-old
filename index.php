7e9c78983fe1b165a349cdaf3b9b370e7c3d46d6
Florent Daignière <nextgens@freenetproject.org>
2008-08-13 20:18:10 +0000

    website: switch to the new google-analytics tracking code
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@21824 67a373e5-eb02-0410-a15c-ee090a768436

--- index.php
+++ index.php
@@ -79,7 +79,14 @@ This website is licensed under the <a href="http://www.gnu.org/licenses/fdl.html
 </div>
 </div>
 	<div class="hideit">Send spam to <a href="mailto:catchme@freenetproject.org">catchme@freenetproject.org</a> ! :)</div>
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script><script type="text/javascript">_uacct = "UA-354970-1";urchinTracker();</script>
+
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	var pageTracker = _gat._getTracker("UA-354970-1");
+	pageTracker._trackPageview();
+	</script>
 </body>
 </html>
