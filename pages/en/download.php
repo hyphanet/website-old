29b73367bfefa597e59fe9af63325242b8d6236c
Matthew Toseland <toad@amphibian.dyndns.org>
2008-09-22 22:57:06 +0000

    Explain the cronjob. Maybe we should delete the last sentence.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@22742 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/download.php
+++ pages/en/download.php
@@ -8,7 +8,10 @@ security level (on the config page) to "normal" or "low", so that your node
 automatically connects to other nodes. It may take several minutes for your 
 node to connect to enough nodes for Freenet to be usable, and performance 
 will likely improve over time after that. You will get best performance if 
-you can leave your node running 24x7.</p>
+you can leave your node running 24x7, so by default the installer will
+install a service/cronjob to start Freenet when your computer boots up. 
+Since Freenet is a distributed network, you will *not* get good performance 
+if you only run it when you are actually using it.</p>
 
 <p><i>If you have a firewall or a NAT box click <a href="/faq.html#firewall"><b>here</b></a>
 for some info.</i></p>
