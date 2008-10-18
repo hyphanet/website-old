fbe250a6655d05adee146fe33651619b4bf9f03d
Matthew Toseland <toad@amphibian.dyndns.org>
2008-10-18 23:11:24 +0000

    specify
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@23007 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/donate.php
+++ pages/en/donate.php
@@ -120,7 +120,8 @@ name="submit">
 <li>You can now also donate to the project by purchasing items from the 
  Freenet <a 
 href="http://cafepress.com/freenetproject/">store</a>.</li>
-<li>Alternatively, please <a href="mailto:ian@freenetproject.org">contact us</a>.</li>
+<li>Alternatively, please <a href="mailto:ian@freenetproject.org">contact us</a> 
+if you cannot use any of these payment methods.</li>
 </ul>
 <h4>How can I be sure that my donation will be used appropriately?</h4>
 All donations go to The  
