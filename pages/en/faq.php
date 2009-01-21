f204a7314a64d9e0721edb69b0b2cfcf76dca1de
Matthew Toseland <toad@amphibian.dyndns.org>
2009-01-21 00:21:31 +0000

    disk encryption
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@25160 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/faq.php
+++ pages/en/faq.php
@@ -493,7 +493,7 @@ configurable at some point, but turning it off enables an easier attack). If the
 attacker can obtain your hard disk, or connect to your node and probe your datastore
 by requesting keys and timing how long they take, he may be able to determine what
 you have been downloading from/uploading to Freenet. Right now the best way to 
-prevent this is to only connect to your trusted friends, as above. In version 0.9, 
+prevent this is to only connect to your trusted friends, as above, and encrypt your hard disk. In version 0.9, 
 we will implement some form of cryptographic tunnels, which will eliminate the 
 need to cache locally requested data in the datastore and largely solve this attack.</li>
 <li><b>Correlation attacks</b>: If you are connected to a node, and can recognise
