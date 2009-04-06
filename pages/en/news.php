77e6b7e9d881413400ac243074c1cfe30e86ec6a
Matthew Toseland <toad@amphibian.dyndns.org>
2009-04-06 20:54:15 +0000

    Delete update
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/website@26571 67a373e5-eb02-0410-a15c-ee090a768436

--- pages/en/news.php
+++ pages/en/news.php
@@ -26,13 +26,13 @@ We have enough money to pay for the project's full time developer and the server
 
 <h3>News</h3>
 
+<p><b>
+
 <p><b>26th March, 2009 - Summer of Code 2009</b></p>
 <p>Freenet has been accepted into <a href="http://socghop.appspot.com/">Google Summer of Code 2009</a>! 
 That means Google will be paying for a bunch of students to work for us over the summer. If you are
 interested, please create an account through the above link, and contact us via IRC or the mailing lists (details below).
 See <a href="http://wiki.freenetproject.org/SummerOfCode2009">here</a> for more details.
-<p><b>UPDATE:</b>The deadline is <i>today (April 3rd)</i>! Get your applications in <a href="http://socghop.appspot.com/">here</a>.
-They don't have to be perfect, we will help you flesh out the details if necessary after the deadline, but they do have to be related to what you want to do!</p>
 
 <p><b>11th Dec, 2008 - Submit your suggestions for Freenet</b></p>
 You can now request and vote on features for Freenet through <a href="http://freenet.uservoice.com/">freenet.uservoice.com</a>.  What are you waiting for?!
