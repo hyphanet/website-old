7db0692242a165e04b4d6a495530d17dc355223f
Matthew Toseland <toad@amphibian.dyndns.org>
2009-05-04 18:11:55 +0100

    Update sponsors page, clarify amounts, and fix formatting

--- pages/en/sponsors.php
+++ pages/en/sponsors.php
@@ -5,23 +5,31 @@ sponsored this project through donations or discounts on hardware and services:
 <ol>
 <li><a href="http://onemansblog.com/">John Pozadzides</a><br>
 John is the founder of HTMLHelp.com and former Vice President of Sales for SAVVIS Communications.  John has donated $10,000 to the
-Freenet Project to fund ongoing development of the Freenet software.
+Freenet Project to fund ongoing development of the Freenet software.</li>
 
 <li><a href="http://www.toad.com/gnu/">John Gilmore</a><br>
-John Gilmore is one of the founders of the Electronic Frontier Foundation, the Cypherpunks mailing list, and Cygnus Solutions. He created the alt.* hierarchy in Usenet and is a major contributor to the GNU project.
+John Gilmore is one of the founders of the Electronic Frontier Foundation, the Cypherpunks mailing list, and Cygnus Solutions. He created the alt.* hierarchy in Usenet and is a major contributor to the GNU project. He has donated $10,000.</li>
 <p>
 <li><a href="http://bytemark.co.uk/">Bytemark Hosting</a><br>
 Bytemark provides discounted hosting facilities to Freenet and comes with 
-our highest recommendation.
+our highest recommendation.</li>
  <p>
 <li><a href="http://www.zoozle.org/">zoozle Torrent Search</a><br>
 zoozle is a search engine for BitTorrent and P2P networks. It is also
 available in <a href="http://www.zoozle.net/" title="deutsche Torrent
 Suchmaschine">german</a> and <a href="http://www.zoozle.biz/" title="le
-français moteur de recherche pour BitTorrent">french</a>.
+français moteur de recherche pour BitTorrent">french</a>. They have 
+donated $1,500.</li>
 <p>
 <li><a href="http://www.yourkit.com/">YourKit, LLC</a><br>
-YourKit is kindly supporting Freenet by providing us a free of charge license of its full-featured Java Profiler. YourKit, LLC is the creator of innovative and intelligent tools for profiling Java applications. You can learn more about <a href="http://www.yourkit.com/java/profiler/index.jsp">YourKit Java Profiler</a> following the link.
+YourKit is kindly supporting Freenet by providing us a free of charge license of its full-featured Java Profiler. YourKit, LLC is the creator of innovative and intelligent tools for profiling Java applications. You can learn more about <a href="http://www.yourkit.com/java/profiler/index.jsp">YourKit Java Profiler</a> following the link.</li>
+<p>
+<li><a href="http://www.google.com/">Google</a><br>
+Google <a href="http://code.google.com/">open source</a> have donated $18,000,
+as well as paying for students to work with us over summer for four years 
+through the <a href="http://code.google.com/soc/">Summer of Code</a> 
+programme.
+</li>
 </ol>
 <p>
 
