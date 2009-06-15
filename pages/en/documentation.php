05632d18472c97c7042749d1bf5dc4133ac9615d
Matthew Toseland <toad@amphibian.dyndns.org>
2009-06-16 00:34:23 +0100

    New Documentation page from Clement's project

--- pages/en/documentation.php
+++ pages/en/documentation.php
@@ -1,4 +1,19 @@
 <h2>Documentation</h2>
+      <div id="navigation">
+	<p> Go to :
+	  <a href="install.html">Install</a>
+	  <a href="connect.html">Connect</a>
+
+	  <a href="content.html">Content</a>
+	  <a href="understand.html">Understand</a>
+	  <a href="freemail.html">Freemail</a>
+	  <a href="frost.html">Frost</a>
+	  <a href="jsite.html">jSite</a>
+	  <a href="thaw.html">Thaw</a>
+
+	  <a href="http://wiki.freenetproject.org">Wiki</a>
+	</p>
+      </div>
 <p>
 The documentation on Freenet is forever changing, since
 this is a project in development. However, here under 
@@ -7,49 +22,58 @@ how to setup Freenet and use the most popular Freenet
 tools as well as a user driven Wiki and an extensive
 Frequently Asked Questions department.
 </p>
-<ul>
-<li><span style="font-weight: bold;">Install</span><br/>
-Contains detailed instructions on how to
-install Freenet using the new installer.
-</li>
-<li><span style="font-weight: bold;">Connect</span><br/>
-How to connect to Freenet.
-</li>
-<li><span style="font-weight: bold;">Content</span><br/>
-Freenet as a Content Distribution System.
-</li>
-<li><span style="font-weight: bold;">Understand</span><br/>
-Explains the workings of Freenet, and a 
-glossary with the most frequently used terms.
-</li>
-<li><span style="font-weight: bold;">Freemail</span><br/>
-How to setup Freenets own anonymous email service.
-</li>
-<li><span style="font-weight: bold;">Frost</span><br/>
-Frost is the oldest and most used messaging and file 
-sharing tool in the Freenet suite. This describes how
-to set it up and use it for the first time.
-</li>
-<li><span style="font-weight: bold;">jSite</span><br/>
-jSite is a Freenet website (a.k.a. Freesite) insertion 
-tool.
-</li>
 
-<li><span style="font-weight: bold;">Thaw</span><br/>
-Thaw is a filesharing utility and upload/download manager. 
-It is used as a graphical interface for Freenet filesharing.
-</li>
+      
+      <ul>
+	<li>
+	  <a href="install.html"><strong>Install</strong></a><br/>
 
-<li><span style="font-weight: bold;">FAQ</span><br/>
-The Frequently Asked Questions department holds both
-philosophical and technical questions on how to avoid
-and solve problems with Freenet.
-</li>
+	  Contains detailed instructions on how to
+	  install Freenet using the new installer.
+	</li>
+	<li>
+	  <a href="connect.html"><strong>Connect</strong></a><br/>
+	  How to connect to Freenet.
+	</li>
+	<li>
+	  <a href="content.html"><strong>Content</strong></a><br/>
+	  Freenet as a Content Distribution System.
+	</li>
 
-<li><span style="font-weight: bold;">Wiki</span><br/>
-The Freenet Wiki is a user driven space where new features
-may be described before finding its way into the website.
-It is a good source of information about all sorts of
-Freenet related issues.
-</li>
-</ul>
+	<li>
+	  <a href="understand.html"><strong>Understand</strong></a><br/>
+	  Explains the workings of Freenet, and a 
+	  glossary with the most frequently used terms.
+	</li>
+	<li>
+	  <a href="freemail.html"><strong>Freemail</strong></a><br/>
+	  How to setup Freenets own anonymous email service.
+	</li>
+	<li>
+
+	  <a href="frost.html"><strong>Frost</strong></a><br/>
+	  Frost is the oldest and most used messaging and file 
+	  sharing tool in the Freenet suite. This describes how
+	  to set it up and use it for the first time.
+	</li>
+	<li>
+	  <a href="jsite.html"><strong>jSite</strong></a><br/>
+	  jSite is a Freenet website (a.k.a. Freesite) insertion 
+	  tool.
+	</li>
+	<li>
+	  <a href="thaw.html"><strong>Thaw</strong></a><br/>
+
+	  Thaw is a filesharing utility and upload/download manager. 
+	  It is used as a graphical interface for Freenet filesharing.
+
+	</li>
+	<li>
+	  <a href="http://wiki.freenetproject.org/"><strong>Wiki</strong></a><br/>
+	  The Freenet Wiki is a user driven space where new features
+	  may be described before finding its way into the website.
+	  It is a good source of information about all sorts of
+	  Freenet related issues.
+	</li>
+      </ul>
+
