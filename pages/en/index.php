ad4011af3d38ffec5cb73e58861c8f5292e4478a
Matthew Toseland <toad@amphibian.dyndns.org>
2009-06-16 21:15:23 +0100

    Improve text about freesites, don't feed people's Tor assumptions

--- pages/en/index.php
+++ pages/en/index.php
@@ -10,10 +10,11 @@ Foundation</a>
 <p>
 <h3>What is Freenet?</h3>
 <p> Freenet is free software which lets you anonymously share files, browse 
-and publish web sites, and chat on forums, without fear of 
-censorship. Users are anonymous, and Freenet is entirely decentralised. 
-Without anonymity there can never be true freedom of speech, and without 
-decentralisation the network would be vulnerable to attack.
+and publish "freesites" (web sites accessible only through Freenet), and 
+chat on forums, without fear of censorship. Users are anonymous, and 
+Freenet is entirely decentralised. Without anonymity there can never be 
+true freedom of speech, and without decentralisation the network would be 
+vulnerable to attack.</p>
 <a href="/whatis.html">Learn more!</a></p>
 <? 
 	include("pages/en/news.php");
