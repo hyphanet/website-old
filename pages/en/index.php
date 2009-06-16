d79a116287a57b11c7d97c73651c8539fe8099d2
Matthew Toseland <toad@amphibian.dyndns.org>
2009-06-16 21:24:11 +0100

    Text flow again

--- pages/en/index.php
+++ pages/en/index.php
@@ -10,7 +10,7 @@ Foundation</a>
 <p>
 <h3>What is Freenet?</h3>
 <p> Freenet is free software which lets you anonymously share files, browse 
-and publish "freesites" (web sites accessible only through Freenet), and 
+and publish "freesites" (web sites accessible only through Freenet) and 
 chat on forums, without fear of censorship. Users are anonymous, and 
 Freenet is entirely decentralised. Without anonymity there can never be 
 true freedom of speech, and without decentralisation the network would be 
