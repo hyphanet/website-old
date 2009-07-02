6f4bcec2c3af28b972ad843ffd6aea9767428052
Matthew Toseland <toad@amphibian.dyndns.org>
2009-07-02 18:01:11 +0100

    Fix!

--- make-pages.sh
+++ make-pages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -Rf output
 (ls pages/en/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output; while read x; do ./make-static-page.sh "$x" > output/${x}.html; done)
-cp -a image papers ss output
+cp -a * output
+rm -Rf output/pages output/includes
--- make-static-page.sh
+++ make-static-page.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PAGE=${1:-index}
-(echo "<?"; echo '$_REQUEST["lang"] = "de";'; echo '$_REQUEST["rewritten"] = 1;'; echo -n '$_REQUEST["page"] = "'; echo -n $PAGE; echo "\";"; echo 'include "index.php";'; echo "?>") | php5-cgi | (while read x; do if [[ $x == $'\r' ]]; then break; fi; done; cat)
+(echo "<?"; echo '$_REQUEST["lang"] = "en";'; echo '$_REQUEST["rewritten"] = 1;'; echo -n '$_REQUEST["page"] = "'; echo -n $PAGE; echo "\";"; echo 'include "index.php";'; echo "?>") | php5-cgi | (while read x; do if [[ $x == $'\r' ]]; then break; fi; done; cat)
