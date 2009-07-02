8383039fdc038ee165c17b0e7f68334a3a87e6aa
Matthew Toseland <toad@amphibian.dyndns.org>
2009-07-02 18:13:07 +0100

    Remove *.php

--- make-pages.sh
+++ make-pages.sh
@@ -3,3 +3,4 @@ rm -Rf output
 (ls pages/en/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output; while read x; do ./make-static-page.sh "$x" > output/${x}.html; done)
 cp -a * output
 rm -Rf output/pages output/includes
+rm output/*.php
