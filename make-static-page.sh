39f3b20220fb8ffb70b7c8a1e47386b28a3691d5
Matthew Toseland <toad@amphibian.dyndns.org>
2009-07-02 17:27:56 +0100

    Script to make a page statically

---
+++ make-static-page.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+PAGE=${1:-index}
+(echo "<?"; echo '$_REQUEST["rewritten"] = 1;'; echo -n '$_REQUEST["page"] = "'; echo -n $PAGE; echo "\";"; echo 'include "index.php";'; echo "?>") | php5-cgi | (while read x; do if [[ $x == $'\r' ]]; then break; fi; done; cat)
