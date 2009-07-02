8a49cf88e8d375e7a1916552901f4895350bec4b
Matthew Toseland <toad@amphibian.dyndns.org>
2009-07-02 18:03:54 +0100

    Missing slash killed submenus

--- make-static-page.sh
+++ make-static-page.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 PAGE=${1:-index}
+PAGE=/$PAGE
 (echo "<?"; echo '$_REQUEST["lang"] = "en";'; echo '$_REQUEST["rewritten"] = 1;'; echo -n '$_REQUEST["page"] = "'; echo -n $PAGE; echo "\";"; echo 'include "index.php";'; echo "?>") | php5-cgi | (while read x; do if [[ $x == $'\r' ]]; then break; fi; done; cat)
