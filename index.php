e0cc016f06a94d3b999b58c75420ed0745ef219d
Matthew Toseland <toad@amphibian.dyndns.org>
2009-07-31 20:26:40 +0100

    Add more tracking code for Ian. IMHO this impacts on performance ... :|

--- index.php
+++ index.php
@@ -102,5 +102,10 @@ include 'includes/common.inc.php';
 	var pageTracker = _gat._getTracker("UA-354970-1");
 	pageTracker._trackPageview();
 	</script>
+<!-- Woopra Code Start --> <script type="text/javascript"> var _wh =
+((document.location.protocol=='https:') ? "https://sec1.woopra.com" : "
+http://static.woopra.com"); document.write(unescape("%3Cscript src='" + _wh
++ "/js/woopra.js' type='text/javascript'%3E%3C/script%3E")); </script> <!--
+Woopra Code End -->
 </body>
 </html>
