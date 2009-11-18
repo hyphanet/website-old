2a81c90f49d96deef2bca1ba91fd4462d268dc4f
Artefact2 <artefact2@gmail.com>
2009-11-18 20:00:32 +0100

    Use links to full-size screenshots.

--- pages/en/index.php
+++ pages/en/index.php
@@ -41,11 +41,11 @@
 	<!--<a href="image/fN_screenshot.png"><img src="image/fN_screenshot_small.png"
 	alt="freenet screenshot" width="383" height="309" /></a>-->
         <ul id="screenshots_content">  
-         <li class="screenshots_item"><a href="#"><img src="image/fN_screenshot_small.png" width="383" height="309" alt="screenshot 2" /></a></li>  
-         <li class="screenshots_item"><a href="#"><img src="image/fN_screenshot_minimalist_small.png" width="383" height="311" alt="screenshot 3" /></a></li>  
-         <li class="screenshots_item"><a href="#"><img src="image/fN_screenshot_downloads_small.png" width="383" height="298" alt="screenshot 5" /></a></li>  
-         <li class="screenshots_item"><a href="#"><img src="image/fN_screenshot_freetalk_messages_small.png" width="383" height="303" alt="screenshot 6" /></a></li>  
-         <li class="screenshots_item"><a href="#"><img src="image/fN_screenshot_searched_small.png" width="383" height="298" alt="screenshot 7" /></a></li>  
+         <li class="screenshots_item"><a href="image/fN_screenshot.png"><img src="image/fN_screenshot_small.png" width="383" height="309" alt="screenshot 2" /></a></li>  
+         <li class="screenshots_item"><a href="image/fN_screenshot_minimalist.png"><img src="image/fN_screenshot_minimalist_small.png" width="383" height="311" alt="screenshot 3" /></a></li>  
+         <li class="screenshots_item"><a href="image/fN_screenshot_downloads.png"><img src="image/fN_screenshot_downloads_small.png" width="383" height="298" alt="screenshot 5" /></a></li>  
+         <li class="screenshots_item"><a href="image/fN_screenshot_freetalk_messages.png"><img src="image/fN_screenshot_freetalk_messages_small.png" width="383" height="303" alt="screenshot 6" /></a></li>  
+         <li class="screenshots_item"><a href="image/fN_screenshot_searched.png"><img src="image/fN_screenshot_searched_small.png" width="383" height="298" alt="screenshot 7" /></a></li>  
         </ul>  
         <div id="screenshots_frame">  
          <img id="screenshots_previous" src="image/prev.png" alt="move previous" />  
