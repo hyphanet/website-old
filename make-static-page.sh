c3631dee8be27f280b14cb263288ee88b4349905
dieppe <cvollet@gmail.com>
2010-01-07 06:45:59 +0100

    Make the make-static-page.sh script take language as a parameter

--- make-static-page.sh
+++ make-static-page.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-PAGE=${1:-index}
+LANG=${1}
+PAGE=${2:-index}
 PAGE=/$PAGE
-(echo "<?"; echo '$_REQUEST["lang"] = "en";'; echo '$_REQUEST["rewritten"] = 1;'; echo -n '$_REQUEST["page"] = "'; echo -n $PAGE; echo "\";"; echo 'include "index.php";'; echo "?>") | php5-cgi | (while read x; do if [[ $x == $'\r' ]]; then break; fi; done; cat)
+
+(echo "<?php"; echo '$_REQUEST["lang"] = "'$LANG'";'; echo '$_REQUEST["rewritten"] = 1;'; echo -n '$_REQUEST["page"] = "'; echo -n $PAGE; echo "\";"; echo 'include "index.php";'; echo "?>") | php5-cgi | (while read x; do if [[ $x == $'\r' ]]; then break; fi; done; cat)
