ff2bdd36a160ef2d182714f1da39872284cdd057
Artefact2 <artefact2@gmail.com>
2010-08-26 23:01:15 +0200

    Fixed broken video code.

--- pages/de/22c3vid.php
+++ pages/de/22c3vid.php
@@ -1,7 +1,8 @@
 <h2>22nd Chaos Computer Congress Vortrag &uuml;ber Freenet 0.7</h2>
 <p>Im Dezember 2005 pr&auml;sentierten Ian Clarke und Oskar Sandberg in Berlin
 den neuen Entwurf f&uuml;r Freenet. Hier ist der Vortrag den sie hielten:</p>
-
+<p>
 	<center>
-		<embed style="width:400px; height:326px;" id="VideoPlayback" align="middle" type="application/x-shockwave-flash" src="http://video.google.com/googleplayer.swf?videoUrl=http%3A%2F%2Fvp.video.google.com%2Fvideodownload%3Fversion%3D0%26secureurl%3DwgAAAG7ggqAHSiJjpW0D3w4aYTWysCVjL1o53SuJpfb3pGYozQ6ekjoc3LE4Q0Xzc8EH30V-bqx3qWEpOiaqiFJ6nRbItEVRrQz6Mze1Bec17umxOM_P4sKIX__shPbADZUU4CDL6gBNCAPgvCNNONhS4AEuiFfhELCOZd0ggh2f_OYShpGeWjHQlkMlWstSFwza8Wl7VmZPWIYTDEtIYj7mEESsLd-IB47qwX8vTL8ELu-6_rPmGlUnNz7ijS5IRItkfsYnYbaHKe_7PjblnpsCsMg%26sigh%3DULIQo-9hMNVcEwlrOtdK-b-Voog%26begin%3D0%26len%3D3871480%26docid%3D-2894489251637986433&thumbnailUrl=http%3A%2F%2Fvideo.google.com%2FThumbnailServer%3Fapp%3Dvss%26contentid%3D860d23ea4a7ee84c%26second%3D5%26itag%3Dw320%26urlcreated%3D1149285760%26sigh%3D-px-hq1tmsjGeG9lo-K5FG1rggI&playerId=-2894489251637986433" allowScriptAccess="sameDomain" quality="best" bgcolor="#ffffff" scale="noScale" wmode="window" salign="TL"  FlashVars="playerMode=embedded"> </embed>
-</center>
+		<embed id=VideoPlayback src=http://video.google.com/googleplayer.swf?docid=-2894489251637986433&hl=en&fs=true style=width:400px;height:326px allowFullScreen=true allowScriptAccess=always type=application/x-shockwave-flash> </embed>
+	</center>
+</p>
