6a682ff869007663f8f8b511075fa9e32922871f
Matthew Toseland <toad@amphibian.dyndns.org>
2010-12-07 17:31:44 +0000

    Update re google

--- pages/en/sponsors.php
+++ pages/en/sponsors.php
@@ -59,7 +59,7 @@
 	  
 	  <li>
 	    <a href="http://www.google.com/">Google</a><br/>
-	    Google <a href="http://code.google.com/">open source</a> have twice donated $18,000,
+	    Google <a href="http://code.google.com/">open source</a> have three times donated $18,000,
 	    as well as paying for students to work with us over summer for four years 
 	    through the <a href="http://code.google.com/soc/">Summer of Code</a> 
 	    programme.
