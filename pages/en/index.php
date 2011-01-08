8e68bb211eb8f9696e7fc7ac9f980c6978a80dff
Matthew Toseland <toad@amphibian.dyndns.org>
2011-01-08 03:03:33 +0000

    Reinstate the paypal counter on the front page.

--- pages/en/index.php
+++ pages/en/index.php
@@ -105,7 +105,7 @@ $(document).ready(function() {
 	<div id="donate_button">
 	     <h4>Make a donation</h4>
 	     <p>We don't charge anyone to download and use Freenet, we're a non-profit organization who rely on your donations to survive.</p>
-<!--	     <p>Our current balance of <b>$MONEYBALANCE</b> will pay for this server and our one paid developer for around another <b>MONEYDAYS days</b>.</p> -->
+	     <p>Our current balance of <b>$MONEYBALANCE</b> will pay for this server and our one paid developer for around another <b>MONEYDAYS days</b>.</p>
 	     <p><a href="donate.html">Donate!</a></p>
 	</div>
 
