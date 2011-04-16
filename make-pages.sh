d651cc30078d5dcd060bee79ca59e5cf89e03dbd
Florent Daigniere <nextgens@freenetproject.org>
2011-04-16 19:37:59 +0100

    ensure ls behaves like the script expects; don't deploy the legacy_pages anymore

--- make-pages.sh
+++ make-pages.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 rm -Rf output
-(ls pages/en/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output; while read x; do ./make-static-page.sh "en" "$x" > output/${x}.html; done)
-(ls pages/fr/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output/fr; while read x; do ./make-static-page.sh "fr" "$x" > output/fr/${x}.html; done)
+(ls -1 pages/en/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output; while read x; do ./make-static-page.sh "en" "$x" > output/${x}.html; done)
+(ls -1 pages/fr/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output/fr; while read x; do ./make-static-page.sh "fr" "$x" > output/fr/${x}.html; done)
 cp -a * output
-rm -Rf output/pages output/includes
-rm output/*.php
+rm -Rf output/pages output/includes output/output output/legacy_pages output/*.php
