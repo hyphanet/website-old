dcf9d7480124682e6b248316d4df8d04676af034
Florent Daigniere <nextgens@freenetproject.org>
2011-04-17 10:57:52 +0100

    We don't need the shellscripts in the output

--- make-pages.sh
+++ make-pages.sh
@@ -8,5 +8,5 @@ rm -Rf output
 (ls -1 pages/en/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output; while read x; do ./make-static-page.sh "en" "$x" > output/${x}.html; done)
 (ls -1 pages/fr/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output/fr; while read x; do ./make-static-page.sh "fr" "$x" > output/fr/${x}.html; done)
 cp -a * output
-rm -Rf output/pages output/includes output/output output/legacy_pages output/*.php
+rm -Rf output/pages output/includes output/output output/legacy_pages output/*.php output/*.sh
 echo "done."
