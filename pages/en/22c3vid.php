32cf2b31825e8d47e99814c3ce5023fab8a82cb9
Matthew Toseland <toad@amphibian.dyndns.org>
2011-04-19 19:28:39 +0100

    Switch to vimeo.

--- pages/en/22c3vid.php
+++ pages/en/22c3vid.php
@@ -2,6 +2,6 @@
 <p>In December 2005, Ian Clarke and Oskar Sandberg presented the new design for Freenet in Berlin.  Here is the talk they gave:</p>
 <p>
 	<center>
-		<embed id=VideoPlayback src=http://video.google.com/googleplayer.swf?docid=-2894489251637986433&hl=en&fs=true style=width:400px;height:326px allowFullScreen=true allowScriptAccess=always type=application/x-shockwave-flash> </embed>
+		<iframe src="http://player.vimeo.com/video/22488244?title=0&amp;byline=0&amp;portrait=0" width="400" height="300" frameborder="0"></iframe><p><a href="http://vimeo.com/22488244">Routing in the Dark: Freenet's new design</a> from <a href="http://vimeo.com/user5754494">Ian Clarke</a> on <a href="http://vimeo.com">Vimeo</a>.</p>
 	</center>
 </p>
