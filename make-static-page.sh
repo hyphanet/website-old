bf0a7349e3ff2032700dd69449aba577382ca316
Florent Daigniere <nextgens@freenetproject.org>
2011-04-26 15:36:52 +0100

    define DOCUMENT_ROOT to fix the 404 detection and remove a warning

--- make-static-page.sh
+++ make-static-page.sh
@@ -3,4 +3,4 @@ LANG=${1}
 PAGE=${2:-index}
 PAGE=/$PAGE
 
-(echo "<?php"; echo '$_REQUEST["lang"] = "'$LANG'";'; echo '$_REQUEST["rewritten"] = 1;'; echo -n '$_REQUEST["page"] = "'; echo -n $PAGE; echo "\";"; echo 'include "index.php";'; echo "?>") | php5-cgi | (while read x; do if [[ $x == $'\r' ]]; then break; fi; done; cat)
+(echo "<?php"; echo '$_SERVER["DOCUMENT_ROOT"] = "'$PWD'";'; echo '$_REQUEST["lang"] = "'$LANG'";'; echo '$_REQUEST["rewritten"] = 1;'; echo -n '$_REQUEST["page"] = "'; echo -n $PAGE; echo "\";"; echo 'include "index.php";'; echo "?>") | php5-cgi | (while read x; do if [[ $x == $'\r' ]]; then break; fi; done; cat)
