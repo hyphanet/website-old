d028c7240ef237cb228d957a5a2b161d17b0a628
Matthew Toseland <toad@amphibian.dyndns.org>
2011-06-15 15:40:32 +0100

    Deprecate the headless installer.

--- pages/en/download.php
+++ pages/en/download.php
@@ -109,33 +109,7 @@
 	  above command lines.
 	</p>
 	
-	<h3>Headless install (not recommended)</h3>
-	
-	<p>
-	  Or do the following on a headless system:
-	</p>
-	
-	<pre>
-	  wget <a class='maybeSwitchProtocol' href="https://downloads.freenetproject.org/alpha/installer/freenet07.tar.gz">https://downloads.freenetproject.org/alpha/installer/freenet07.tar.gz</a>
-	  tar xvzf freenet07.tar.gz
-	  cd freenet
-	  ./run.sh start</pre>
-	
-	<p>
-	  Read the <a href="faq.html#fproxy-lan">FAQ</a> on how to
-	  enable web-access from a remote computer.
-	</p>
-	
-	<p>Note that this will not install the system tray icon or desktop/menu shortcuts, so
-	you should open Freenet manually in a web browser:</p>
-      
-      <pre>
-	<a href="http://127.0.0.1:8888/">http://127.0.0.1:8888/</a></pre>
-	
-	<p>Note also that most people use the GUI installer - use that if you can. The scripts
-	in the headless installer can be a bit unreliable, so try again if it doesn't work the
-	first time. And <b>you must have Sun Java installed and set as default</b> 
-	(man update-java-alternatives on debian/ubuntu) before you install.</p>
+	<p><b>Headless servers (geeks only!):</b> The standard installer won't work on a headless (console-only) system unless you install the X libraries and ssh -X into it. We <i>strongly</i> recommend you do this. Or you can use the deprecated and unreliable <a class='maybeSwitchProtocol' href="https://downloads.freenetproject.org/alpha/installer/freenet07.tar.gz">headless installer</a>. To make this work, you must first install the latest Sun Java, make sure it is on the path (update-java-alternatives on Ubuntu), and make sure you have wget or curl installed. Also you should increase the memory limit in wrapper.conf after install. Then open http://127.0.0.1:8888/ in a web browser (see <a href="faq.html#fproxy-lan">here</a> for how to open it to your LAN).</p>
 
 	<div id="mirrored">
 	  
