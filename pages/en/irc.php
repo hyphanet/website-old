c79c86517eebfe70f3835c9b54a07b8d97465df9
Matthew Toseland <toad@amphibian.dyndns.org>
2011-07-09 18:48:31 +0100

    IRC landing page, warn users that they won't get an instant answer.

---
+++ pages/en/irc.php
@@ -0,0 +1,9 @@
+<h1>Chat with us on IRC</h1>
+
+<p>Many of the developers and users of Freenet can often be found on an <a href="http://en.wikipedia.org/wiki/IRC">IRC</a> channel,
+#freenet on irc.freenode.org. Almost everyone on the channel is a volunteer, and 
+may be busy with other things, so you may not get an instant answer: <b>Ask your 
+question/say what the problem is, then wait for a few minutes, and somebody 
+may help you</b>.</p>
+
+<a href="http://webchat.freenode.net/?randomnick=1&channels=freenet">Chat with us</a>
--- pages/en/menu.php
+++ pages/en/menu.php
@@ -19,7 +19,7 @@
 	      <li><a href="faq.html">FAQ</a></li>
 	      <li><a href="lists.html">Mailing lists</a></li>
 	      <li><a href="http://freenet.uservoice.com/">Suggestions</a></li>
-	      <li class="last"><a href="http://webchat.freenode.net/?randomnick=1&channels=freenet">Chat with us</a></li>
+	      <li class="last"><a href="irc.html">Chat with us</a></li>
 	    </ul>
 	  </li>
 	  <li class="dir"><a href="donate.html">Donate</a>
