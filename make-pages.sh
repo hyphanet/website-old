7b806769a69a7334d20c3519548b97b1e3a0a091
Steve Dougherty <steve@asksteved.com>
2012-03-04 16:54:54 -0500

    Remove readme from generated output.

--- make-pages.sh
+++ make-pages.sh
@@ -9,5 +9,5 @@ mkdir output
 (ls -1 pages/en/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output/en; while read x; do ./make-static-page.sh "en" "$x" > output/en/${x}.html; done)
 (ls -1 pages/es/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output/es; while read x; do ./make-static-page.sh "es" "$x" > output/es/${x}.html; done)
 cp -a * output
-rm -Rf output/pages output/includes output/output output/legacy_pages output/*.php output/*.sh
+rm -Rf output/pages output/includes output/output output/legacy_pages output/*.php output/*.sh output/README.md
 echo "done."
