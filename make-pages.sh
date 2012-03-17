5ddd92bc91c62fe5a580fa3f4828f1ea57a9bdfd
Steve Dougherty <steve@asksteved.com>
2012-03-17 15:45:57 -0400

    Exclude from copying undesired files/directories. Otherwise the copy command will error out upon trying to copy output to output.

--- make-pages.sh
+++ make-pages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -o errexit
+shopt -s extglob
 echo -e "\ngenerating the website..."
 rm -Rf output
 mkdir output
 for lang in en es; do
     (ls -1 "pages/$lang/" | sed -n "s/\\(.*\\)\.php/\\1/p") | (while read x; do ./make-static-page.sh "$lang" "$x" > "output/${x}.$lang.html"; done)
 done
-cp -a * output
-rm -Rf output/pages output/includes output/output output/legacy_pages output/*.php output/*.sh output/README.md
+cp -a !(pages|includes|output|legacy_pages|*.php|*.sh|README.md) output
 echo "done."
