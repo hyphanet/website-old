d4cc225676343ad2627fc8c4336fa85b7f09e5ca
Steve Dougherty <steve@asksteved.com>
2012-03-17 19:12:34 -0400

    Apache actually uses suffixes for language codes.

--- make-pages.sh
+++ make-pages.sh
@@ -5,7 +5,7 @@ echo -e "\ngenerating the website..."
 rm -Rf output
 mkdir output
 for lang in en es; do
-    (ls -1 "pages/$lang/" | sed -n "s/\\(.*\\)\.php/\\1/p") | (while read x; do ./make-static-page.sh "$lang" "$x" > "output/${x}.$lang.html"; done)
+    (ls -1 "pages/$lang/" | sed -n "s/\\(.*\\)\.php/\\1/p") | (while read x; do ./make-static-page.sh "$lang" "$x" > "output/${x}.html.$lang"; done)
 done
 cp -a !(pages|includes|output|legacy_pages|*.php|*.sh|README.md) output
 echo "done."
