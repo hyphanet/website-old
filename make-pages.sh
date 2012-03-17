fdf75580bdfd7504dda655e0e40cff127839723d
Steve Dougherty <steve@asksteved.com>
2012-03-17 15:19:31 -0400

    Output languages as part of filename before the file type for use of mod_negotiate localization.

--- make-pages.sh
+++ make-pages.sh
@@ -6,8 +6,9 @@ if [ $? -ne 1 ]; then echo "All the links above should be relative to / (otherwi
 echo -e "\ngenerating the website..."
 rm -Rf output
 mkdir output
-(ls -1 pages/en/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output/en; while read x; do ./make-static-page.sh "en" "$x" > output/en/${x}.html; done)
-(ls -1 pages/es/ | sed -n "s/\\(.*\\)\.php/\\1/p") | (mkdir output/es; while read x; do ./make-static-page.sh "es" "$x" > output/es/${x}.html; done)
+for lang in en es; do
+    (ls -1 "pages/$lang/" | sed -n "s/\\(.*\\)\.php/\\1/p") | (while read x; do ./make-static-page.sh "$lang" "$x" > "output/${x}.$lang.html"; done)
+done
 cp -a * output
 rm -Rf output/pages output/includes output/output output/legacy_pages output/*.php output/*.sh output/README.md
 echo "done."
