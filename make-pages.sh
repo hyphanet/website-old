76aced4b89727fdc31f37755faf6f97794c82965
Florent Daigniere <nextgens@freenetproject.org>
2012-04-09 18:11:31 +0100

    Ensure that we all have the same mtimes on our files

--- make-pages.sh
+++ make-pages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -o errexit
 shopt -s extglob
+echo -e "\nResetting the Mtime of files..."
+git log --name-only --date=iso --reverse --pretty=format:%at "$@"| perl -00ln -e '($d,@f)=split/\n/;$d{$_}=$d for grep{-e}@f' -e '}{utime undef,$d{$_},$_ for keys%d'
 echo -e "\ngenerating the website..."
 rm -Rf output
 mkdir output
