6bd56b80e7160633c934bc4ac02d680b78646e71
Steve Dougherty <steve@asksteved.com>
2012-09-09 12:03:49 -0400

    Correct Freenode domain name on chat page.

--- pages/en/irc.php
+++ pages/en/irc.php
@@ -1,7 +1,7 @@
 <h1>Chat with us on IRC</h1>
 
 <p>Many of the developers and users of Freenet can often be found on an <a href="https://en.wikipedia.org/wiki/IRC">IRC</a> channel,
-#freenet on irc.freenode.org. Almost everyone on the channel is a volunteer, and 
+#freenet on chat.freenode.net. Almost everyone on the channel is a volunteer, and 
 may be busy with other things, so you may not get an instant answer: <b>Ask your 
 question/say what the problem is, then wait for a few minutes, and somebody 
 may help you</b>.</p>
--- pages/es/irc.php
+++ pages/es/irc.php
@@ -1,7 +1,7 @@
 <h1>Conversa con nosotros en IRC</h1>
 
 <p>Muchos de los desarrolladores y usuarios de Freenet pueden a menudo ser encontrados en el canal <a href="https://en.wikipedia.org/wiki/IRC">IRC</a>,
-#freenet en irc.freenode.org. Casi todos en el canal son voluntarios, y 
+#freenet en chat.freenode.net. Casi todos en el canal son voluntarios, y 
 pueden estar ocupados con otras cosas, así que quizas no tengas una respuesta inmediata: <b>Haz tu 
 pregunta/di cual es el problema, luego espera unos pocos minutos, y alguien  
 te ayudará</b>.</p>
