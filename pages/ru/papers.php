e9ab21182c2a8ae4355f545b3b6b480da3bb2594
wannamore <deiamformb@gmail.com>
2013-06-10 21:05:34 +0100

    Russian translation for papers.php

---
+++ pages/ru/papers.php
@@ -0,0 +1,95 @@
+<div id="papers">
+  <h1>Документы</h1>
+
+  <p>
+    <img src="/image/pdf.icon.jpg"/>
+    <a href="/papers/freenet-0.7.5-paper.pdf">Темный Фринет</a> (PDF)<br/>
+    Подробный документ о сети Фринет версии 0.7.5, рассматривающий разные вопросы кроме алгоритма маршрутизации, 
+    который уже был подробно описан в других документах. В документ вошли несколько новых симуляций. Он был 
+    представлен к PET 2010.
+  </p>
+
+    <img style="margin-right: 16px;" src="/image/video.icon.png" />
+  <p>
+
+    <a href="/22c3vid.html">Видео выступления про Малые Миры, Берлин, Декабрь 2005</a><br/>
+    Это видео – разговор Яна Кларка и Оскара Сэндберга на Chaos Computer Congress в Берлине в декабре 2005 года, 
+    описывающий новую архитектуру Фринет версии 0.7. Также вы можете загрузить <a href="/papers/ccc/ccc-slideshow.pdf.bz2">слайд-шоу</a>, 
+    и исходный <a href="/papers/ccc/ccc-freenet-demo.tar.bz2">демонстрационный код</a> для Java (требуется 
+    поддержка Java 1.5).
+  </p>
+
+  <p>
+    <img src="/image/pdf.icon.jpg"/>
+    <a href="/papers/lic.pdf">Поиск в Малых Мирах</a> (PDF)<br/>
+    Диссертация Оскара Сэндберга, описывающая простые распределенные механизмы для конструирования сетей Малых 
+    Миров. Он был вдохновлен оригинальным дизайном Фринет. Вторая часть диссертации описывает основы архитектуры 
+    даркнет.
+  </p>
+
+  <p>
+    <img src="/image/pdf.icon.jpg"/>
+    <a href="/papers/swroute.pdf">Распределенная маршрутизация в Малых Мировых Сетях</a> (PDF)<br/>
+    Документ Оскара Сэндберга, описывающий теоретическую основу для нового механизма маршрутизации «даркнет», 
+    введенного в Фринет версии 0.7.
+  </p>
+  
+  <p>
+    <img src="/image/pdf.icon.jpg"/>
+    Выступление на <a href="http://events.ccc.de/congress/2005/fahrplan/events/492.en.html">Chaos Computer 
+    Congress</a> (слайд-шоу)<br/>
+    Это <a href="/papers/ccc/ccc-slideshow.pdf.bz2">слайд-шоу</a> беседы с Chaos Computer Congress 30 декабря 
+    2005 в Берлине в Германии Яна Кларка и Оскара Сэндберга. Оно описывает новый подход – даркнет, который будет 
+    использоваться в Фринет версии 0.7. Java демонстрация, сопровождающая беседу, <a href="/papers/ccc/ccc-freenet-demo.tar.bz2">также 
+    доступна</a>.    
+  </p>
+
+  <p>
+    <img src="/image/pdf.icon.jpg"/>
+    <a href="/papers/vilhelm_thesis.pdf">Коммутация для Малых Миров</a> (PDF)<br/>
+    Научный труд Вильгельма Веренделя, изучающий пути оптимизации алгоритма обмена.
+  </p>
+  
+  <p>
+    <img src="/image/pdf.icon.jpg"/>
+    <a href="http://grothoff.org/christian/pitchblack.pdf">Маршрутизация в Темноте: Pitch Black</a> (<a href="http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.117.1543">citeseer</a>) (PDF)<br/>
+    Документ, описывающий некоторые атаки на алгоритм обмена Фринет с целью выявить местоположение нод-узлов.
+  </p>
+  
+  <p style="margin-left: 39px;">
+    <a href="/ngrouting.html">Алгоритм маршрутизации «Next Generation»</a><br/>
+    Эта статья описывает алгоритм маршрутизации «Next Generation». Он использует новый подход, при котором 
+    нод-узлы принимают маршрутные решения на основе сложного анализа времени, потребовавшегося для маршрутизации 
+    предыдущих запросов. Этот алгоритм многообещающий, но мы отказались от него в конечном итоге в пользу 
+    алгоритма с гораздо более простым подходом в Фринет 0.7.
+  </p>
+  
+  <p>
+    <img src="/image/pdf.icon.jpg"/>
+    <a href="/papers/freenet-ieee.pdf">Защита свободы информации онлайн с Фринет</a> (PDF)<br/>
+    Статья IEEE Internet Computing описывающая архитектуру Фринет примерно в 2002 году, вероятно, лучшее введение 
+    в теорию, стоящую за Фринет.
+  </p>
+  
+  <p>
+    <img src="/image/pdf.icon.jpg"/>
+    <a href="/papers/ddisrs.pdf">Техническое описание Фринет</a> (PDF)<br/>
+    Оригинальное техническое описание Фринет Яна Кларка, кафедра информатики Университета Эдинбурга, 1999.
+  </p>
+  
+  <hr/>
+  
+  <p>
+    Смотрите также остальную <a href="https://wiki.freenetproject.org/FreenetSpecifications">документацию для 
+    разработчиков</a>.
+  <p>
+  
+  <!-- It's way out of date
+  <p>
+    And an unofficial, more
+    elaborate <a href="http://www.freenethelp.org">Freenet Help
+      Site</a> was created, to go deeper into the questions
+    newbies may have about the usage and  inner-workings of
+    Freenet.
+  </p>-->
+</div>
