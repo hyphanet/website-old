0f20918a4b86e1faaf27bc29c81ac10c55572e36
Steve Dougherty <steve@asksteved.com>
2013-07-11 16:12:44 -0400

    Phrasing.

--- pages/en/index.php
+++ pages/en/index.php
@@ -135,7 +135,7 @@ $(document).ready(function() {
     <div style="clear: both"></div>
 
     <div id="commits">
-        <h4>Latest Commits</h4>
+        <h4>Latest Developer Activity</h4>
         <div id="latest-commits"></div>
     </div>
     <script type="text/javascript">
