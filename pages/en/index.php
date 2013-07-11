b199cec2ec886bb08653ec7650b02d981e102889
Steve Dougherty <steve@asksteved.com>
2013-07-11 14:11:31 -0400

    Update English page to jQuery 1.10.2.
    Keep 1.3 around for now as well. Still testing.

--- pages/en/index.php
+++ pages/en/index.php
@@ -1,7 +1,7 @@
 <!--
   jQuery library
 -->
-<script type="text/javascript" src="/js/jquery-1.3.min.js"></script>
+<script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
 
 <!--
   jCarousel library
