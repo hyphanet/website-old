6d184a5380e588e5c78d76e8fbf4323898d071c1
Matthew Toseland <toad@amphibian.dyndns.org>
2013-08-01 22:42:04 +0100

    Add some recent papers (not all of them). Use an <hr> to separate the ones that are "ours".

--- pages/en/papers.php
+++ pages/en/papers.php
@@ -50,11 +50,6 @@
 	  <a href="/papers/vilhelm_thesis.pdf">Switching for a small world</a> (PDF)<br/>
 	  A thesis by Vilhelm Verendel exploring ways to optimise the swapping algorithm.
 	</p>
-	<p>
-	  <img src="/image/pdf.icon.jpg"/>
-	  <a href="http://grothoff.org/christian/pitchblack.pdf">Routing in the Dark: Pitch Black</a> (<a href="http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.117.1543">citeseer</a>) (PDF)<br/>
-	  A paper describing some attacks on Freenet 0.7's location swapping algorithm.
-	</p>
 	<p style="margin-left: 39px;">
 	  <a href="/ngrouting.html">Next Generation Routing Algorithm</a><br/>
 	  This article describes Freenet's "Next Generation" routing algorithm.  This was a novel
@@ -74,6 +69,35 @@
 	  <img src="/image/pdf.icon.jpg"/>
 	  <a href="/papers/ddisrs.pdf">FreeNet White Paper</a> (PDF)<br/>
 	  Original white paper by Ian Clarke, Division of Informatics, University of Edinburgh 1999.
+	</p>
+	<hr/>
+	<p>
+	  <img src="/image/pdf.icon.jpg"/>
+	  <a href="http://www.ukp.tu-darmstadt.de/fileadmin/user_upload/Group_P2P/share/publications/Attack_Resistant_Network_Embeddings_for_Darknets.pdf">Attack Resistant Network Embeddings for Darknets</a> (PDF)<br/>
+	  A proposal for changing the darknet swapping algorithm which we are still considering
+	  (we have some doubts about long-term performance).
+	</p>
+	<p>
+	  <img src="/image/pdf.icon.jpg"/>
+	  <a href="http://www.p2p.tu-darmstadt.de/publications/details/?no_cache=1&tx_bibtex_pi1%5Bpub_id%5D=TUD-CS-2013-0036">A Contribution to Analyzing and Enhancing Darknet Routing</a> (<a href="http://www.p2p.tu-darmstadt.de/fileadmin/user_upload/Group_P2P/share/INFOCOM.pdf">PDF</a>)<br/>
+	  A proposal for changing the routing algorithm which we are still considering (the worst case 
+	  performance i.e. when a block has been lost may be unacceptable).
+	</p>
+	<p>
+	  <img src="/image/pdf.icon.jpg"/>
+	  <a href="https://www.icsi.berkeley.edu/icsi/sites/default/files/events/events_1303_strufe.pdf">Presentation: Towards "Dark" Social Networking Services (Strufe et al)</a> (PDF)<br/>
+	  An interesting presentation by the group responsible for the two above papers.
+	</p>
+	<p>
+	  <img src="/image/pdf.icon.jpg"/>
+	  <a href="http://arxiv.org/abs/1208.6326">Pisces: Anonymous Communication Using Social Networks</a><br/>
+	  An algorithm for setting up onion-like tunnels on darknets. We may implement this for Freenet 0.8/0.9.
+	</p>
+	<p>
+	  <img src="/image/pdf.icon.jpg"/>
+	  <a href="http://grothoff.org/christian/pitchblack.pdf">Routing in the Dark: Pitch Black</a> (<a href="http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.117.1543">citeseer</a>) (PDF)<br/>
+	  A paper describing some attacks on Freenet 0.7's location swapping algorithm. We have solutions for this
+	  but they are still being tested.
 	</p>
 	<hr/>
 	<p>
