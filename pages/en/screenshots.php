c4eb52d0dc8b22911c4914bc43307fbdf081cd53
Steve Dougherty <steve@asksteved.com>
2014-07-12 14:08:15 -0400

    Remove thickbox class from non-JS screenshots page

--- pages/en/screenshots.php
+++ pages/en/screenshots.php
@@ -1,8 +1,8 @@
 	<div id="screenshots">
            <ul>
-             <li><a href="image/screenshot/browse.png" class="thickbox"><img src="image/screenshot/browse.png" width="383" height="311" alt="Default bookmarks" /></a></li>  
-             <li><a href="image/screenshot/sone.png" class="thickbox"><img src="image/screenshot/sone.png" width="383" height="298" alt="Sone" /></a></li>
-             <li><a href="image/screenshot/fms.png" class="thickbox"><img src="image/screenshot/fms.png" width="383" height="298" alt="FMS forum" /></a></li>
+             <li><a href="image/screenshot/browse.png"><img src="image/screenshot/browse.png" width="383" height="311" alt="Default bookmarks" /></a></li>
+             <li><a href="image/screenshot/sone.png"><img src="image/screenshot/sone.png" width="383" height="298" alt="Sone" /></a></li>
+             <li><a href="image/screenshot/fms.png"><img src="image/screenshot/fms.png" width="383" height="298" alt="FMS forum" /></a></li>
            </ul>
 	</div>
 
