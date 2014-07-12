905661e7feae6549c2c431fee072bd4843b108d0
Steve Dougherty <steve@asksteved.com>
2014-07-12 14:04:53 -0400

    Remove screenshot in which someone talks about anti-piracy resistance
    toad_ is concerned that featuring it could be interpreted as endorsement
    of piracy.

--- pages/en/screenshots.php
+++ pages/en/screenshots.php
@@ -3,7 +3,6 @@
              <li><a href="image/screenshot/browse.png" class="thickbox"><img src="image/screenshot/browse.png" width="383" height="311" alt="Default bookmarks" /></a></li>  
              <li><a href="image/screenshot/sone.png" class="thickbox"><img src="image/screenshot/sone.png" width="383" height="298" alt="Sone" /></a></li>
              <li><a href="image/screenshot/fms.png" class="thickbox"><img src="image/screenshot/fms.png" width="383" height="298" alt="FMS forum" /></a></li>
-             <li><a href="image/screenshot/fms2.png" class="thickbox"><img src="image/screenshot/fms2.png" width="383" height="298" alt="FMS thread" /></a></li>
            </ul>
 	</div>
 
