821c6922dfb71da9f2a2f48ca6b10906ebf7c7e0
drak@kaverne <drak@kaverne>
2015-01-24 08:50:17 +0100

    update the list of people.

--- pages/en/people.php
+++ pages/en/people.php
@@ -34,6 +34,90 @@
 	  boutique security consultancy firm.
 	</p>
 
+<h5>toad</h5>
+
+<p>The first paid developer. He has been pushing freenet through obstacles for ten years which no sane person would have dared to tackle - and who succeeded at solving at least three unsolvable problems. Now took up studying CS again at Cambridge. Previously a zealous christian who believed that god would judge him fairly, now an atheist who fights for the future of human civilization.
+</p>
+
+<h5>operhiem1</h5>
+
+<p>Really good at integrating the huge number of different libraries and tools which make up freenet and at the same time able to keep cheerful contact with the folks who contribute to freenet. Now provides his skills at writing very clean code and documenting it effectively to a company for paying back his student loan - besides being freenet maintainer and release manager.
+</p>
+
+<h5>nextgens</h5>
+
+<p>Long-term contributor who tends to be proven right in the end and works as full-time security consultant besides taking on the crazy crypto challenges in freenet and mentoring GSoC students on tackling hard crypto tasks.
+</p>
+
+<h5>digger3</h5>
+
+<p>Long-term contributor, statistics-guru, very friendly (when he thinks that being friendly is constructive in the situation), good at quickly solving problems in three days which take months when others tackle - and now finished with his PhD. Moved to teaching collegues at his employer that having a camera in the lunch room is a pretty bad idea.
+</p>
+
+<h5>p0s/xor/he-who-cannot-be-named</h5>
+
+<p>The one who actually implemented WoT and who’s taken on the mission on making it work well and fast, despite knowing that scaling distributed spam-protection is a problem which isn’t even solved in the clearnet. The second paid developer. Prefers being paid in BitCoin. Actually works sensible hours and managed to get his stuff together while working on freenet.
+</p>
+
+<h5>Bombe</h5>
+
+<p>The demo-scene artist in exile who writes Sone, the first freenet application which looks like it’s from the current millenium, and enables actually working,decentralized, anonymous microblogging in twitter- or facebook-style using the Web of Trust to find new people - and get rid of spammers. He’s pretty good at avoiding distractions by anything which looks like trolling.
+</p>
+
+<h5>Eleriseth</h5>
+
+<p>An anonymous contributor who provides pretty nice core-fixes over FMS. Went head-on-head with toad in autumn 2013 and took a break afterwards. Started again in2014. Named here, because his patches are great and got merged and because anonymous contributors are the central pillar we need to strengthen to make Freenet future proof if our political systems keep degrading at the current pace.
+</p>
+
+
+<h5>somedude</h5> 
+<p>The anonymous contributor who writes FMS. I don’t know anything about him. Or her. Except that somedude uses FreenetHg and writes FMS in C++.
+</p>
+
+<h5>infinity0</h5> 
+<p>One of the people doing good work without showing off. I think. Actually below my radar most of the time - which can be a good sign ☺
+</p>
+
+<h5>bertm</h5> 
+<p>Joined in 2014 and seems pretty sane, friendly and competent - a strange mix, though much less rare in our community than it seems from the outside.
+</p>
+
+<h5>TheSeeker</h5> 
+<p>Long-term contributor. Claims to not be able to code, and at the same time fixes plugins and ponders rewriting Freenet in Go ☺
+</p>
+
+<h5>SeekingFor</h5> 
+<p>Long-term contributor. Might once have used the suffix Attention.
+</p>
+
+<h5>Lacrocivious</h5> 
+<p>An unwilling windows wizard, but not user, with decades (yes, decades!) of knowledge about building computers and the damage Microsoft did to computing. Tries to push us to realize the impact Freenet could have. 
+</p>
+
+<h5>Tommy[D]</h5> 
+<p>Gentoo packager who actually pulled off the stunt of untangling all the dependencies of freenet-ext.
+</p>
+
+<h5>The folks from Frost</h5> 
+<p>Old-Time users, some with conspiracy theories about the developers, others who patch their nodes to be parasitic for the network. We have them to thank for the <a href="/USK@ZLwcSLwqpM1527Tw1YmnSiXgzITU0neHQ11Cyl0iLmk,f6FLo3TvsEijIcJq-X3BTjjtm0ErVZwAPO7AUd9V7lY,AQACAAE/fix-link-length/14/">link-length-fix</a>. If they had not broken the network this badly, we would likely not have dared to try the fix.
+</p>
+
+<h5>ArneBab</h5> 
+<p>Long-term Freenet user, Mid-term Freenet contributor, long-term Pythonista, fledgling Schemer. Switched from Gnutella to Freenet, because Freenet has the strongest legal case: Fighting for Freedom of the Press and Freedom of Expression. “When free speech dies, we need a place to organize to take up the political battle”.
+</p>
+
+
+	<p>
+	  And <b>hundreds of others</b>, who either haven't asked to be added here, who 
+	  prefer to remain nameless, or who we just haven't got around to thanking. 
+	  Not to mention thousands of users, testers,
+	  and <a href="sponsors.html">donors</a>!
+	</p>
+
+
+
+<h1>Past contributors</h1>
+
 	<h5>Scott Miller</h5>
 	<p>
 	  Scott is responsible for the implementation of much of the cryptography
@@ -72,13 +156,6 @@
 	  David (Zothar) has helped the Freenet project as time permits and interest
 	  directs, including configuration, statistics and peer management via FCP,
 	  the FProxy stats page and Node 2 Node Messages (N2NM/N2NTMs).
-	</p>
-
-	<p>
-	  And <b>hundreds of others</b>, who either haven't asked to be added here, who 
-	  prefer to remain nameless, or who we just haven't got around to thanking. 
-	  Not to mention thousands of users, testers,
-	  and <a href="sponsors.html">donors</a>!
 	</p>
 
       </div>
