e3b38bf02659254ee680ecc7ecb700888e11935f
drak@kaverne <drak@kaverne>
2015-05-30 11:32:25 +0200

    fix tag typo

--- pages/en/latestnews.php
+++ pages/en/latestnews.php
@@ -1,9 +1,9 @@
-		  (2015-05-22) <a href="news.html#20150522-united-nations-report-anonymity">UN: Encryption and Anonymity Deserve Strong Protection</a></br>
-		  (2015-04-14) <a href="news.html#20150414-suma-video">SUMA Award Acceptance Speech (video)</a></br>
-		  (2015-03-29) <a href="news.html#20150329-new-installer">New Linux/Unix/OS X installer</a></br>
-		  (2015-03-15) <a href="news.html#20150315-1468-progress">Progress toward build 1468</a></br>
-		  (2015-03-14) <a href="news.html#20150314-localization-lab">Freenet translation joins Localization Lab</a></br>
-		  (2015-02-15) <a href="news.html#20150215-csharp-tray">New Windows tray app for testing</a></br>
+		  (2015-05-22) <a href="news.html#20150522-united-nations-report-anonymity">UN: Encryption and Anonymity Deserve Strong Protection</a><br/>
+		  (2015-04-14) <a href="news.html#20150414-suma-video">SUMA Award Acceptance Speech (video)</a><br/>
+		  (2015-03-29) <a href="news.html#20150329-new-installer">New Linux/Unix/OS X installer</a><br/>
+		  (2015-03-15) <a href="news.html#20150315-1468-progress">Progress toward build 1468</a><br/>
+		  (2015-03-14) <a href="news.html#20150314-localization-lab">Freenet translation joins Localization Lab</a><br/>
+		  (2015-02-15) <a href="news.html#20150215-csharp-tray">New Windows tray app for testing</a><br/>
           (2015-02-11) <a href="news.html#20150211-suma-award">Freenet received the SUMA Award 2015</a><br/>
           (2015-01-05) <a href="news.html#20150105-mempo-apt-get">apt-get over Freenet</a><br/>
 		  (2014-11-23) <a href="news.html#build01467">Freenet 0.7.5 build 1467 released</a><br/>
