e491ef30e859fd0a9a4b38d17f2afd19ee63ff3e
Steve Dougherty <steve@asksteved.com>
2015-07-13 23:34:05 -0400

    Add pt-br entry for documentation page

--- includes/documentation.inc.php
+++ includes/documentation.inc.php
@@ -5,6 +5,8 @@ $pages = array(
 'en' => 'pages/en/documentation.php',
 'es' => 'pages/es/documentation.php',
 'fr' => 'pages/fr/documentation.php',
-'de' => 'pages/de/documentation.php')
+'de' => 'pages/de/documentation.php',
+'pt-br' => 'pages/pt-br/documentation.php',
+)
 
 ?>
