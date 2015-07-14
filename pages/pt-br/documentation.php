3ef7b1faa9e936268ac069554cb0ffee97a6b026
Steve Dougherty <steve@asksteved.com>
2015-07-13 23:01:04 -0400

    Translate documentation page

---
+++ pages/pt-br/documentation.php
@@ -0,0 +1,61 @@
+      <h1>Documentation</h1>
+      <div id="navigation">
+	<p> Go to :
+	  <a href="install.html">Instalar</a>
+	  <a href="connect.html">Conectar</a>
+	  <a href="content.html">Conteúdo</a>
+	  <a href="understand.html">Entendi</a>
+	  <a href="freemail.html">Freemail</a>
+	  <a href="frost.html">Frost</a>
+	  <a href="jsite.html">jSite</a>
+	  <a href="https://wiki.freenetproject.org">Wiki</a>
+	</p>
+      </div>
+      <p>
+	A documentação de Freenet este sempre mudando, já que este é um projeto em desenvolvimeto. No entanto, aqui no menu Documentação,
+  você pode encontrar documentos úteis sobre como configurar Freenet e usar suas ferramentas mais populares, bem como um wiki orientado ao utilizador e uma extensa sessão de Perguntas e Respostas Frequentes.
+      </p>
+
+      <ul>
+	<li>
+	  <a href="install.html"><strong>Instalar</strong></a><br/>
+	  Contém instruções detalhadas  de como instalar Freenet usando o novo instalador.
+	</li>
+
+	<li>
+	  <a href="connect.html"><strong>Conectar</strong></a><br/>
+	  Como conectar-se à Freenet.
+	</li>
+
+	<li>
+	  <a href="content.html"><strong>Conteúdo</strong></a><br/>
+	  Freenet como um Sistema de Distribuição de Conteúdo.
+	</li>
+
+	<li>
+	  <a href="understand.html"><strong>Entendi</strong></a><br/>
+	  Explica o funcionamento de Freenet, e um glossário com os termos fequentemente mais usados.
+	</li>
+
+	<li>
+	  <a href="freemail.html"><strong>Freemail</strong></a><br/>
+	  Como configurar o seu próprio serviço de email anônimo Freenet.
+	</li>
+
+	<li>
+	  <a href="frost.html"><strong>Frost</strong></a><br/>
+	  Frost é a mais antiga e usada ferramenta de compartilhamento de arquivos na suite Freenet. Aqui há uma descrição de como configurá-la e usá-la pela primeira vez.
+	</li>
+
+	<li>
+	  <a href="jsite.html"><strong>jSite</strong></a><br/>
+	  jSite é uma ferramenta de inserção de websites na Freenet (a.k.a. Freesite) .
+	</li>
+
+	<li>
+	  <a href="https://wiki.freenetproject.org/"><strong>Wiki</strong></a><br/>
+	  A Freenet Wiki é um espaço voltado para os usuários onde novas funcionalidades podem ser descritas antes de
+    serem encontradas no website. É uma boa fonte de informação sobre todos os tipos de questões relativas à Freenet.
+	</li>
+
+      </ul>
