a8913447f1c258e54b96379a2b201c8497f6d988
Steve Dougherty <steve@asksteved.com>
2015-09-17 23:42:40 -0400

    Remove 6th latest news entry
    The target number of entries kept is 5.

--- pages/en/latestnews.php
+++ pages/en/latestnews.php
@@ -3,4 +3,3 @@
           (2015-08-07) <a href="news.html#20150807-1469-wininstaller">Windows translation updates</a></br>
           (2015-07-19) <a href="news.html#20150719-1469-release">Freenet 0.7.5 build 1469 released</a></br>
           (2015-07-13) <a href="news.html#20150713-1468-known-issues">Build 1468 known issues</a></br>
-          (2015-07-11) <a href="news.html#20150711-1468-release">Freenet 0.7.5 build 1468 released</a></br>
